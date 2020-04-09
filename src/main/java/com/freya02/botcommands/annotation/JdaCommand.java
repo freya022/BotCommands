a3459bd1fe66c2754a65537c06b15d8f2f16069b
freya022 <41875020+freya022@users.noreply.github.com>
2020-04-09 15:16:37 +0200

    Add docs

--- src/main/java/com/freya02/botcommands/annotation/JdaCommand.java
+++ src/main/java/com/freya02/botcommands/annotation/JdaCommand.java
@@ -13,29 +13,49 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface JdaCommand {
-    /** Primary name of the command, <b>must not contain any spaces</b> */
+    /** Primary name of the command, <b>must not contain any spaces</b>
+     * @return Name of the command
+     */
     String name();
-    /** Secondary names of the command, <b>must not contain any spaces</b> */
+    /** Secondary names of the command, <b>must not contain any spaces</b>
+     * @return Secondary names of the command
+     */
     String[] aliases() default {};
-    /** Short description of the command, it is displayed in the help command */
+    /** Short description of the command, it is displayed in the help command
+     * @return Short description of the command
+     */
     String description() default "No description";
 
-    /** Required {@linkplain Permission permissions} of the bot */
+    /** Required {@linkplain Permission permissions} of the bot
+     * @return Required {@linkplain Permission permissions} of the bot
+     */
     Permission[] botPermissions() default {};
-    /** Required {@linkplain Permission permissions} of the user */
+    /** Required {@linkplain Permission permissions} of the user
+     * @return Required {@linkplain Permission permissions} of the user
+     */
     Permission[] userPermissions() default {};
 
-    /** Required role of the user */
+    /** Required role of the user
+     * @return Required role of the user
+     */
     String requiredRole() default "";
 
-    /** Cooldown time <b>in milliseconds</b> before the command can be used again in the scope specified by {@linkplain #cooldownScope()} */
+    /** Cooldown time <b>in milliseconds</b> before the command can be used again in the scope specified by {@linkplain #cooldownScope()}
+     * @return Cooldown time
+     */
     int cooldown() default 0;
-    /** Scope of the cooldown, can be either {@linkplain CooldownScope#USER}, {@linkplain CooldownScope#CHANNEL} or {@linkplain CooldownScope#GUILD} */
+    /** Scope of the cooldown, can be either {@linkplain CooldownScope#USER}, {@linkplain CooldownScope#CHANNEL} or {@linkplain CooldownScope#GUILD}
+     * @return Scope of the cooldown
+     */
     CooldownScope cooldownScope() default CooldownScope.USER;
 
-    /** Name of the category the command should be in */
+    /** Name of the category the command should be in
+     * @return Name of the category
+     */
     String category() default "No category";
 
-    /** Classes of the subcommands for this command, they must extend {@linkplain Command} and have the annotation {@linkplain JdaSubcommand} */
+    /** Classes of the subcommands for this command, they must extend {@linkplain Command} and have the annotation {@linkplain JdaSubcommand}
+     * @return List of subcommand classes
+     */
     Class<? extends Command>[] subcommands() default {};
 }
--- src/main/java/com/freya02/botcommands/annotation/JdaSubcommand.java
+++ src/main/java/com/freya02/botcommands/annotation/JdaSubcommand.java
@@ -11,21 +11,35 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface JdaSubcommand {
-    /** Primary name of the command, <b>must not contain any spaces</b> */
+    /** Primary name of the command, <b>must not contain any spaces</b>
+     * @return Name of the command
+     */
     String name();
-    /** Short description of the command, it is displayed in the help command */
+    /** Short description of the command, it is displayed in the help command
+     * @return Short description of the command
+     */
     String description() default "No description";
 
-    /** Required {@linkplain Permission permissions} of the bot */
+    /** Required {@linkplain Permission permissions} of the bot
+     * @return Required {@linkplain Permission permissions} of the bot
+     */
     Permission[] botPermissions() default {};
-    /** Required {@linkplain Permission permissions} of the user */
+    /** Required {@linkplain Permission permissions} of the user
+     * @return Required {@linkplain Permission permissions} of the user
+     */
     Permission[] userPermissions() default {};
 
-    /** Required role of the user */
+    /** Required role of the user
+     * @return Required role of the user
+     */
     String requiredRole() default "";
 
-    /** Cooldown time <b>in milliseconds</b> before the command can be used again in the scope specified by {@linkplain #cooldownScope()} */
+    /** Cooldown time <b>in milliseconds</b> before the command can be used again in the scope specified by {@linkplain #cooldownScope()}
+     * @return Cooldown time
+     */
     int cooldown() default 0;
-    /** Scope of the cooldown, can be either {@linkplain CooldownScope#USER}, {@linkplain CooldownScope#CHANNEL} or {@linkplain CooldownScope#GUILD} */
+    /** Scope of the cooldown, can be either {@linkplain CooldownScope#USER}, {@linkplain CooldownScope#CHANNEL} or {@linkplain CooldownScope#GUILD}
+     * @return Scope of the cooldown
+     */
     CooldownScope cooldownScope() default CooldownScope.USER;
 }
