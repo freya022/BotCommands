fa3793f94ef7202dadfbddec9f80c8dc56791aed
freya022 <41875020+freya022@users.noreply.github.com>
2020-04-09 13:58:13 +0200

    Add docs

--- src/main/java/com/freya02/botcommands/CooldownScope.java
+++ src/main/java/com/freya02/botcommands/CooldownScope.java
@@ -1,7 +1,19 @@
 package com.freya02.botcommands;
 
+import com.freya02.botcommands.annotation.JdaCommand;
+import com.freya02.botcommands.annotation.JdaSubcommand;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+/**
+ * <p>Provides cooldown scopes for executed commands</p>
+ * <p>The cooldown time is specified with {@linkplain JdaCommand#cooldown()} <i>or {@linkplain JdaSubcommand#cooldown()} for subcommands</i></p>
+ */
 public enum CooldownScope {
+    /** Enables cooldown for the user who called the command */
     USER,
+    /** Enables cooldown for the {@linkplain Guild guild} the command got called in */
     GUILD,
+    /** Enables cooldown for the {@linkplain TextChannel TextChannel} the command got called in */
     CHANNEL
 }
