bb8fc8253657ba8f38c7d9723297736f2c172659
freya022 <41875020+freya022@users.noreply.github.com>
2020-04-09 14:27:32 +0200

    Add docs

--- src/main/java/com/freya02/botcommands/utils/SimpleStream.java
+++ src/main/java/com/freya02/botcommands/utils/SimpleStream.java
@@ -1,9 +1,18 @@
 package com.freya02.botcommands.utils;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.function.Consumer;
 
+/**
+ * <p>Provides an {@linkplain InputStream} without exceptions</p>
+ * <p>The stream can be closed without having to handle exceptions</p>
+ * <p>If an exception occurs, then the <code>onException</code> consumer is used</p>
+ * <p>This is particularly useful when closing streams in RestAction success / failure callbacks to reduce boilerplate code</p>
+ */
 public class SimpleStream extends InputStream {
 	private final InputStream stream;
 	private final Consumer<? super Throwable> onException;
@@ -13,7 +22,12 @@ public class SimpleStream extends InputStream {
 		this.onException = onException;
 	}
 
-	public static SimpleStream of(InputStream stream, Consumer<? super Throwable> onException) {
+	/** Constructs a new SimpleStream with the specified {@linkplain InputStream} and the exception consumer
+	 * @param stream {@linkplain InputStream} to use when doing reading / closing operations
+	 * @param onException {@linkplain Consumer} to use when an exception occurs when closing the stream
+	 * @return A {@linkplain SimpleStream}
+	 */
+	public static SimpleStream of(@NotNull InputStream stream, @Nullable Consumer<? super Throwable> onException) {
 		return new SimpleStream(stream, onException);
 	}
 
