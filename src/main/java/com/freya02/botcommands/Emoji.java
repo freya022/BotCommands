dc1da30153df010f8bd56782bb822e9551e0de2a
freya022 <41875020+freya022@users.noreply.github.com>
2021-02-23 23:44:42 +0100

    Add docs

--- src/main/java/com/freya02/botcommands/Emoji.java
+++ src/main/java/com/freya02/botcommands/Emoji.java
@@ -2,6 +2,9 @@ package com.freya02.botcommands;
 
 import net.dv8tion.jda.api.entities.IMentionable;
 
+/**
+ * Represents an Unicode Emoji
+ */
 public interface Emoji extends IMentionable {
 	String getUnicode();
 }
--- src/main/java/com/freya02/botcommands/annotation/Optional.java
+++ src/main/java/com/freya02/botcommands/annotation/Optional.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Denotes an optional parameter in an {@linkplain Executable} command
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface Optional { }
