5009f71fc3362c00dccfc5705f22e02baf8a6ae6
freya022 <41875020+freya022@users.noreply.github.com>
2021-02-27 14:39:49 +0100

    Remove never thrown exception

--- src/main/java/com/freya02/botcommands/regex/PatternSolver.java
+++ src/main/java/com/freya02/botcommands/regex/PatternSolver.java
@@ -1,8 +1,7 @@
 package com.freya02.botcommands.regex;
 
-import com.freya02.botcommands.exceptions.BadIdException;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public interface PatternSolver {
-	Object solve(GuildMessageReceivedEvent event, String[] groups) throws BadIdException;
+	Object solve(GuildMessageReceivedEvent event, String[] groups);
 }
