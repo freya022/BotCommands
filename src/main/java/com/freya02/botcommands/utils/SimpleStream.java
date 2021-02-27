a2c4135048168bbbf148aab3573077ea3c26096d
freya022 <41875020+freya022@users.noreply.github.com>
2021-02-27 14:03:44 +0100

    Auto close SimpleStream when unreachable

--- src/main/java/com/freya02/botcommands/utils/SimpleStream.java
+++ src/main/java/com/freya02/botcommands/utils/SimpleStream.java
@@ -5,21 +5,39 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.ref.Cleaner;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 /**
- * <p>Provides an {@linkplain InputStream} without exceptions</p>
- * <p>The stream can be closed without having to handle exceptions</p>
+ * <p>Provides an {@linkplain InputStream} without {@linkplain #close()} exceptions</p>
+ * <p>The stream is closed automatically once unreachable and collected by the GC</p>
  * <p>If an exception occurs, then the <code>onException</code> consumer is used</p>
- * <p>This is particularly useful when closing streams in RestAction success / failure callbacks to reduce boilerplate code</p>
+ * <p>This is particularly useful when closing streams after RestAction used them, to reduce boilerplate code</p>
  */
 public class SimpleStream extends InputStream {
+	private static final Cleaner CLEANER = Cleaner.create(r -> {
+		final Thread thread = new Thread(r, "SimpleStream Cleaner Thread");
+		thread.setDaemon(true);
+		return thread;
+	});
+
 	private final InputStream stream;
 	private final Consumer<? super Throwable> onException;
 
-	private SimpleStream(InputStream stream, Consumer<? super Throwable> onException) {
-		this.stream = stream;
+	private SimpleStream(InputStream stream, @Nullable Consumer<? super Throwable> onException) {
+		this.stream = Objects.requireNonNull(stream, "Input stream is null");
 		this.onException = onException;
+
+		CLEANER.register(this, () -> {
+			try {
+				stream.close();
+			} catch (IOException e) {
+				if (onException != null) {
+					onException.accept(e);
+				}
+			}
+		});
 	}
 
 	/** Constructs a new SimpleStream with the specified {@linkplain InputStream} and the exception consumer
