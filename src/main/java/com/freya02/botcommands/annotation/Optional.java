73ec9564520e30369cf6e04b0a982d108efc6b2b
freya022 <41875020+freya022@users.noreply.github.com>
2021-05-27 21:58:51 +0200

    More docs for slash command optionals

--- src/main/java/com/freya02/botcommands/annotation/Optional.java
+++ src/main/java/com/freya02/botcommands/annotation/Optional.java
@@ -1,6 +1,7 @@
 package com.freya02.botcommands.annotation;
 
 import com.freya02.botcommands.prefixed.annotation.Executable;
+import com.freya02.botcommands.slash.annotations.JdaSlashCommand;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -8,11 +9,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Denotes an optional parameter in an {@linkplain Executable} command
+ * Denotes an optional parameter in an {@linkplain Executable} or a {@linkplain JdaSlashCommand} command
  *
- * <br><b>The behavior of {@linkplain Executable} commands is pretty unsafe if you combine strings and numbers in the same command</b>
+ * <p>
+ * <h2>For regex commands: Consider this annotation as experimental</h2>
+ * <b>The behavior of {@linkplain Executable} commands is pretty unsafe if you combine strings and numbers in the same command</b>
+ * </p>
  *
- * <h2>Consider this annotation as experimental</h2>
+ * <p>
+ * <h2>For slash commands:</h2>
+ * <b>This works perfectly as it's just a hint for discord</b>
+ * </p>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
--- src/main/java/com/freya02/botcommands/slash/annotations/Option.java
+++ src/main/java/com/freya02/botcommands/slash/annotations/Option.java
@@ -1,5 +1,7 @@
 package com.freya02.botcommands.slash.annotations;
 
+import com.freya02.botcommands.annotation.Optional;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,6 +11,8 @@ import java.lang.annotation.Target;
  * Annotation to set name and description of {@linkplain JdaSlashCommand slash commands}
  * <p>
  * {@linkplain #name()} is optional if parameter name is available (add -parameters to your java compiler)
+ *
+ * @see Optional
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER})
