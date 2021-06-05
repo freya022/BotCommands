89741278b786d89e1a6befb180049917cba6a937
freya022 <41875020+freya022@users.noreply.github.com>
2021-06-05 11:20:32 +0200

    Add doc warns

--- src/main/java/com/freya02/botcommands/annotation/DontInheritPermissions.java
+++ src/main/java/com/freya02/botcommands/annotation/DontInheritPermissions.java
@@ -6,6 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /** Use this annotation if you want the subcommand to not inherit the permissions of its parent
+ *
+ * <br>
+ * <h2>This does not work on slash commands</h2>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
--- src/main/java/com/freya02/botcommands/annotation/RequireOwner.java
+++ src/main/java/com/freya02/botcommands/annotation/RequireOwner.java
@@ -6,6 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /** Use this annotation if you want the command to be usable only by the owners, <b>the 'help' command will still display it</b>
+ *
+ * <br>
+ * <h2>This does not work on slash commands</h2>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
