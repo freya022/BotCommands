eaa3c285479752c97b16c0e91cb3835accc94658
freya022 <41875020+freya022@users.noreply.github.com>
2021-07-13 21:16:01 +0200

    Change expired error message

--- src/main/java/com/freya02/botcommands/components/ComponentErrorReason.java
+++ src/main/java/com/freya02/botcommands/components/ComponentErrorReason.java
@@ -2,7 +2,7 @@ package com.freya02.botcommands.components;
 
 public enum ComponentErrorReason {
 	NOT_OWNER(null),
-	EXPIRED("This component is not associated with an action anymore"),
+	EXPIRED("This component is not usable anymore (expired)"),
 	DONT_EXIST("This component could not be found");
 
 	private String reason;
