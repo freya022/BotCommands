d39a53955b53bd7636093e1787d166d7abdfdc22
freya022 <41875020+freya022@users.noreply.github.com>
2021-07-27 15:44:08 +0200

    Experiment using updated emojis

--- src/main/java/com/freya02/botcommands/utils/EmojiUtils.java
+++ src/main/java/com/freya02/botcommands/utils/EmojiUtils.java
@@ -1,12 +1,12 @@
 package com.freya02.botcommands.utils;
 
-import com.vdurmont.emoji.EmojiParser;
+import com.freya02.emojis.Emojis;
 import net.dv8tion.jda.api.entities.Emoji;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.NoSuchElementException;
 
-public class EmojiUtils extends EmojiParser {
+public class EmojiUtils {
 	/**
 	 * Resolves a shortcode emoji (e.g: :joy:) into a unicode emoji for JDA to use (on reactions for example)
 	 *
@@ -15,17 +15,10 @@ public class EmojiUtils extends EmojiParser {
 	 */
 	@NotNull
 	public static String resolveEmojis(String input) {
-		//Find emoji aliases (shortcodes)
-		AliasCandidate alias = getAliasAt(input, 0);
+		final com.freya02.emojis.Emoji emoji = Emojis.ofShortcode(input);
 
-		if (alias != null)
-			return alias.emoji.getUnicode() + (alias.fitzpatrick == null ? "" : alias.fitzpatrick.unicode);
-
-		//Find unicode emojis
-		UnicodeCandidate candidate = getNextUnicodeCandidate(input.replace("\uFE0F", "").toCharArray(), 0);
-		if (candidate != null) return candidate.getEmoji().getUnicode() + candidate.getFitzpatrickUnicode();
-
-		throw new NoSuchElementException("No emoji for input: " + input);
+		if (emoji == null) throw new NoSuchElementException("No emoji for input: " + input);
+		return emoji.unicode();
 	}
 
 	public static Emoji resolveJdaEmoji(String input) {
