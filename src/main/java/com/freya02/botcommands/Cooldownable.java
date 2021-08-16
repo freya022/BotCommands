4c380e2fbbebc02c240554a795426ca81f3f199e
freya02 <41875020+freya022@users.noreply.github.com>
2021-08-16 20:00:02 +0200

    Use guild local user cooldown

--- src/main/java/com/freya02/botcommands/Cooldownable.java
+++ src/main/java/com/freya02/botcommands/Cooldownable.java
@@ -1,10 +1,12 @@
 package com.freya02.botcommands;
 
 import com.freya02.botcommands.slash.SlashCommandListener;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.slf4j.Logger;
 
+import javax.annotation.Nonnull;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +17,7 @@ public abstract class Cooldownable {
 	private final int cooldown;
 
 	//The values is the time on which the cooldown expires
-	private final Map<Long, Long> userCooldowns = Collections.synchronizedMap(new HashMap<>());
+	private final Map<Long, Map<Long, Long>> userCooldowns = Collections.synchronizedMap(new HashMap<>());
 	private final Map<Long, Long> channelCooldowns = Collections.synchronizedMap(new HashMap<>());
 	private final Map<Long, Long> guildCooldowns = Collections.synchronizedMap(new HashMap<>());
 
@@ -35,7 +37,7 @@ public abstract class Cooldownable {
 	public void applyCooldown(GuildMessageReceivedEvent event) {
 		switch (cooldownScope) {
 			case USER:
-				userCooldowns.put(event.getAuthor().getIdLong(), System.currentTimeMillis() + cooldown);
+				getGuildUserCooldownMap(event.getGuild()).put(event.getAuthor().getIdLong(), System.currentTimeMillis() + cooldown);
 				break;
 			case GUILD:
 				guildCooldowns.put(event.getGuild().getIdLong(), System.currentTimeMillis() + cooldown);
@@ -46,10 +48,17 @@ public abstract class Cooldownable {
 		}
 	}
 
+	@Nonnull
+	private Map<Long, Long> getGuildUserCooldownMap(Guild guild) {
+		return userCooldowns.computeIfAbsent(guild.getIdLong(), x -> Collections.synchronizedMap(new HashMap<>()));
+	}
+
 	public void applyCooldown(SlashCommandEvent event) {
 		switch (cooldownScope) {
 			case USER:
-				userCooldowns.put(event.getUser().getIdLong(), System.currentTimeMillis() + cooldown);
+				if (event.getGuild() == null) break;
+				
+				getGuildUserCooldownMap(event.getGuild()).put(event.getUser().getIdLong(), System.currentTimeMillis() + cooldown);
 				break;
 			case GUILD:
 				if (event.getGuild() == null) break;
@@ -65,7 +74,7 @@ public abstract class Cooldownable {
 	public int getCooldown(GuildMessageReceivedEvent event) {
 		switch (cooldownScope) {
 			case USER:
-				return (int) Math.max(0, userCooldowns.getOrDefault(event.getAuthor().getIdLong(), 0L) - System.currentTimeMillis());
+				return (int) Math.max(0, getGuildUserCooldownMap(event.getGuild()).getOrDefault(event.getAuthor().getIdLong(), 0L) - System.currentTimeMillis());
 			case GUILD:
 				return (int) Math.max(0, guildCooldowns.getOrDefault(event.getGuild().getIdLong(), 0L) - System.currentTimeMillis());
 			case CHANNEL:
@@ -78,7 +87,13 @@ public abstract class Cooldownable {
 	public int getCooldown(SlashCommandEvent event) {
 		switch (cooldownScope) {
 			case USER:
-				return (int) Math.max(0, userCooldowns.getOrDefault(event.getUser().getIdLong(), 0L) - System.currentTimeMillis());
+				if (event.getGuild() == null) {
+					LOGGER.warn("Slash command '{}' wasn't used in a guild but uses a guild-wide cooldown", SlashCommandListener.reconstructCommand(event));
+
+					return 0;
+				}
+				
+				return (int) Math.max(0, getGuildUserCooldownMap(event.getGuild()).getOrDefault(event.getUser().getIdLong(), 0L) - System.currentTimeMillis());
 			case GUILD:
 				if (event.getGuild() == null) {
 					LOGGER.warn("Slash command '{}' wasn't used in a guild but uses a guild-wide cooldown", SlashCommandListener.reconstructCommand(event));
