fba622209c7a7ca70ef00e7facbd674d4139391c
freya02 <41875020+freya022@users.noreply.github.com>
2021-09-03 21:36:44 +0200

    Add CustomResolver docs

--- src/main/java/com/freya02/botcommands/parameters/CustomResolver.java
+++ src/main/java/com/freya02/botcommands/parameters/CustomResolver.java
@@ -1,11 +1,17 @@
 package com.freya02.botcommands.parameters;
 
+import com.freya02.botcommands.application.slash.annotations.Option;
 import net.dv8tion.jda.api.events.Event;
 
 import javax.annotation.Nullable;
 import java.util.function.Function;
 
-public class CustomResolver extends ParameterResolver {
+/**
+ * Represents a custom resolver for interaction parameters
+ * <br>This does not need to be implemented unlike other resolvers such as {@link SlashParameterResolver}
+ * <br><b>This resolver only gets used if an interaction parameter is not annotated with {@link Option}</b>
+ */
+public final class CustomResolver extends ParameterResolver {
 	private final Function<Event, ?> function;
 
 	public <T> CustomResolver(Class<T> clazz, Function<Event, T> function) {
