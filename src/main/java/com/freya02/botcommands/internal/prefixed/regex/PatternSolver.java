527a6bc01b8fe4d95196f9f754164c4cdaca451f
freya022 <41875020+freya022@users.noreply.github.com>
2021-10-05 15:51:37 +0200

    Remove old regex traces

--- src/main/java/com/freya02/botcommands/internal/prefixed/regex/PatternSolver.java
+++
@@ -1,7 +0,0 @@
-package com.freya02.botcommands.internal.prefixed.regex;
-
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-
-public interface PatternSolver {
-	Object solve(GuildMessageReceivedEvent event, String[] groups);
-}
