cf3a38e04af148de84d0f58164a1ad2b2938a9ce
freya022 <41875020+freya022@users.noreply.github.com>
2021-10-19 16:46:16 +0200

    Examples - wiki - Add constructor injection snippets

---
+++ examples/src/main/java/com/freya02/bot/wiki/ctorinj/CtorInjMain.java
@@ -0,0 +1,46 @@
+package com.freya02.bot.wiki.ctorinj;
+
+import com.freya02.bot.Config;
+import com.freya02.bot.wiki.ctorinj.commands.SlashCtorInjectionTest;
+import com.freya02.botcommands.api.CommandsBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.sql.Connection;
+
+public class CtorInjMain {
+	public static void main(String[] args) {
+		try {
+			final Config config = Config.readConfig();
+
+			final JDA jda = JDABuilder.createLight(config.getToken()).build().awaitReady();
+
+			alt1(jda);
+//			alt2(jda);
+		} catch (IOException | LoginException | InterruptedException e) {
+			e.printStackTrace();
+
+			System.exit(-1);
+		}
+	}
+
+	private static void alt1(JDA jda) throws IOException {
+		Connection connection = null; //Just a test value
+		CommandsBuilder.newBuilder(0L)
+				.extensionsBuilder(extensionsBuilder ->
+						extensionsBuilder.registerConstructorParameter(Connection.class, ignored -> connection)
+				)
+				.build(jda, "com.freya02.bot.wiki.ctorinj.commands");
+	}
+
+	private static void alt2(JDA jda) throws IOException {
+		Connection connection = null; //Just a test value
+		CommandsBuilder.newBuilder(0L)
+				.extensionsBuilder(extensionsBuilder ->
+						extensionsBuilder.registerInstanceSupplier(SlashCtorInjectionTest.class, context -> new SlashCtorInjectionTest(context, connection))
+				)
+				.build(jda, "com.freya02.bot.wiki.ctorinj.commands");
+	}
+}
---
+++ examples/src/main/java/com/freya02/bot/wiki/ctorinj/commands/SlashCtorInjectionTest.java
@@ -0,0 +1,23 @@
+package com.freya02.bot.wiki.ctorinj.commands;
+
+import com.freya02.botcommands.api.BContext;
+import com.freya02.botcommands.api.application.ApplicationCommand;
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+
+import java.sql.Connection;
+
+public class SlashCtorInjectionTest extends ApplicationCommand {
+	private final BContext context;
+	private final Connection connection;
+
+	public SlashCtorInjectionTest(BContext context, Connection connection) {
+		this.context = context;
+		this.connection = connection;
+	}
+
+	@JDASlashCommand(name = "ctorinj")
+	public void run(GuildSlashEvent event) {
+		event.replyFormat("My fields are %s and %s", context, connection).queue();
+	}
+}
