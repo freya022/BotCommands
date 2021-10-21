ebcf51a92712ec83d51e3c59f4ca613310c12a15
freya022 <41875020+freya022@users.noreply.github.com>
2021-10-21 09:35:10 +0200

    Examples - wiki - Add instance supplier snippets

---
+++ examples/src/main/java/com/freya02/bot/wiki/instancesupplier/commands/SlashInstanceSupplierTest.java
@@ -0,0 +1,23 @@
+package com.freya02.bot.wiki.instancesupplier.commands;
+
+import com.freya02.botcommands.api.application.ApplicationCommand;
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+import com.freya02.botcommands.internal.Logging;
+import org.slf4j.Logger;
+
+public class SlashInstanceSupplierTest extends ApplicationCommand {
+	private static final Logger LOGGER = Logging.getLogger();
+
+	public static class Dummy {}
+
+	//Making a non-instantiable constructor so only I can construct it
+	public SlashInstanceSupplierTest(Dummy dummy) {
+		LOGGER.debug("I got constructed with {}", dummy);
+	}
+
+	@JDASlashCommand(name = "instancesupplier")
+	public void run(GuildSlashEvent event) {
+		event.reply("I ran").queue();
+	}
+}
