45892a7201b8304bddc4bab830bbba7caf9e8135
freya02 <41875020+freya022@users.noreply.github.com>
2021-12-26 22:35:33 +0100

    Add warn for MethodRunner#invoke

--- src/main/java/com/freya02/botcommands/internal/runner/MethodRunner.java
+++ src/main/java/com/freya02/botcommands/internal/runner/MethodRunner.java
@@ -1,9 +1,14 @@
 package com.freya02.botcommands.internal.runner;
 
+import org.jetbrains.annotations.NonBlocking;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
 public interface MethodRunner {
+	/**
+	 * <h2>WARN: This method may not block</h2>
+	 */
+	@NonBlocking
 	void invoke(@NotNull Object[] args, Consumer<Throwable> throwableConsumer) throws Exception;
 }
