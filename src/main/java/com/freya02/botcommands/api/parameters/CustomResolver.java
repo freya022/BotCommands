4242597778a5e9a3e57cc25de53ac6745c99750e
freya022 <41875020+freya022@users.noreply.github.com>
2022-01-07 18:08:34 +0100

    Add docs

--- src/main/java/com/freya02/botcommands/api/parameters/CustomResolver.java
+++ src/main/java/com/freya02/botcommands/api/parameters/CustomResolver.java
@@ -11,16 +11,24 @@ import org.jetbrains.annotations.Nullable;
  * Represents a custom resolver for interaction parameters
  * <br>This does not need to be implemented unlike other resolvers such as {@link SlashParameterResolver}
  * <br><b>This resolver only gets used if an interaction parameter is not annotated with {@link AppOption} or {@link TextOption}</b>
+ *
+ * @see CustomResolverFunction
  */
 public final class CustomResolver extends ParameterResolver {
 	private final CustomResolverFunction<?> function;
 
+	/**
+	 * @see CustomResolverFunction
+	 */
 	public <T> CustomResolver(Class<T> clazz, CustomResolverFunction<T> function) {
 		super(clazz);
-		
+
 		this.function = function;
 	}
 
+	/**
+	 * @see CustomResolverFunction
+	 */
 	@Nullable
 	public Object resolve(BContext context, ExecutableInteractionInfo executableInteractionInfo, Event event) {
 		return function.apply(context, executableInteractionInfo, event);
--- src/main/java/com/freya02/botcommands/api/parameters/CustomResolverFunction.java
+++ src/main/java/com/freya02/botcommands/api/parameters/CustomResolverFunction.java
@@ -4,6 +4,19 @@ import com.freya02.botcommands.api.BContext;
 import com.freya02.botcommands.internal.ExecutableInteractionInfo;
 import net.dv8tion.jda.api.events.Event;
 
-public interface CustomResolverFunction<T> {
-	T apply(BContext context, ExecutableInteractionInfo executableInteractionInfo, Event event);
+/**
+ * @param <R> Type of the returned custom object
+ * @see #apply(BContext, ExecutableInteractionInfo, Event)
+ */
+public interface CustomResolverFunction<R> {
+	/**
+	 * Computes a custom object from this event
+	 *
+	 * @param context                   The {@link BContext} of this bot
+	 * @param executableInteractionInfo Some information about the executable which triggered this resolver
+	 *                                  <br>You try to use <code>instanceof</code> on this object to see what type of command info it could be
+	 * @param event                     The event that triggered the execution of this resolver
+	 * @return The custom object which got computed
+	 */
+	R apply(BContext context, ExecutableInteractionInfo executableInteractionInfo, Event event);
 }
