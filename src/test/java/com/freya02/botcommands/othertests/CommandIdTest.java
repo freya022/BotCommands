5fb9c4cb1432bc3a8e1776780ddea06c64096869
freya022 <41875020+freya022@users.noreply.github.com>
2022-03-03 08:50:18 +0100

    Check if a guild has more than one command that share the same path under different ids

--- src/test/java/com/freya02/botcommands/othertests/CommandIdTest.java
+++ src/test/java/com/freya02/botcommands/othertests/CommandIdTest.java
@@ -3,6 +3,8 @@ package com.freya02.botcommands.othertests;
 import com.freya02.botcommands.api.CommandStatus;
 import gnu.trove.list.TLongList;
 import gnu.trove.list.array.TLongArrayList;
+import gnu.trove.map.TLongObjectMap;
+import gnu.trove.map.hash.TLongObjectHashMap;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -71,7 +73,26 @@ public class CommandIdTest {
 		commandPathToCommandIdsMap.computeIfAbsent("specific", x -> new ArrayList<>()).add("global_run");
 
 		commandIdToGuildsMap.put("specific_run", TLongArrayList.wrap(new long[]{123L}));
-		commandIdToGuildsMap.put("specific_run2", TLongArrayList.wrap(new long[]{123L}));
+		commandIdToGuildsMap.put("specific_run2", TLongArrayList.wrap(new long[]{12345L}));
+
+		for (Map.Entry<String, List<String>> entry : commandPathToCommandIdsMap.entrySet()) {
+			final TLongObjectMap<String> usedGuildIdToCommandIdMap = new TLongObjectHashMap<>();
+
+			for (String commandId : entry.getValue()) {
+				final TLongList list = commandIdToGuildsMap.get(commandId);
+				if (list == null) continue;
+
+				list.forEach(guildId -> {
+					final String oldId = usedGuildIdToCommandIdMap.put(guildId, commandId);
+
+					if (oldId != null) {
+						throw new IllegalArgumentException("Guild ID %d has two commands IDs ('%s' and '%s') that share the same path '%s'".formatted(guildId, oldId, commandId, entry.getKey()));
+					}
+
+					return true;
+				});
+			}
+		}
 
 		//If a command id has been marked as allowed in a guild then the commands with the same names must be disabled
 		//Let's check for "specific_run" in guild 123, it should be allowed
@@ -82,13 +103,14 @@ public class CommandIdTest {
 		System.out.println(getStatus("specific", "global_run", 1234));          //Can we run the global     command in the other    command guild ? yes
 		System.out.println(getStatus("specific", "specific_run", 1234));        //Can we run the specific   command in the other    command guild ? no
 		System.out.println(getStatus("specific", "specific_run2", 1234));       //Can we run the specific 2 command in the other    command guild ? no
-		System.out.println(getStatus("specific", "specific_run2", 123));        //Can we run the specific 2 command in the specific command guild ? yes
-		//TODO this one should actually be NO, this shares the same command path as specific_run as well as the same guild ID
+		System.out.println(getStatus("specific", "specific_run2", 123));        //Can we run the specific 2 command in the specific command guild ? no
 
-		for (long guildId : List.of(123, 1234)) {
+		for (long guildId : List.of(123, 1234, 12345)) {
 			for (String cmdId : List.of("specific_run", "global_run", "sth_run", "specific_run2")) {
 				System.out.println("getStatus(\"specific\", " + cmdId + ", " + guildId + ") = " + getStatus("specific", cmdId, guildId));
 			}
+
+			System.out.println();
 		}
 	}
 }
