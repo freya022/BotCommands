67bdff236b0bbf30252788f9e393c06fed7b79ec
freya022 <41875020+freya022@users.noreply.github.com>
2022-03-16 15:19:37 +0100

    Add docs

--- src/main/java/com/freya02/botcommands/api/application/slash/DefaultValueSupplier.java
+++ src/main/java/com/freya02/botcommands/api/application/slash/DefaultValueSupplier.java
@@ -1,8 +1,11 @@
 package com.freya02.botcommands.api.application.slash;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 @FunctionalInterface
 public interface DefaultValueSupplier {
-	Object getDefaultValue(SlashCommandInteractionEvent event);
+	@Nullable
+	Object getDefaultValue(@NotNull SlashCommandInteractionEvent event);
 }
