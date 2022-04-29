3348475e23011fd7efd34ebc47daa51deca4c8c8
freya02 <41875020+freya022@users.noreply.github.com>
2022-04-29 18:37:48 +0200

    Don't put commands with command ids in the global scope

--- src/main/java/com/freya02/botcommands/internal/application/ApplicationCommandInfo.java
+++ src/main/java/com/freya02/botcommands/internal/application/ApplicationCommandInfo.java
@@ -42,8 +42,8 @@ public abstract class ApplicationCommandInfo extends AbstractCommandInfo<Applica
 		if ((userPermissions.size() != 0 || botPermissions.size() != 0) && !guildOnly)
 			throw new IllegalArgumentException(Utils.formatMethodShort(commandMethod) + " : application command with permissions should be guild-only");
 
-		if (getCommandId() != null && !isGuildOnly()) {
-			throw new IllegalArgumentException(Utils.formatMethodShort(commandMethod) + " : application command with guild-specific ID should be guild-only");
+		if (getCommandId() != null && scope != CommandScope.GUILD) {
+			throw new IllegalArgumentException(Utils.formatMethodShort(commandMethod) + " : application command with guild-specific ID must have the GUILD scope");
 		}
 	}
 
