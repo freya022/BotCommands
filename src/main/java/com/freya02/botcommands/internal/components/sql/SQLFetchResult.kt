32ea1d28fbb5de6e7b4376e0379c9033ee9a46f2
freya02 <41875020+freya022@users.noreply.github.com>
2022-08-02 18:27:55 +0200

    Cleanup

--- src/main/java/com/freya02/botcommands/internal/components/sql/SQLFetchResult.kt
+++ src/main/java/com/freya02/botcommands/internal/components/sql/SQLFetchResult.kt
@@ -3,7 +3,6 @@ package com.freya02.botcommands.internal.components.sql
 import com.freya02.botcommands.api.components.FetchResult
 import com.freya02.botcommands.core.internal.db.Transaction
 import com.freya02.botcommands.internal.throwInternal
-import java.sql.Connection
 
 internal class SQLFetchResult(val fetchedComponent: SQLFetchedComponent?, transaction: Transaction?) : FetchResult(fetchedComponent) {
     private var closed = false
@@ -14,13 +13,6 @@ internal class SQLFetchResult(val fetchedComponent: SQLFetchedComponent?, transa
             check(!closed) { "Cannot get Transaction from SQLFetchedComponent as it has been closed" }
             return _transaction ?: throwInternal("Transaction was used when the fetch result was erroneous")
         }
-
-    @Deprecated("Deprecated in favor of transaction")
-    fun getConnection(): Connection {
-        check(!closed) { "Cannot get Connection from SQLFetchedComponent as it has been closed" }
-
-        return transaction.connection
-    }
 
     @Throws(Exception::class)
     override fun close() {
