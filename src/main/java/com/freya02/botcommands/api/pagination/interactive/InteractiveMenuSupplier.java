bebfcb29b89adf028f09931a86284228a1a0c8e3
freya02 <41875020+freya022@users.noreply.github.com>
2022-08-06 21:36:40 +0200

    pageNumber docs

--- src/main/java/com/freya02/botcommands/api/pagination/interactive/InteractiveMenuSupplier.java
+++ src/main/java/com/freya02/botcommands/api/pagination/interactive/InteractiveMenuSupplier.java
@@ -18,7 +18,7 @@ public interface InteractiveMenuSupplier<T extends BasicInteractiveMenu<T>> {
 	 *                          <li>Modify the interactive menu's state when a button is triggered</li>
 	 *                          <li>Delete the menu, cancel the timeout and cleanup the components when a button is clicked</li>
 	 *                        </ul>
-	 * @param pageNumber
+	 * @param pageNumber      The page number of the currently displayed menu
 	 * @param messageBuilder  The {@link MessageBuilder} for this interactive menu, you can mostly ignore it but can use it to add attachments for examples, to use them in your embeds
 	 * @param components      The {@link PaginatorComponents} for this interactive menu's page, this allows you to add components on this page
 	 *                        <br><b>Do not use {@link MessageBuilder#setActionRows(ActionRow...)} and such, these will be overridden by the menu</b>
