ef4530ebc9f18adda887ef7db17a28dcf1930fbf
freya02 <41875020+freya022@users.noreply.github.com>
2022-08-08 17:13:19 +0200

    Convert BCLocalizationFunction

--- src/main/java/com/freya02/botcommands/internal/application/localization/BCLocalizationFunction.kt
+++ src/main/java/com/freya02/botcommands/internal/application/localization/BCLocalizationFunction.kt
@@ -1,57 +1,57 @@
-package com.freya02.botcommands.internal.application.localization;
+package com.freya02.botcommands.internal.application.localization
 
-import com.freya02.botcommands.api.Logging;
-import com.freya02.botcommands.api.builder.DebugBuilder;
-import com.freya02.botcommands.api.localization.Localization;
-import com.freya02.botcommands.api.localization.LocalizationTemplate;
-import com.freya02.botcommands.internal.BContextImpl;
-import net.dv8tion.jda.api.interactions.DiscordLocale;
-import net.dv8tion.jda.api.interactions.commands.localization.LocalizationFunction;
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
+import com.freya02.botcommands.api.Logging
+import com.freya02.botcommands.api.builder.DebugBuilder
+import com.freya02.botcommands.api.localization.Localization
+import com.freya02.botcommands.internal.BContextImpl
+import net.dv8tion.jda.api.interactions.DiscordLocale
+import net.dv8tion.jda.api.interactions.commands.localization.LocalizationFunction
+import java.util.*
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+class BCLocalizationFunction(context: BContextImpl) : LocalizationFunction {
+    private val baseNameToLocalesMap: Map<String, List<Locale>> = context.applicationCommandsContext.baseNameToLocalesMap
 
-public class BCLocalizationFunction implements LocalizationFunction {
-	private static final Logger LOGGER = Logging.getLogger();
-	private final Map<String, List<Locale>> baseNameToLocalesMap;
+    override fun apply(localizationKey: String): Map<DiscordLocale, String> {
+        val map: MutableMap<DiscordLocale, String> = EnumMap(DiscordLocale::class.java)
 
-	public BCLocalizationFunction(BContextImpl context) {
-		baseNameToLocalesMap = context.getApplicationCommandsContext().getBaseNameToLocalesMap();
-	}
+        baseNameToLocalesMap.forEach { (baseName, locales) ->
+            for (locale in locales) {
+                val instance = Localization.getInstance(baseName, locale)
+                if (instance != null) {
+                    if (instance.effectiveLocale !== locale) {
+                        if (Logging.tryLog(baseName, locale.toLanguageTag(), instance.effectiveLocale.toLanguageTag())) {
+                            logger.warn(
+                                "Localization bundle '{}' with locale '{}' was specified to be valid but was not found, falling back to '{}'",
+                                baseName,
+                                locale,
+                                instance.effectiveLocale
+                            )
+                        }
+                    }
 
-	@NotNull
-	@Override
-	public Map<DiscordLocale, String> apply(@NotNull String localizationKey) {
-		final Map<DiscordLocale, String> map = new HashMap<>();
+                    val template = instance[localizationKey]
+                    if (template != null) {
+                        map[DiscordLocale.from(locale)] = template.localize()
+                    } else if (DebugBuilder.isLogMissingLocalizationEnabled()) {
+                        if (Logging.tryLog(baseName, locale.toLanguageTag(), localizationKey)) {
+                            logger.warn(
+                                "Localization template '{}' could not be found in bundle '{}' with locale '{}' or below",
+                                localizationKey,
+                                baseName,
+                                locale
+                            )
+                        }
+                    }
+                } else if (Logging.tryLog(baseName, locale.toLanguageTag())) {
+                    logger.warn("Localization bundle '{}' with locale '{}' was specified to be valid but was not found.", baseName, locale)
+                }
+            }
+        }
 
-		baseNameToLocalesMap.forEach((baseName, locales) -> {
-			for (Locale locale : locales) {
-				final Localization instance = Localization.getInstance(baseName, locale);
-				if (instance != null) {
-					if (instance.getEffectiveLocale() != locale) {
-						if (Logging.tryLog(baseName, locale.toLanguageTag(), instance.getEffectiveLocale().toLanguageTag())) {
-							LOGGER.warn("Localization bundle '{}' with locale '{}' was specified to be valid but was not found, falling back to '{}'", baseName, locale, instance.getEffectiveLocale());
-						}
-					}
+        return map
+    }
 
-					final LocalizationTemplate template = instance.get(localizationKey);
-					if (template != null) {
-						map.put(DiscordLocale.from(locale), template.localize());
-					} else if (DebugBuilder.isLogMissingLocalizationEnabled()) {
-						if (Logging.tryLog(baseName, locale.toLanguageTag(), localizationKey)) {
-							LOGGER.warn("Localization template '{}' could not be found in bundle '{}' with locale '{}' or below", localizationKey, baseName, locale);
-						}
-					}
-				} else if (Logging.tryLog(baseName, locale.toLanguageTag())) {
-					LOGGER.warn("Localization bundle '{}' with locale '{}' was specified to be valid but was not found.", baseName, locale);
-				}
-			}
-		});
-
-		return map;
-	}
+    companion object {
+        private val logger = Logging.getLogger()
+    }
 }
