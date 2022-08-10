7e5461110d4ac991e465efa13dd012824b74faaf
freya02 <41875020+freya022@users.noreply.github.com>
2022-08-10 17:13:00 +0200

    Remove old event handler, modal handlers builder, components builder

--- src/main/java/com/freya02/botcommands/internal/events/EventConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.freya02.botcommands.internal.events;
-
-import net.dv8tion.jda.api.events.Event;
-
-@Deprecated
-@FunctionalInterface
-public interface EventConsumer {
-	void accept(Event event) throws Exception;
-}
