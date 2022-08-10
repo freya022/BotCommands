c961ca6021731a1178b6664e1839efc9f1d5e047
freya02 <41875020+freya022@users.noreply.github.com>
2022-08-10 22:57:50 +0200

    Separate internal class scanning from user classes

--- src/main/kotlin/com/freya02/botcommands/internal/utils/ReflectionMetadata.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/utils/ReflectionMetadata.kt
@@ -5,10 +5,7 @@ import com.freya02.botcommands.internal.throwInternal
 import com.freya02.botcommands.internal.throwUser
 import com.freya02.botcommands.internal.utils.ReflectionUtilsKt.isService
 import com.freya02.botcommands.internal.utils.ReflectionUtilsKt.nonInstanceParameters
-import io.github.classgraph.ArrayTypeSignature
-import io.github.classgraph.ClassGraph
-import io.github.classgraph.ClassInfoList
-import io.github.classgraph.TypeVariableSignature
+import io.github.classgraph.*
 import java.util.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
@@ -48,21 +45,36 @@ internal object ReflectionMetadata {
         Collections.unmodifiableMap(functionMetadataMap_)
     }
 
-    internal fun runScan(packages: Collection<String>, userClasses: Collection<Class<*>>): List<Class<*>> = ClassGraph()
-        .acceptPackages(*packages.toTypedArray(), "com.freya02.botcommands")
-        .acceptClasses(*userClasses.map { it.simpleName }.toTypedArray())
-        .enableMethodInfo()
-        .enableAnnotationInfo()
-        .scan()
-        .let { scanResult ->
-            val list = scanResult.allStandardClasses
-                .filter {
-                    if (it.packageName.startsWith("com.freya02.botcommands") && !it.packageName.startsWith("com.freya02.botcommands.test")) {
-                        return@filter it.isService() || it.outerClasses.any { outer -> outer.isService() }
-                    }
-
-                    return@filter true
+    internal fun runScan(packages: Collection<String>, userClasses: Collection<Class<*>>): List<Class<*>> {
+        val scanned: List<Pair<ScanResult, ClassInfoList>> = buildList {
+            ClassGraph()
+                .acceptPackages("com.freya02.botcommands")
+                .enableMethodInfo()
+                .enableAnnotationInfo()
+                .scan()
+                .also { scanResult -> // Don't keep test classes
+                    add(scanResult to scanResult.allStandardClasses.filter {
+                        if (it.packageName.startsWith("com.freya02.botcommands.test")) {
+                            return@filter false
+                        } else {
+                            return@filter it.isService() || it.outerClasses.any { outer -> outer.isService() }
+                        }
+                    })
                 }
+
+            ClassGraph()
+                .acceptPackages(*packages.toTypedArray())
+                .acceptClasses(*userClasses.map { it.simpleName }.toTypedArray())
+                .enableMethodInfo()
+                .enableAnnotationInfo()
+                .scan()
+                .also { scanResult ->
+                    add(scanResult to scanResult.allStandardClasses)
+                }
+        }
+
+        return scanned.flatMap { (scanResult, classes) ->
+            classes
                 .filter {
                     it.annotationInfo.directOnly()["kotlin.Metadata"]?.let { annotationInfo ->
                         return@filter KotlinClassHeader.Kind.getById(annotationInfo.parameterValues["k"].value as Int) == KotlinClassHeader.Kind.CLASS
@@ -73,11 +85,11 @@ internal object ReflectionMetadata {
                 .filter(ReflectionUtilsKt::isInstantiable)
                 .also { readAnnotations(it) }
                 .loadClasses()
-
-            scanResult.close()
-
-            list
+                .also {
+                    scanResult.close()
+                }
         }
+    }
 
 
     private fun readAnnotations(classInfoList: ClassInfoList) {
