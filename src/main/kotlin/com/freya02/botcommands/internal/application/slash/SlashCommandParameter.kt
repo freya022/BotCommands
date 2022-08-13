0d0f187fe3b262bb8491bc519d5f609e8d33c0b2
freya02 <41875020+freya022@users.noreply.github.com>
2022-08-13 15:37:01 +0200

    Don't search for `@ChannelType` on manually declared commands

--- src/main/kotlin/com/freya02/botcommands/internal/application/slash/SlashCommandParameter.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/application/slash/SlashCommandParameter.kt
@@ -1,6 +1,5 @@
 package com.freya02.botcommands.internal.application.slash
 
-import com.freya02.botcommands.annotations.api.application.slash.annotations.ChannelTypes
 import com.freya02.botcommands.api.application.ValueRange
 import com.freya02.botcommands.api.application.builder.OptionBuilder
 import com.freya02.botcommands.api.application.builder.SlashCommandOptionBuilder
@@ -13,7 +12,6 @@ import net.dv8tion.jda.api.entities.ChannelType
 import net.dv8tion.jda.api.interactions.commands.Command
 import java.util.*
 import kotlin.reflect.KParameter
-import kotlin.reflect.full.findAnnotation
 
 class SlashCommandParameter(
     slashCommandInfo: SlashCommandInfo,
@@ -53,16 +51,7 @@ class SlashCommandParameter(
             else -> kParameter.isNullable
         }
 
-        val channelTypesAnnotation = kParameter.findAnnotation<ChannelTypes>()
-        if (optionBuilder.channelTypes != null && channelTypesAnnotation != null) {
-            throwUser("Parameter $kParameter has channel types specified in both the annotation and the DSL builder, consider only use one of them")
-        }
-
-        this.channelTypes = when {
-            channelTypesAnnotation != null -> enumSetOf(*channelTypesAnnotation.value)
-            optionBuilder.channelTypes != null -> optionBuilder.channelTypes
-            else -> enumSetOf()
-        }
+        this.channelTypes = optionBuilder.channelTypes ?: enumSetOf()
     }
 
     fun hasAutocomplete() = autocompleteHandler != null
