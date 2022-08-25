574375b9206db1679ac51e0b85db3be7b15ef0a8
freya02 <41875020+freya022@users.noreply.github.com>
2022-08-25 13:01:54 +0200

    Inline AutocompleteFunctionContainer

--- src/main/kotlin/com/freya02/botcommands/internal/commands/application/autocomplete/AutocompleteInfoContainer.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/commands/application/autocomplete/AutocompleteInfoContainer.kt
@@ -9,15 +9,16 @@ import com.freya02.botcommands.internal.core.ClassPathContainer
 import com.freya02.botcommands.internal.core.requireFirstArg
 import com.freya02.botcommands.internal.core.requireNonStatic
 import com.freya02.botcommands.internal.core.requireReturnType
-import com.freya02.botcommands.internal.throwInternal
+import com.freya02.botcommands.internal.throwUser
+import com.freya02.botcommands.internal.utils.ReflectionUtils
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
+import kotlin.reflect.KFunction
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.jvm.javaType
+import kotlin.reflect.jvm.jvmErasure
 
 @BService
-internal class AutocompleteInfoContainer(
-    autocompleteFunctionContainer: AutocompleteFunctionContainer,
-    classPathContainer: ClassPathContainer
-) {
+internal class AutocompleteInfoContainer(classPathContainer: ClassPathContainer) {
     private val infoMap: Map<String, AutocompleteInfo>
 
     init {
@@ -26,13 +27,18 @@ internal class AutocompleteInfoContainer(
             .requireFirstArg(CommandAutoCompleteInteractionEvent::class)
             .requireReturnType(Collection::class)
             .associate {
-                val autocompleteFunction = it.function
+                val returnType = it.function.returnType
+                ReflectionUtils.getCollectionReturnType(returnType.jvmErasure.java, returnType.javaType) ?: throwUser(
+                    it.function,
+                    "Autocomplete handler needs to return a Collection"
+                )
+
+                @Suppress("UNCHECKED_CAST")
+                val autocompleteFunction = it.function as KFunction<Collection<*>>
                 val autocompleteHandlerAnnotation = autocompleteFunction.findAnnotation<AutocompleteHandler>()!!
 
                 val info = AutocompleteInfoBuilder(autocompleteHandlerAnnotation.name).apply {
-                    function = autocompleteFunctionContainer[autocompleteHandlerAnnotation.name] ?: throwInternal(
-                        "Autocomplete handler '${autocompleteHandlerAnnotation.name}' was not found"
-                    )
+                    function = autocompleteFunction
 
                     mode = autocompleteHandlerAnnotation.mode
                     showUserInput = autocompleteHandlerAnnotation.showUserInput
