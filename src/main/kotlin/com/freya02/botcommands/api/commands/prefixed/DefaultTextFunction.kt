e1bab8b4d58aecc6fa25a46f758d334da4b605d4
freya02 <41875020+freya022@users.noreply.github.com>
2022-09-10 12:35:35 +0200

    Delete DefaultTextFunction

--- src/main/kotlin/com/freya02/botcommands/api/commands/prefixed/DefaultTextFunction.kt
+++
@@ -1,9 +0,0 @@
-package com.freya02.botcommands.api.commands.prefixed
-
-import com.freya02.botcommands.api.core.annotations.LateService
-import com.freya02.botcommands.internal.throwInternal
-
-@LateService
-class DefaultTextFunction {
-    fun guild(event: BaseCommandEvent): Nothing = throwInternal("Default function was used")
-}
