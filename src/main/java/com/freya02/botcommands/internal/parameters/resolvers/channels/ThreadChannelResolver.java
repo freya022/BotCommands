78158cdaaa2079193f76827b4f92bd50c5925bce
freya02 <41875020+freya022@users.noreply.github.com>
2022-09-11 21:11:40 +0200

    Update ThreadChannelResolver

--- src/main/java/com/freya02/botcommands/internal/parameters/resolvers/channels/ThreadChannelResolver.java
+++ src/main/java/com/freya02/botcommands/internal/parameters/resolvers/channels/ThreadChannelResolver.java
@@ -7,6 +7,7 @@ import com.freya02.botcommands.internal.annotations.IncludeClasspath;
 import com.freya02.botcommands.internal.commands.application.slash.SlashCommandInfo;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.ThreadChannel;
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
 import net.dv8tion.jda.api.interactions.commands.CommandInteractionPayload;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -16,7 +17,10 @@ import org.jetbrains.annotations.Nullable;
 import java.util.EnumSet;
 
 @IncludeClasspath
-public class ThreadChannelResolver extends ParameterResolver implements SlashParameterResolver, ChannelResolver {
+public class ThreadChannelResolver
+		extends ParameterResolver<ThreadChannelResolver, ThreadChannel>
+		implements SlashParameterResolver<ThreadChannelResolver, ThreadChannel>,
+		           ChannelResolver {
 	private static final EnumSet<ChannelType> THREAD_TYPES = EnumSet.of(ChannelType.GUILD_NEWS_THREAD, ChannelType.GUILD_PUBLIC_THREAD, ChannelType.GUILD_PRIVATE_THREAD);
 
 	public ThreadChannelResolver() {
@@ -31,8 +35,13 @@ public class ThreadChannelResolver extends ParameterResolver implements SlashPar
 
 	@Override
 	@Nullable
-	public Object resolve(@NotNull BContext context, @NotNull SlashCommandInfo info, @NotNull CommandInteractionPayload event, @NotNull OptionMapping optionMapping) {
-		return optionMapping.getAsChannel();
+	public ThreadChannel resolve(@NotNull BContext context, @NotNull SlashCommandInfo info, @NotNull CommandInteractionPayload event, @NotNull OptionMapping optionMapping) {
+		final GuildChannelUnion channel = optionMapping.getAsChannel();
+		if (channel.getType().isThread()) {
+			return channel.asThreadChannel();
+		}
+
+		return null;
 	}
 
 	@Override
