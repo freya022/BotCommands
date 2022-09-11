3377b0b0b5c6dd96d3f084771766f161fb6e249a
freya02 <41875020+freya022@users.noreply.github.com>
2022-09-11 21:21:39 +0200

    Update docs

--- src/main/kotlin/com/freya02/botcommands/api/parameters/RegexParameterResolver.kt
+++ src/main/kotlin/com/freya02/botcommands/api/parameters/RegexParameterResolver.kt
@@ -13,10 +13,11 @@ interface RegexParameterResolver<T : ParameterResolver<T, R>, R> {
     /**
      * Returns a resolved object from this text command interaction
      *
-     * @param context The [BContext] of this bot
-     * @param info    The text command info of the command being executed
-     * @param event   The event of this received message
-     * @param args    The text arguments of this command, extracted with [pattern]
+     * @param context   The [BContext] of this bot
+     * @param variation The text command variation of the command being executed
+     * @param event     The event of this received message
+     * @param args      The text arguments of this command, extracted with [pattern]
+     *
      * @return The resolved option mapping
      */
     fun resolve(
