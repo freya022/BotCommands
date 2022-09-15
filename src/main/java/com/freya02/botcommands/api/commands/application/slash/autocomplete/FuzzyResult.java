440cba3d0c6ee77b61f9edfe063e84598c2b6988
freya02 <41875020+freya022@users.noreply.github.com>
2022-09-15 18:20:13 +0200

    Fix FuzzyResult equality

--- src/main/java/com/freya02/botcommands/api/commands/application/slash/autocomplete/FuzzyResult.java
+++ src/main/java/com/freya02/botcommands/api/commands/application/slash/autocomplete/FuzzyResult.java
@@ -9,6 +9,25 @@ public record FuzzyResult<T>(T item, String string, double distance) implements 
 
 	@Override
 	public int compareTo(@NotNull FuzzyResult<T> o) {
+		if (distance == o.distance) { //This is needed as TreeSet considers entries as duplicated if compare result is 0
+			return string.compareTo(o.string);
+		}
+
 		return Double.compare(distance, o.distance);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		FuzzyResult<?> that = (FuzzyResult<?>) o;
+
+		return item.equals(that.item);
+	}
+
+	@Override
+	public int hashCode() {
+		return item.hashCode();
 	}
 }
