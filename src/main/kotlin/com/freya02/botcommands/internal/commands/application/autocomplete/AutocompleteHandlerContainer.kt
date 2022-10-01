65877b2bbb7ab248e650cdff7a9085886779e925
freya02 <41875020+freya022@users.noreply.github.com>
2022-10-01 14:35:39 +0200

    Use locks in AutocompleteHandlerContainer

--- src/main/kotlin/com/freya02/botcommands/internal/commands/application/autocomplete/AutocompleteHandlerContainer.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/commands/application/autocomplete/AutocompleteHandlerContainer.kt
@@ -4,14 +4,17 @@ import com.freya02.botcommands.api.core.annotations.BService
 import com.freya02.botcommands.internal.commands.application.slash.autocomplete.AutocompleteHandler
 import com.freya02.botcommands.internal.throwUser
 import com.freya02.botcommands.internal.utils.ReflectionUtilsKt.shortSignatureNoSrc
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 @BService
 internal class AutocompleteHandlerContainer {
+    private val handlersLock = ReentrantLock()
     private val autocompleteHandlers: MutableMap<String, AutocompleteHandler> = hashMapOf()
 
-    operator fun get(handlerName: String): AutocompleteHandler? = autocompleteHandlers[handlerName]
+    operator fun get(handlerName: String): AutocompleteHandler? = handlersLock.withLock { autocompleteHandlers[handlerName] }
 
-    operator fun plusAssign(handler: AutocompleteHandler) {
+    operator fun plusAssign(handler: AutocompleteHandler) = handlersLock.withLock {
         autocompleteHandlers[handler.autocompleteInfo.name]?.let {
             if (it.autocompleteInfo == handler.autocompleteInfo) {
                 return //Skip assignation & exception if both autocompletes are the exact same
