9cfdb372fbf197bb902e2adbe2d5d27562fe565a
freya02 <41875020+freya022@users.noreply.github.com>
2022-10-03 19:23:17 +0200

    Add lazily computed context strings I have no idea what im doing at this point

--- src/main/kotlin/com/freya02/botcommands/internal/core/ExceptionContext.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/core/ExceptionContext.kt
@@ -5,6 +5,7 @@ import com.freya02.botcommands.internal.BContextImpl
 import com.freya02.botcommands.internal.getDeepestCause
 import mu.KotlinLogging
 import java.util.*
+import java.util.function.Supplier
 
 internal class ExceptionContextInfo {
     var logMessage: () -> String = { "An unhandled exception has been caught" }
@@ -16,10 +17,12 @@ internal class ExceptionContext private constructor(
     private val context: BContextImpl,
     private var contextBlock: ExceptionContextInfo.() -> Unit
 ) {
-    private val descStack: Stack<String> = Stack()
+    private val descStack: Stack<Supplier<String>> = Stack()
 
-    inline fun <R> exceptionContext(desc: String, block: ExceptionContext.() -> R): R {
-        descStack += desc
+    inline fun <R> exceptionContext(desc: String, block: ExceptionContext.() -> R) = exceptionContext({ desc }, block)
+
+    inline fun <R> exceptionContext(descSupplier: Supplier<String>, block: ExceptionContext.() -> R): R {
+        descStack += descSupplier
         return let(block).also { descStack.pop() }
     }
 
@@ -32,14 +35,14 @@ internal class ExceptionContext private constructor(
         return block().also { this.contextBlock = oldBlock }
     }
 
-    private suspend inline fun <R> runContext(desc: String, block: ExceptionContext.() -> R): Result<R> {
-        descStack += desc
+    private suspend inline fun <R> runContext(descSupplier: Supplier<String>, block: ExceptionContext.() -> R): Result<R> {
+        descStack += descSupplier
         return runCatching(block).onFailure { e ->
             val exceptionContextInfo = ExceptionContextInfo().apply(contextBlock)
 
             val baseEx = e.getDeepestCause()
 
-            val descStr = descStack.joinToString("\n\t          ")
+            val descStr = StringJoiner("\n\t          ").apply { descStack.forEach { add(it.get()) } }.toString()
             val contextStr = "\tContext:  $descStr"
 
             //Compiler error when using the function type method
@@ -51,10 +54,16 @@ internal class ExceptionContext private constructor(
 
     internal class ExceptionContextBuilder(
         private val context: BContextImpl,
-        private val desc: String,
+        private val descSupplier: Supplier<String>,
         private val contextBlock: ExceptionContextInfo.() -> Unit
     ) {
+        constructor(context: BContextImpl, desc: String, contextBlock: ExceptionContextInfo.() -> Unit) : this(
+            context,
+            { desc },
+            contextBlock
+        )
+
         suspend inline fun <R> build(block: ExceptionContext.() -> R): Result<R> =
-            ExceptionContext(context, contextBlock).runContext(desc, block)
+            ExceptionContext(context, contextBlock).runContext(descSupplier, block)
     }
 }
