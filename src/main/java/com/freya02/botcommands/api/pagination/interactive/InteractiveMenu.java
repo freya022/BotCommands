c4d5fa8d7c9c8cb1832c4e6e7d455674e4fb2d8b
freya02 <41875020+freya022@users.noreply.github.com>
2022-10-28 16:48:18 +0200

    Change some docs

--- src/main/java/com/freya02/botcommands/api/pagination/interactive/InteractiveMenu.java
+++ src/main/java/com/freya02/botcommands/api/pagination/interactive/InteractiveMenu.java
@@ -3,13 +3,13 @@ package com.freya02.botcommands.api.pagination.interactive;
 import com.freya02.botcommands.api.components.InteractionConstraints;
 import com.freya02.botcommands.api.pagination.TimeoutInfo;
 import com.freya02.botcommands.api.utils.ButtonContent;
-import net.dv8tion.jda.api.interactions.components.selections.SelectMenu;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 /**
- * A type of pagination which shows embeds and provides a {@link SelectMenu} to navigate between menus
+ * A type of pagination which shows embeds and provides a {@link StringSelectMenu} to navigate between menus
  * <br>Each embed is bound to a selection menu
  * <br><i>This does not provide pagination for each embed</i> (no arrow buttons, only the selection menu)
  */
