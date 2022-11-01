c3c45ac837d80899a075cc2c65f1baed2d18267c
freya02 <41875020+freya022@users.noreply.github.com>
2022-11-01 23:10:41 +0100

    Improve test

--- src/test/kotlin/com/freya02/botcommands/othertests/ExceptionContextTest.kt
+++ src/test/kotlin/com/freya02/botcommands/othertests/ExceptionContextTest.kt
@@ -5,22 +5,23 @@ import com.freya02.botcommands.internal.BContextImpl
 import com.freya02.botcommands.internal.core.ExceptionContext
 import com.freya02.botcommands.internal.core.ExceptionContextInfo
 import dev.minn.jda.ktx.events.CoroutineEventManager
+import okio.IOException
 
 suspend fun main() {
     val context = BContextImpl(BConfig().apply { packages += "com.freya02.botcommands.oldtests" }, CoroutineEventManager())
     val contextBlock: ExceptionContextInfo.() -> Unit = {
-        logMessage = { "ok" }
-        dispatchMessage = { "ok" }
-        postRun = { println("oké") }
+        logMessage = { "Generic log message" }
+        dispatchMessage = { "Generic discord log message" }
+        postRun = { println("I ran after an exception") }
     }
 
-    ExceptionContext(context, contextBlock).runContext({ "Test" }) {
+    ExceptionContext(context, contextBlock).runContext({ "Receiving a SlashCommandInteractionEvent" }) {
         println("do sth")
 //        throw AssertionError("throw")
 
-        withNewExceptionContext(contextBlock).runContext({ "Something" }) {
-            withExceptionContext("Test 2") {
-                throw AssertionError("throw")
+        withNewExceptionContext(contextBlock).runContext({ "Retrieving caller" }) {
+            withExceptionContext("Awaiting network call") {
+                throw IOException()
             }
         }
 
