dfcbcdd39fbc5323c610a55ddcdc75807398aa3a
freya02 <41875020+freya022@users.noreply.github.com>
2022-11-06 18:58:57 +0100

    Integrate ServiceDependency in ConditionalService

--- src/main/kotlin/com/freya02/botcommands/api/core/annotations/ConditionalService.kt
+++ src/main/kotlin/com/freya02/botcommands/api/core/annotations/ConditionalService.kt
@@ -2,11 +2,12 @@ package com.freya02.botcommands.api.core.annotations
 
 import com.freya02.botcommands.api.core.ConditionalServiceChecker
 import java.lang.annotation.Inherited
+import kotlin.reflect.KClass
 
 /**
  * Indicates the class is a service that might be available under certain conditions.
  *
- * You will need to implement [ConditionalServiceChecker].
+ * You will need to implement [ConditionalServiceChecker], or have dependencies.
  *
  * @see BService
  * @see InjectedService
@@ -16,6 +17,18 @@ import java.lang.annotation.Inherited
 @Inherited
 @Target(AnnotationTarget.CLASS)
 annotation class ConditionalService(
+    /**
+     * Whether the service should be initialized lazily
+     */
     val lazy: Boolean = false,
+    /**
+     * Makes this service depend on others, this also makes you able to skip the [ConditionalServiceChecker] implementation
+     *
+     * This may be useful in situations where the service shares some checks with another service
+     */
+    val dependencies: Array<KClass<*>> = [],
+    /**
+     * The message displayed if this class cannot be instantiated
+     */
     val message: String = "Conditional object"
 )
