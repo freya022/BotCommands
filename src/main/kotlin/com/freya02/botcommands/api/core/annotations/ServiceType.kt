52f5a04500195ff6967c37a69d916683fc4e5502
freya02 <41875020+freya022@users.noreply.github.com>
2022-11-10 23:11:43 +0100

    Clarify ServiceType

--- src/main/kotlin/com/freya02/botcommands/api/core/annotations/ServiceType.kt
+++ src/main/kotlin/com/freya02/botcommands/api/core/annotations/ServiceType.kt
@@ -5,6 +5,9 @@ import kotlin.reflect.KClass
 /**
  * Changes the type this service is registered with
  *
+ * This does **not** allow lazy loaded services to be loaded via their interfaces,
+ * if you have a lazy loaded implementation, getting the interface as a service will not work until the implementation is loaded
+ *
  * This may be useful in situations where impl files are services, but only the interface needs to be exposed
  *
  * @see BService
