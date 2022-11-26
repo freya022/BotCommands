67da77a1864e65002874322c5b6f179996dd9fe0
freya02 <41875020+freya022@users.noreply.github.com>
2022-11-26 13:53:49 +0100

    Get lifetime/component types from int keys

--- src/main/kotlin/com/freya02/botcommands/internal/data/LifetimeType.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/data/LifetimeType.kt
@@ -2,13 +2,13 @@ package com.freya02.botcommands.internal.data
 
 import com.freya02.botcommands.internal.throwUser
 
-internal enum class LifetimeType(val id: Int) {
+enum class LifetimeType(val key: Int) {
     PERSISTENT(1),
     EPHEMERAL(2);
 
     companion object {
-        fun fromId(id: Int): LifetimeType {
-            return values().find { it.id == id } ?: throwUser("Unknown LifetimeType: $id")
+        fun fromId(key: Int): LifetimeType {
+            return values().find { it.key == key } ?: throwUser("Unknown LifetimeType: $key")
         }
     }
 }
