053f6865376790a1cc8ac46a03c54db5f9740e0a
freya02 <41875020+freya022@users.noreply.github.com>
2022-11-29 18:33:31 +0100

    Fix compile

--- src/main/kotlin/com/freya02/botcommands/api/new_components/ComponentGroup.kt
+++ src/main/kotlin/com/freya02/botcommands/api/new_components/ComponentGroup.kt
@@ -2,7 +2,7 @@ package com.freya02.botcommands.api.new_components
 
 import net.dv8tion.jda.api.events.interaction.component.GenericComponentInteractionCreateEvent
 
-class ComponentGroup(private val componentIds: List<Int>) {
+class ComponentGroup(private val componentIds: List<String>) {
     @JvmSynthetic
     suspend fun await(): GenericComponentInteractionCreateEvent = TODO()
 }
