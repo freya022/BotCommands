d51202c16efde80f4573ac20f39f6c33d74734cc
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-03 00:01:25 +0100

    Merge IActionableComponent sub-interfaces

--- src/main/kotlin/com/freya02/botcommands/api/new_components/builder/IActionableComponent.kt
+++ src/main/kotlin/com/freya02/botcommands/api/new_components/builder/IActionableComponent.kt
@@ -1,7 +1,20 @@
 package com.freya02.botcommands.api.new_components.builder
 
+import com.freya02.botcommands.api.components.event.ButtonEvent
 import com.freya02.botcommands.internal.new_components.ComponentHandler
+import java.util.function.Consumer
 
 interface IActionableComponent {
     val handler: ComponentHandler?
+}
+
+interface IPersistentActionableComponent<T : IPersistentActionableComponent<T>> : IActionableComponent {
+    fun bindTo(handlerName: String, vararg data: Any?): T
+}
+
+interface IEphemeralActionableComponent<T : IEphemeralActionableComponent<T>> : IActionableComponent {
+    fun bindTo(handler: Consumer<ButtonEvent>): T = bindTo { handler.accept(it) }
+
+    @JvmSynthetic
+    fun bindTo(handler: suspend (ButtonEvent) -> Unit): T
 }
--- src/main/kotlin/com/freya02/botcommands/api/new_components/builder/IPersistentActionableComponent.kt
+++
@@ -1,5 +0,0 @@
-package com.freya02.botcommands.api.new_components.builder
-
-interface IPersistentActionableComponent<T : IPersistentActionableComponent<T>> : IActionableComponent {
-    fun bindTo(handlerName: String, vararg data: Any?): T
-}
