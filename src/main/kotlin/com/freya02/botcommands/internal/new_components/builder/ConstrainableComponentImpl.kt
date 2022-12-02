ddb044f53923d5ecf8b13e3415499c5472338af0
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-02 20:02:01 +0100

    Add constrainable & unique component feature impls

---
+++ src/main/kotlin/com/freya02/botcommands/internal/new_components/builder/ConstrainableComponentImpl.kt
@@ -0,0 +1,13 @@
+package com.freya02.botcommands.internal.new_components.builder
+
+import com.freya02.botcommands.api.components.InteractionConstraints
+import com.freya02.botcommands.api.new_components.builder.IConstrainableComponent
+
+class ConstrainableComponentImpl<T : IConstrainableComponent<T>> : IConstrainableComponent<T> {
+    override var constraints: InteractionConstraints = InteractionConstraints()
+
+    override fun constraints(block: InteractionConstraints.() -> Unit): T = this.also { constraints.apply(block) } as T
+
+    //TODO prefer property accessors
+    override fun setConstraints(constraints: InteractionConstraints): T = this.also { this.constraints = constraints } as T
+}
---
+++ src/main/kotlin/com/freya02/botcommands/internal/new_components/builder/UniqueComponentImpl.kt
@@ -0,0 +1,10 @@
+package com.freya02.botcommands.internal.new_components.builder
+
+import com.freya02.botcommands.api.new_components.builder.IUniqueComponent
+
+class UniqueComponentImpl<T : IUniqueComponent<T>> : IUniqueComponent<T> {
+    override var oneUse: Boolean = false
+
+    //TODO prefer property accessors
+    override fun oneUse(): T = this.also { oneUse = true } as T
+}
