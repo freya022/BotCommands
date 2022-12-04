1fe998489b26e7b78cc7bce046502389ea6b7b46
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-04 15:20:55 +0100

    Use feature impls in AbstractComponentBuilder

--- src/main/kotlin/com/freya02/botcommands/api/new_components/builder/AbstractComponentBuilder.kt
+++ src/main/kotlin/com/freya02/botcommands/api/new_components/builder/AbstractComponentBuilder.kt
@@ -1,12 +1,9 @@
 package com.freya02.botcommands.api.new_components.builder
 
-import com.freya02.botcommands.api.ReceiverConsumer
-import com.freya02.botcommands.api.apply
-import com.freya02.botcommands.api.components.InteractionConstraints
+import com.freya02.botcommands.internal.new_components.builder.ConstrainableComponentImpl
+import com.freya02.botcommands.internal.new_components.builder.UniqueComponentImpl
 
-abstract class AbstractComponentBuilder internal constructor() : ComponentBuilder {
-    final override var oneUse: Boolean = false
-    final override var constraints: InteractionConstraints = InteractionConstraints()
-
-    override fun constraints(block: ReceiverConsumer<InteractionConstraints>) { constraints.apply(block) }
-}
+abstract class AbstractComponentBuilder internal constructor() :
+    ComponentBuilder,
+    IUniqueComponent by UniqueComponentImpl(),
+    IConstrainableComponent by ConstrainableComponentImpl()
