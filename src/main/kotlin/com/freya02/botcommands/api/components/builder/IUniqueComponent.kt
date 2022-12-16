9398f47aa5f7f337608a6d783357afc7a6d5449e
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-16 11:09:17 +0100

    IUniqueComponent docs

--- src/main/kotlin/com/freya02/botcommands/api/components/builder/IUniqueComponent.kt
+++ src/main/kotlin/com/freya02/botcommands/api/components/builder/IUniqueComponent.kt
@@ -1,5 +1,17 @@
 package com.freya02.botcommands.api.components.builder
 
+/**
+ * Allows components to be used once before being deleted
+ *
+ * After the component is used, it will be deleted from the database.
+ *
+ * If the component is a group, then all of it's owned components will also be deleted.
+ *
+ * If the component is inside a group, then all the group's components will also be deleted.
+ */
 interface IUniqueComponent {
+    /**
+     * Sets this component as being usable only once before being deleted
+     */
     var oneUse: Boolean
 }
