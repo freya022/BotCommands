84b4cb4dcfd5ee1bfd4cf656c374287d9163f09c
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-16 11:58:55 +0100

    IConstrainableComponent docs

--- src/main/kotlin/com/freya02/botcommands/api/components/builder/IConstrainableComponent.kt
+++ src/main/kotlin/com/freya02/botcommands/api/components/builder/IConstrainableComponent.kt
@@ -4,16 +4,28 @@ import com.freya02.botcommands.api.ReceiverConsumer
 import com.freya02.botcommands.api.components.data.InteractionConstraints
 import net.dv8tion.jda.api.Permission
 
+/**
+ * Allows components to have constraints
+ *
+ * @see InteractionConstraints
+ */
 interface IConstrainableComponent {
     var constraints: InteractionConstraints
 
+    /** Adds user IDs to the constraints */
     fun addUserIds(vararg userIds: Long) = addUserIds(userIds.asList())
+    /** Adds role IDs to the constraints */
     fun addRoleIds(vararg roleIds: Long) = addRoleIds(roleIds.asList())
+    /** Adds permissions to the constraints */
     fun addPermissions(vararg permissions: Permission) = addPermissions(permissions.asList())
 
+    /** Adds user IDs to the constraints */
     fun addUserIds(userIds: Collection<Long>)
+    /** Adds role IDs to the constraints */
     fun addRoleIds(roleIds: Collection<Long>)
+    /** Adds permissions to the constraints */
     fun addPermissions(permissions: Collection<Permission>)
 
+    /** Allows manipulating the [InteractionConstraints] instance */
     fun constraints(block: ReceiverConsumer<InteractionConstraints>)
 }
