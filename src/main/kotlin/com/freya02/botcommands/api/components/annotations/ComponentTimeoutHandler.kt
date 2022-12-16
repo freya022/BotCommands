407d916388577a96ab9e9f53ce6a38f657a1924d
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-16 18:20:35 +0100

    Timeout handler annotation docs

--- src/main/kotlin/com/freya02/botcommands/api/components/annotations/ComponentTimeoutHandler.kt
+++ src/main/kotlin/com/freya02/botcommands/api/components/annotations/ComponentTimeoutHandler.kt
@@ -1,4 +1,13 @@
 package com.freya02.botcommands.api.components.annotations
 
+import com.freya02.botcommands.api.components.data.ComponentTimeoutData
+
+/**
+ * Annotation marking a method as a persistent component timeout handler
+ *
+ * Requirements:
+ * - Public & non-static
+ * - First parameter is a [ComponentTimeoutData]
+ */
 @Target(AnnotationTarget.FUNCTION)
 annotation class ComponentTimeoutHandler(val name: String)
