882ff248f7088aef60829681b723645e2ac7a235
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-19 13:59:40 +0100

    Move BuilderFunctionHolder facade, inside class

--- src/main/kotlin/com/freya02/botcommands/api/builder/BuilderFunctionHolder.kt
+++ src/main/kotlin/com/freya02/botcommands/api/builder/BuilderFunctionHolder.kt
@@ -5,12 +5,6 @@ import com.freya02.botcommands.internal.throwUser
 import com.freya02.botcommands.internal.utils.ReflectionUtilsKt.reflectReference
 import kotlin.reflect.KFunction
 
-private object Dummy {
-    fun dummy(): Nothing = throwInternal("A command with no function set has been used")
-}
-
-private val NO_FUNCTION = Dummy::dummy
-
 @Suppress("UNCHECKED_CAST")
 open class BuilderFunctionHolder<R> internal constructor() : IBuilderFunctionHolder<R> {
     override var function: KFunction<R> = NO_FUNCTION
@@ -24,5 +18,13 @@ open class BuilderFunctionHolder<R> internal constructor() : IBuilderFunctionHol
         if (!isFunctionInitialized()) {
             throwUser("A command must have its function set")
         }
+    }
+
+    private object Dummy {
+        fun dummy(): Nothing = throwInternal("A command with no function set has been used")
+    }
+
+    companion object {
+        private val NO_FUNCTION = Dummy::dummy
     }
 }
