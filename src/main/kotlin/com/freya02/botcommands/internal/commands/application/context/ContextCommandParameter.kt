14ff0af46eb5d034e184b2badcefe6849f2cf629
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-19 11:42:42 +0100

    Move `MethodParameter#isOptional` in a lazy block in subclasses

--- src/main/kotlin/com/freya02/botcommands/internal/commands/application/context/ContextCommandParameter.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/commands/application/context/ContextCommandParameter.kt
@@ -2,6 +2,7 @@ package com.freya02.botcommands.internal.commands.application.context
 
 import com.freya02.botcommands.internal.parameters.MethodParameter
 import com.freya02.botcommands.internal.parameters.MethodParameterType
+import com.freya02.botcommands.internal.utils.ReflectionMetadata.isNullable
 import kotlin.reflect.KParameter
 
 abstract class ContextCommandParameter<R>(
@@ -10,4 +11,5 @@ abstract class ContextCommandParameter<R>(
 ) : MethodParameter {
     override val methodParameterType = MethodParameterType.OPTION
     override val kParameter = parameter
+    override val isOptional: Boolean by lazy { kParameter.isNullable || kParameter.isOptional }
 }
--- src/main/kotlin/com/freya02/botcommands/internal/parameters/MethodParameter.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/parameters/MethodParameter.kt
@@ -2,7 +2,6 @@ package com.freya02.botcommands.internal.parameters
 
 import com.freya02.botcommands.internal.findDeclarationName
 import com.freya02.botcommands.internal.throwInternal
-import com.freya02.botcommands.internal.utils.ReflectionMetadata.isNullable
 import kotlin.reflect.KParameter
 import kotlin.reflect.KType
 import kotlin.reflect.jvm.jvmErasure
@@ -22,7 +21,6 @@ interface MethodParameter {
     val isOption: Boolean
         get() = methodParameterType == MethodParameterType.OPTION
     val isOptional: Boolean
-        get() = kParameter.isNullable || kParameter.isOptional
     /** This checks the java primitiveness, not kotlin, kotlin.Double is an object. */
     val isPrimitive: Boolean
         get() = kParameter.type.jvmErasure.java.isPrimitive
