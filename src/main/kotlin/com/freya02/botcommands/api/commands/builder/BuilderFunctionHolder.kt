6b2e73b4e6e8a8775b3f730d6a78ef323dc1d326
freya02 <41875020+freya022@users.noreply.github.com>
2022-12-28 16:37:27 +0100

    Throw if function reference is not public

--- src/main/kotlin/com/freya02/botcommands/api/commands/builder/BuilderFunctionHolder.kt
+++ src/main/kotlin/com/freya02/botcommands/api/commands/builder/BuilderFunctionHolder.kt
@@ -4,11 +4,16 @@ import com.freya02.botcommands.internal.throwInternal
 import com.freya02.botcommands.internal.throwUser
 import com.freya02.botcommands.internal.utils.ReflectionUtilsKt.reflectReference
 import kotlin.reflect.KFunction
+import kotlin.reflect.KVisibility
 
 @Suppress("UNCHECKED_CAST")
 open class BuilderFunctionHolder<R> internal constructor() : IBuilderFunctionHolder<R> {
     override var function: KFunction<R> = NO_FUNCTION
         set(value) {
+            if (value.visibility != KVisibility.PUBLIC) {
+                //Cannot use KFunction#shortSignature as ReflectionMetadata doesn't read private methods
+                throwUser("$value : Function needs to be public")
+            }
             field = value.reflectReference() as KFunction<R>
         }
 
