60ed13592e9347f589d384260b2aa8b5e2b8ac8a
freya02 <41875020+freya022@users.noreply.github.com>
2023-01-16 14:51:35 +0100

    Add SlashUploadStream

---
+++ src/test/kotlin/com/freya02/botcommands/test_kt/commands/slash/SlashUploadStream.kt
@@ -0,0 +1,30 @@
+package com.freya02.botcommands.test_kt.commands.slash
+
+import com.freya02.botcommands.api.annotations.CommandMarker
+import com.freya02.botcommands.api.commands.application.ApplicationCommand
+import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
+import dev.minn.jda.ktx.coroutines.await
+import dev.minn.jda.ktx.messages.Embed
+import net.dv8tion.jda.api.utils.FileUpload
+import kotlin.io.path.Path
+import kotlin.io.path.inputStream
+
+@CommandMarker
+class SlashUploadStream : ApplicationCommand() {
+    @JDASlashCommand(name = "upload_stream")
+    suspend fun execute(event: GuildSlashEvent) {
+        Embed {
+            description = "empty"
+        }.let { event.replyEmbeds(it).setEphemeral(true).await() }
+
+        Path("target", "apidocs", "resources", "glass.png")
+                .inputStream()
+                .let { FileUpload.fromData(it, "glass.png") }
+                .use { upload ->
+                    Embed {
+                        image = "attachment://glass.png"
+                    }.also { event.hook.editOriginalEmbeds(it).setFiles(upload).queue() }
+                }
+    }
+}
