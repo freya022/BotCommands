996b741c8fc71fd61bc50ec7c9afe8fb171600c3
freya02 <41875020+freya022@users.noreply.github.com>
2023-01-18 21:41:14 +0100

    A resolver is needed for each context type

--- src/main/kotlin/com/freya02/botcommands/internal/parameters/resolvers/localization/LocalizationContextResolverFactory.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/parameters/resolvers/localization/LocalizationContextResolverFactory.kt
@@ -1,7 +1,9 @@
 package com.freya02.botcommands.internal.parameters.resolvers.localization
 
 import com.freya02.botcommands.api.localization.annotations.LocalizationBundle
+import com.freya02.botcommands.api.localization.context.AppLocalizationContext
 import com.freya02.botcommands.api.localization.context.LocalizationContext
+import com.freya02.botcommands.api.localization.context.TextLocalizationContext
 import com.freya02.botcommands.api.parameters.ParameterResolverFactory
 import com.freya02.botcommands.internal.annotations.IncludeClasspath
 import com.freya02.botcommands.internal.localization.LocalizationContextImpl
@@ -12,15 +14,33 @@ import kotlin.reflect.KParameter
 import kotlin.reflect.full.findAnnotation
 
 @IncludeClasspath
-internal class LocalizationContextResolverFactory : ParameterResolverFactory<LocalizationContextResolver, LocalizationContext>(
-    LocalizationContextResolver::class,
-    LocalizationContext::class
+internal class LocalizationContextResolverFactory : ParameterResolverFactory<AppLocalizationContextResolver, AppLocalizationContext>(
+    AppLocalizationContextResolver::class,
+    AppLocalizationContext::class
 ) {
-    override fun get(parameter: KParameter): LocalizationContextResolver {
+    override fun get(parameter: KParameter): AppLocalizationContextResolver {
         val annotation = parameter.findAnnotation<LocalizationBundle>()
             ?: throwUser(parameter.function, "${LocalizationContext::class.simpleName} parameters must be annotated with @${LocalizationBundle::class.simpleName}")
 
-        return LocalizationContextResolver(LocalizationContextImpl(
+        return AppLocalizationContextResolver(LocalizationContextImpl(
+            localizationBundle = annotation.value,
+            localizationPrefix = annotation.prefix.nullIfEmpty(),
+            guildLocale = null,
+            userLocale = null
+        ))
+    }
+}
+
+@IncludeClasspath
+internal class TextLocalizationContextResolverFactory : ParameterResolverFactory<TextLocalizationContextResolver, TextLocalizationContext>(
+    TextLocalizationContextResolver::class.java,
+    TextLocalizationContext::class.java
+) {
+    override fun get(parameter: KParameter): TextLocalizationContextResolver {
+        val annotation = parameter.findAnnotation<LocalizationBundle>()
+            ?: throwUser(parameter.function, "${LocalizationContext::class.simpleName} parameters must be annotated with @${LocalizationBundle::class.simpleName}")
+
+        return TextLocalizationContextResolver(LocalizationContextImpl(
             localizationBundle = annotation.value,
             localizationPrefix = annotation.prefix.nullIfEmpty(),
             guildLocale = null,
