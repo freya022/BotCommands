6f38b51e36814e52a03f2fbb9dbca6681523e37f
freya02 <41875020+freya022@users.noreply.github.com>
2023-01-18 22:07:53 +0100

    Add localization test

---
+++ src/test/kotlin/com/freya02/botcommands/test_kt/commands/slash/SlashLocalization.kt
@@ -0,0 +1,36 @@
+package com.freya02.botcommands.test_kt.commands.slash
+
+import com.freya02.botcommands.api.annotations.CommandMarker
+import com.freya02.botcommands.api.commands.application.ApplicationCommand
+import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
+import com.freya02.botcommands.api.localization.Localization.Entry.entry
+import com.freya02.botcommands.api.localization.annotations.LocalizationBundle
+import com.freya02.botcommands.api.localization.context.AppLocalizationContext
+import dev.minn.jda.ktx.messages.reply_
+import net.dv8tion.jda.api.interactions.DiscordLocale
+
+@CommandMarker
+class SlashLocalization : ApplicationCommand() {
+    @JDASlashCommand(name = "localization")
+    fun onSlashLocalization(event: GuildSlashEvent,
+                            @LocalizationBundle("Test", prefix = "commands.localization") ctx: AppLocalizationContext) {
+        val content = """
+            User localized (${ctx.userLocale}): %s
+            Guild localized (${ctx.guildLocale}): %s
+            German localized: %s
+        """.trimIndent().format(
+            ctx.localizeUser("response",
+                entry("guild_users", event.guild.memberCount),
+                entry("uptime", 3.141519)),
+            ctx.localizeGuild("response",
+                entry("guild_users", event.guild.memberCount),
+                entry("uptime", 3.141519)),
+            ctx.localize(DiscordLocale.GERMAN, "response",
+                entry("guild_users", event.guild.memberCount),
+                entry("uptime", 3.141519))
+        )
+
+        event.reply_(content, ephemeral = true).queue()
+    }
+}
