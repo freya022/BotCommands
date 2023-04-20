8233d2e7388e5bf6716d791512080826c70433a7
freya02 <41875020+freya022@users.noreply.github.com>
2023-04-20 20:54:32 +0200

    Support annotated varargs

--- src/test/kotlin/com/freya02/botcommands/test_kt/commands/slash/SlashVarargs.kt
+++ src/test/kotlin/com/freya02/botcommands/test_kt/commands/slash/SlashVarargs.kt
@@ -4,12 +4,16 @@ import com.freya02.botcommands.api.annotations.CommandMarker
 import com.freya02.botcommands.api.commands.application.ApplicationCommand
 import com.freya02.botcommands.api.commands.application.GlobalApplicationCommandManager
 import com.freya02.botcommands.api.commands.application.annotations.AppDeclaration
+import com.freya02.botcommands.api.commands.application.annotations.AppOption
 import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
+import com.freya02.botcommands.api.commands.application.slash.annotations.VarArgs
 import dev.minn.jda.ktx.messages.reply_
 
 @CommandMarker
 class SlashVarargs : ApplicationCommand() {
-    fun onSlashVarargs(event: GuildSlashEvent, list: List<Int?>) {
+    @JDASlashCommand(name = "varargs_annotated")
+    fun onSlashVarargs(event: GuildSlashEvent, @AppOption @VarArgs(3, numRequired = 1) list: List<Int?>) {
         event.reply_("ok $list", ephemeral = true).queue()
     }
 
