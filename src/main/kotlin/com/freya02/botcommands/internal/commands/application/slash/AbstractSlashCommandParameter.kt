37bdf6a090ea272fb990236b4ab7bbc35bc20a90
freya02 <41875020+freya022@users.noreply.github.com>
2023-04-22 17:09:54 +0200

    Check if parameter type is a List when constructing AbstractSlashCommandParameter

--- src/main/kotlin/com/freya02/botcommands/internal/commands/application/slash/AbstractSlashCommandParameter.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/commands/application/slash/AbstractSlashCommandParameter.kt
@@ -3,11 +3,13 @@ package com.freya02.botcommands.internal.commands.application.slash
 import com.freya02.botcommands.api.commands.application.slash.builder.SlashCommandOptionBuilder
 import com.freya02.botcommands.api.parameters.SlashParameterResolver
 import com.freya02.botcommands.internal.commands.application.ApplicationCommandParameter
+import com.freya02.botcommands.internal.requireUser
 import com.freya02.botcommands.internal.throwUser
 import com.freya02.botcommands.internal.utils.ReflectionMetadata.function
 import com.freya02.botcommands.internal.utils.ReflectionMetadata.isJava
 import com.freya02.botcommands.internal.utils.ReflectionUtils.collectionElementType
 import kotlin.reflect.KParameter
+import kotlin.reflect.jvm.jvmErasure
 
 abstract class AbstractSlashCommandParameter(
     parameter: KParameter,
@@ -20,9 +22,15 @@ abstract class AbstractSlashCommandParameter(
         get() = varArgs != -1
 
     init {
-        if (isVarArg && varArgs != numRequired) {
-            if (parameter.collectionElementType?.isMarkedNullable == false && !parameter.function.isJava) {
-                throwUser(parameter.function, "List element type should be nullable on vararg parameters where not all arguments are required")
+        if (isVarArg) {
+            requireUser(kParameter.type.jvmErasure == List::class, kParameter.function) {
+                "Type of vararg options should be List"
+            }
+
+            if (varArgs != numRequired) {
+                if (parameter.collectionElementType?.isMarkedNullable == false && !parameter.function.isJava) {
+                    throwUser(parameter.function, "List element type should be nullable on vararg parameters where not all arguments are required")
+                }
             }
         }
     }
