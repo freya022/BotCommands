bc664efc4178aa7b401af30dcab6e88a230446cb
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-01 18:53:09 +0200

    Fix AbstractOption#kParameter docs

--- src/main/kotlin/com/freya02/botcommands/internal/AbstractOption.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/AbstractOption.kt
@@ -1,6 +1,6 @@
 package com.freya02.botcommands.internal
 
-import com.freya02.botcommands.api.commands.application.IApplicationCommandManager
+import com.freya02.botcommands.internal.core.options.builder.OptionAggregateBuildersImpl
 import com.freya02.botcommands.internal.parameters.MethodParameterType
 import com.freya02.botcommands.internal.parameters.MultiParameter
 import kotlin.reflect.KParameter
@@ -10,15 +10,14 @@ interface AbstractOption {
     val multiParameter: MultiParameter
 
     /**
-     * This might be a KParameter from the command function or from an aggregate function
+     * This might be a KParameter from the command function or from an aggregate function.
      *
-     * - In the case the option was created from a (Type)CommandBuilder aggregate, the KParameter is grabbed from the command function, set in [IApplicationCommandManager.slashCommand]
-     *      - This means the KFunction is mandatory, and must be passed to the command declaration (before the DSL is executed)
-     * - In the case the option was created in a user-defined aggregate, then the KParameter is grabbed from the aggregate function
+     * - In the case the option was created from [the internal aggregator][OptionAggregateBuildersImpl.theSingleAggregator], the KParameter is grabbed from the command function.
+     * - In the case the option was created in a user-defined aggregator, then the KParameter is grabbed from the aggregate function.
      *
-     * **Beware of the types and nullabilities**, the KParameter could be of an array type, which should be supported at a reason of 1 per aggregate
+     * **Beware of the types and nullabilities**, the KParameter could be of an array type.
      *
-     * **Also note:** this is not unique, multiple options can be bound to the same KParameter (varargs for example)
+     * **Also note:** this is not unique, multiple options can be bound to the same KParameter. (varargs for example)
      */
     val kParameter: KParameter
         get() = multiParameter.typeCheckingParameter
