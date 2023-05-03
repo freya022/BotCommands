633af1bb4309f35740590d6c05fafeb58406615b
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-03 11:40:35 +0200

    Refactor SlashUtils#getMethodOptions

--- src/main/kotlin/com/freya02/botcommands/internal/commands/application/slash/SlashUtils.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/commands/application/slash/SlashUtils.kt
@@ -5,7 +5,6 @@ import com.freya02.botcommands.api.commands.application.builder.ApplicationComma
 import com.freya02.botcommands.api.commands.application.slash.GlobalSlashEvent
 import com.freya02.botcommands.internal.IExecutableInteractionInfo
 import com.freya02.botcommands.internal.commands.application.ApplicationGeneratedMethodParameter
-import com.freya02.botcommands.internal.core.options.OptionType
 import com.freya02.botcommands.internal.parameters.resolvers.channels.ChannelResolver
 import com.freya02.botcommands.internal.requireUser
 import com.freya02.botcommands.internal.throwInternal
@@ -45,100 +44,98 @@ internal object SlashUtils {
         }
     }
 
-    @JvmStatic
-    fun SlashCommandInfo.getMethodOptions(guild: Guild?): List<OptionData> {
-        val list: MutableList<OptionData> = ArrayList()
-
-        val options = parameters.flatMap { it.commandOptions }
+    fun SlashCommandInfo.getMethodOptions(guild: Guild?) = parameters
+        .flatMap { it.commandOptions }
+        .filterIsInstance<SlashCommandOption>()
         //Move all optional options at the front
-        val orderedOptions = options.sortedWith { o1, o2 ->
-            when {
-                //Optional options are placed after required options
-                o1.isOptional && !o2.isOptional -> 1
-                //Required options are placed before optional options
-                !o1.isOptional && o2.isOptional -> -1
-                //If both are optional/required, keep order using index
-                else -> options.indexOf(o1).compareTo(options.indexOf(o2))
+        .let { options ->
+            options.sortedWith { o1, o2 ->
+                when {
+                    //Optional options are placed after required options
+                    o1.isOptional && !o2.isOptional -> 1
+                    //Required options are placed before optional options
+                    !o1.isOptional && o2.isOptional -> -1
+                    //If both are optional/required, keep order using index
+                    else -> options.indexOf(o1).compareTo(options.indexOf(o2))
+                }
             }
         }
-        for (option in orderedOptions) {
-            if (option.optionType != OptionType.OPTION) continue
-
-            option as SlashCommandOption
-
-            val name = option.discordName
-            val description = option.description
-
-            val resolver = option.resolver
-            val optionType = resolver.optionType
-
-            val data = OptionData(optionType, name, description)
-
-            when (optionType) {
-                JDAOptionType.CHANNEL -> {
-                    //If there are no specified channel types, then try to get the channel type from AbstractChannelResolver
-                    // Otherwise set the channel types of the option, if available
-                    if (option.channelTypes.isEmpty() && resolver is ChannelResolver) {
-                        data.setChannelTypes(resolver.channelTypes)
-                    } else if (option.channelTypes.isEmpty()) {
-                        data.setChannelTypes(option.channelTypes)
-                    }
-                }
-                JDAOptionType.INTEGER -> {
-                    option.range?.let {
-                        data.setMinValue(it.min.toLong())
-                        data.setMaxValue(it.max.toLong())
-                    }
-                }
-                JDAOptionType.NUMBER -> {
-                    option.range?.let {
-                        data.setMinValue(it.min.toDouble())
-                        data.setMaxValue(it.max.toDouble())
-                    }
-                }
-                JDAOptionType.STRING -> {
-                    option.length?.let {
-                        data.setRequiredLength(it.min, it.max)
-                    }
-                }
-                else -> {}
-            }
-
-            if (option.hasAutocomplete()) {
-                requireUser(optionType.canSupportChoices(), option.kParameter.function) {
-                    "Slash command option #${option.index} does not support autocomplete"
-                }
-
-                data.isAutoComplete = true
-            }
-
-            if (optionType.canSupportChoices()) {
-                var choices: Collection<Command.Choice>? = null
-
-                if (!option.choices.isNullOrEmpty()) {
-                    choices = option.choices
-                } else if (option.usePredefinedChoices) { //Opt in
-                    val predefinedChoices = resolver.getPredefinedChoices(guild)
-                    if (predefinedChoices.isEmpty())
-                        throwUser(option.kParameter.function, "Predefined choices were used for option '${option.declaredName}' but no choices were returned")
-                    choices = predefinedChoices
-                }
-
-                if (choices != null) {
-                    requireUser(!option.hasAutocomplete(), option.kParameter.function) {
-                        "Slash command option #${option.index} cannot have autocomplete and choices at the same time"
-                    }
-
-                    data.addChoices(choices)
-                }
-            }
-
-            data.isRequired = !option.isOptional
-
-            list.add(data)
+        .map { option ->
+            OptionData(option.resolver.optionType, option.discordName, option.description)
+                .also { configureOptionData(option, it, guild) }
         }
 
-        return list
+    private fun configureOptionData(option: SlashCommandOption, data: OptionData, guild: Guild?) {
+        val resolver = option.resolver
+        val optionType = resolver.optionType
+
+        when (optionType) {
+            JDAOptionType.CHANNEL -> {
+                //If there are no specified channel types, then try to get the channel type from AbstractChannelResolver
+                // Otherwise set the channel types of the option, if available
+                if (option.channelTypes.isEmpty() && resolver is ChannelResolver) {
+                    data.setChannelTypes(resolver.channelTypes)
+                } else if (option.channelTypes.isEmpty()) {
+                    data.setChannelTypes(option.channelTypes)
+                }
+            }
+
+            JDAOptionType.INTEGER -> {
+                option.range?.let {
+                    data.setMinValue(it.min.toLong())
+                    data.setMaxValue(it.max.toLong())
+                }
+            }
+
+            JDAOptionType.NUMBER -> {
+                option.range?.let {
+                    data.setMinValue(it.min.toDouble())
+                    data.setMaxValue(it.max.toDouble())
+                }
+            }
+
+            JDAOptionType.STRING -> {
+                option.length?.let {
+                    data.setRequiredLength(it.min, it.max)
+                }
+            }
+
+            else -> {}
+        }
+
+        if (option.hasAutocomplete()) {
+            requireUser(optionType.canSupportChoices(), option.kParameter.function) {
+                "Slash command option #${option.index} does not support autocomplete"
+            }
+
+            data.isAutoComplete = true
+        }
+
+        if (optionType.canSupportChoices()) {
+            var choices: Collection<Command.Choice>? = null
+
+            if (!option.choices.isNullOrEmpty()) {
+                choices = option.choices
+            } else if (option.usePredefinedChoices) { //Opt in
+                val predefinedChoices = resolver.getPredefinedChoices(guild)
+                if (predefinedChoices.isEmpty())
+                    throwUser(
+                        option.kParameter.function,
+                        "Predefined choices were used for option '${option.declaredName}' but no choices were returned"
+                    )
+                choices = predefinedChoices
+            }
+
+            if (choices != null) {
+                requireUser(!option.hasAutocomplete(), option.kParameter.function) {
+                    "Slash command option #${option.index} cannot have autocomplete and choices at the same time"
+                }
+
+                data.addChoices(choices)
+            }
+        }
+
+        data.isRequired = !option.isOptional
     }
 
     internal inline fun <reified T> ApplicationCommandBuilder<*>.checkEventScope() {
