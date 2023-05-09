2929f1668b06b66428717ffa4aecab607f3e0f9e
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-09 21:51:19 +0200

    Fix intent check *displaying* the wrong missing intents, warn instead of throwing

---
+++ src/test/java/com/freya02/botcommands/test/commands/slash/SlashEventWaiter.java
@@ -0,0 +1,20 @@
+package com.freya02.botcommands.test.commands.slash;
+
+import com.freya02.botcommands.api.application.ApplicationCommand;
+import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+import com.freya02.botcommands.api.waiter.EventWaiter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class SlashEventWaiter extends ApplicationCommand {
+	@JDASlashCommand(
+			name = "event_waiter"
+	)
+	public void onSlashEventWaiter(GuildSlashEvent event) {
+		event.reply("Waiting for a message").setEphemeral(true).queue();
+
+		EventWaiter.of(MessageReceivedEvent.class)
+				.setOnSuccess(e -> event.getHook().setEphemeral(true).sendMessage("Received !").queue())
+				.submit();
+	}
+}
