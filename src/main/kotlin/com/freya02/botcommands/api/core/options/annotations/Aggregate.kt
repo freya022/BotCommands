876b5c5187ef44445679060bdeb01a7b9950feb1
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-20 11:34:45 +0200

    Add docs for `@Aggregate`

--- src/main/kotlin/com/freya02/botcommands/api/core/options/annotations/Aggregate.kt
+++ src/main/kotlin/com/freya02/botcommands/api/core/options/annotations/Aggregate.kt
@@ -1,5 +1,15 @@
 package com.freya02.botcommands.api.core.options.annotations
 
+import com.freya02.botcommands.api.components.annotations.JDAButtonListener
+import com.freya02.botcommands.api.modals.annotations.ModalHandler
+
+/**
+ * Annotation used to mark an option as being the result of an aggregate.
+ *
+ * The framework will take the constructor of that parameter, so, annotations must be on the constructor's parameters instead.
+ *
+ * Can be used on parameters of [ModalHandler] or [JDAButtonListener] functions.
+ */
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.VALUE_PARAMETER)
 annotation class Aggregate
