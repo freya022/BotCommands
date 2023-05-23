c5f13589a1591abb2c81fbbdbdb7029a49e4ec52
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-23 21:24:55 +0200

    Fix imports

--- src/test/java/com/freya02/botcommands/test/commands/slash/SlashEventWaiter.java
+++ src/test/java/com/freya02/botcommands/test/commands/slash/SlashEventWaiter.java
@@ -1,8 +1,8 @@
 package com.freya02.botcommands.test.commands.slash;
 
-import com.freya02.botcommands.api.application.ApplicationCommand;
-import com.freya02.botcommands.api.application.slash.GuildSlashEvent;
-import com.freya02.botcommands.api.application.slash.annotations.JDASlashCommand;
+import com.freya02.botcommands.api.commands.application.ApplicationCommand;
+import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand;
 import com.freya02.botcommands.api.waiter.EventWaiter;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
