1259865b912b640baa2c7fca6401923bf69a47c1
freya02 <41875020+freya022@users.noreply.github.com>
2023-05-27 22:06:21 +0200

    Fix Function#equals

--- src/main/kotlin/com/freya02/botcommands/internal/core/reflection/Function.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/core/reflection/Function.kt
@@ -7,6 +7,14 @@ sealed class Function<R>(kFunction: KFunction<R>) {
     val kFunction = kFunction.reflectReference()
     val parametersSize = kFunction.parameters.size
 
-    override fun equals(other: Any?) = kFunction == other
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Function<*>
+
+        return kFunction == other.kFunction
+    }
+
     override fun hashCode() = kFunction.hashCode()
 }
