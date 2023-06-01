3b2af84bae9cd127ec9e71816160b8190595ce30
freya02 <41875020+freya022@users.noreply.github.com>
2023-06-01 18:26:20 +0200

    Use a second map to find by type faster

--- src/main/kotlin/com/freya02/botcommands/internal/core/service/ServiceProviders.kt
+++ src/main/kotlin/com/freya02/botcommands/internal/core/service/ServiceProviders.kt
@@ -1,22 +1,27 @@
 package com.freya02.botcommands.internal.core.service
 
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 
 internal class ServiceProviders {
-    private val map: MutableMap<String, ServiceProvider> = ConcurrentHashMap()
+    private val nameMap: MutableMap<String, ServiceProvider> = ConcurrentHashMap()
+    private val typeMap: MutableMap<KClass<*>, MutableList<ServiceProvider>> = ConcurrentHashMap()
 
     internal fun putServiceProvider(serviceProvider: ServiceProvider) {
-        if (serviceProvider.name in map)
+        if (serviceProvider.name in nameMap)
             throw IllegalArgumentException("Service provider for ${serviceProvider.providerKey} already exists")
-        map[serviceProvider.name] = serviceProvider
+        nameMap[serviceProvider.name] = serviceProvider
+        serviceProvider.types.forEach { type ->
+            typeMap.computeIfAbsent(type) { CopyOnWriteArrayList() }.add(serviceProvider)
+        }
     }
 
     internal fun putServiceProvider(kClass: KClass<*>) = putServiceProvider(ClassServiceProvider(kClass))
     internal fun putServiceProvider(kFunction: KFunction<*>) = putServiceProvider(FunctionServiceProvider(kFunction))
 
-    internal fun findAllForType(type: KClass<*>): List<ServiceProvider> = map.values.filter { type in it.types }
-    internal fun findForType(type: KClass<*>): ServiceProvider? = map.values.find { type in it.types }
-    internal fun findForName(name: String): ServiceProvider? = map[name]
+    internal fun findAllForType(type: KClass<*>): List<ServiceProvider> = typeMap[type] ?: emptyList()
+    internal fun findForType(type: KClass<*>): ServiceProvider? = typeMap[type]?.first()
+    internal fun findForName(name: String): ServiceProvider? = nameMap[name]
 }
