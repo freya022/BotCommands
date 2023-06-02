e50ad832e578ed1bb658f5f6931d2ad19ca7020f
freya02 <41875020+freya022@users.noreply.github.com>
2023-06-03 00:35:48 +0200

    Add test code

---
+++ src/test/kotlin/com/freya02/botcommands/test_kt/services/FactoryServiceTest.kt
@@ -0,0 +1,19 @@
+package com.freya02.botcommands.test_kt.services
+
+import com.freya02.botcommands.api.core.service.annotations.BService
+import com.freya02.botcommands.api.core.service.annotations.ConditionalService
+import com.freya02.botcommands.api.core.service.annotations.Dependencies
+
+//Can test failure if FactoryServiceTest is not instantiable, by commenting @Dependencies
+@BService
+@Dependencies([FactoryServiceTest::class])
+class FactoryServiceTestUser(service: FactoryServiceTest)
+
+class FactoryServiceTest private constructor() {
+//    @BService //Not necessary
+    object FactoryServiceTestProvider {
+        @BService
+        @ConditionalService([ConditionalServiceTest.Companion::class])
+        fun getFactory() = FactoryServiceTest()
+    }
+}
