a471d08f93dade9f161133055f3be61b3efb3396
freya02 <41875020+freya022@users.noreply.github.com>
2023-06-17 23:31:23 +0200

    Add missing `@CommandDSL`

--- src/main/kotlin/com/freya02/botcommands/api/commands/application/slash/autocomplete/builder/AutocompleteCacheInfoBuilder.kt
+++ src/main/kotlin/com/freya02/botcommands/api/commands/application/slash/autocomplete/builder/AutocompleteCacheInfoBuilder.kt
@@ -2,7 +2,9 @@ package com.freya02.botcommands.api.commands.application.slash.autocomplete.buil
 
 import com.freya02.botcommands.api.commands.application.slash.autocomplete.AutocompleteCacheInfo
 import com.freya02.botcommands.api.commands.application.slash.autocomplete.AutocompleteCacheMode
+import com.freya02.botcommands.internal.commands.CommandDSL
 
+@CommandDSL
 class AutocompleteCacheInfoBuilder internal constructor(val cacheMode: AutocompleteCacheMode) {
     internal fun build() = AutocompleteCacheInfo(this)
 
