2dda0e373ec344a9b812e2fe7dac3befa50884a3
freya02 <41875020+freya022@users.noreply.github.com>
2023-07-04 21:25:50 +0200

    Convert EventWaiter interface to Kotlin, add EventWaiter#await

--- src/main/java/com/freya02/botcommands/api/waiter/EventWaiter.kt
+++ src/main/java/com/freya02/botcommands/api/waiter/EventWaiter.kt
@@ -1,43 +1,52 @@
-package com.freya02.botcommands.api.waiter;
+package com.freya02.botcommands.api.waiter
 
-import net.dv8tion.jda.api.events.Event;
-import org.jetbrains.annotations.NotNull;
+import com.freya02.botcommands.api.core.service.annotations.InjectedService
+import dev.minn.jda.ktx.coroutines.await
+import net.dv8tion.jda.api.events.Event
 
 /**
- * An event waiter - if you need to wait for an event to occur while not blocking threads or having listeners everywhere.
- * <br>You provide the type of the JDA event you want to get
- * <br>You can then set properties such as preconditions, timeouts and actions to run when the event gets received / has an exception, etc...
- * <br>This event waiter cannot be constructed and does not need to be registered to the JDA instance, it is already done automatically
+ * An event waiter - if you need to wait for a specific event to occur while not blocking threads or having listeners everywhere.
  *
- * <h2>Example</h2>
- * <h3>This example uses every actions, has a timeout of 1 second and only triggers if the caller is the same as the user who triggered the previously entered command</h3>
- * <pre><code>
- * final{@literal Future<GuildMessageReceivedEvent>} future = EventWaiter.of(GuildMessageReceivedEvent.class)
- * 		.setOnComplete((f, e, t){@literal ->} System.out.println("Completed"))
- * 		.setOnTimeout((){@literal ->} System.err.println("Timeout"))
- * 		.setOnSuccess(e{@literal ->} System.out.println("Success"))
- * 		.setOnCancelled((){@literal ->} System.err.println("Cancelled"))
- * 		.setTimeout(1, TimeUnit.SECONDS)
- * 		.addPrecondition(e{@literal ->} e.getAuthor().getIdLong() == event.getAuthor().getIdLong())
- * 		.submit();
+ * You can set multiple preconditions, timeouts and actions to run when the event gets received / has an exception, etc...
  *
+ * ### Example
+ * This example uses every action, has a timeout of 1 second and only triggers if the caller is the same as the user who triggered the previously entered command
+ *
+ * ```java
+ * final Future<MessageReceivedEvent> future = EventWaiter.of(MessageReceivedEvent.class)
+ *     .setOnComplete((f, evt, throwable) -> System.out.println("Completed with an event " + evt + " or an exception " + throwable))
+ *     .setOnTimeout(() -> System.err.println("Timeout"))
+ *     .setOnSuccess(evt -> System.out.println("Success, received event: " + evt))
+ *     .setOnCancelled(() -> System.err.println("Cancelled"))
+ *     .setTimeout(1, TimeUnit.SECONDS)
+ *     // Here "event" is the original event, such as a MessageReceivedEvent
+ *     .addPrecondition(e -> e.getAuthor().getIdLong() == event.getAuthor().getIdLong())
+ *     .submit();
+ *
+ * // If you want to cancel the event waiter
  * //future.cancel(true);
  *
- * try { //Use this **only** if you need to block your thread waiting for the event, this is considered bad practise so rather use setOnSuccess / setonComplete
- * 	final GuildMessageReceivedEvent guildMessageReceivedEvent = future.get();
+ * // While I recommend to stick to setOnSuccess / setOnComplete,
+ * // you can use this if you need to block while waiting for the event,
+ * // but be aware that this might block a thread indefinitely if you're not careful.
+ * try {
+ *     final MessageReceivedEvent messageReceivedEvent = future.get();
  * } catch (InterruptedException | ExecutionException e) {
- * 	e.printStackTrace();
+ *     LOGGER.error("Error waiting for event", e);
  * }
- * </code></pre>
+ * ```
  */
-public interface EventWaiter { //TODO convert to kt and add await
+@InjectedService
+interface EventWaiter {
     /**
-     * Creates a new event waiter builder, waiting for the specified event to occur
+     * Creates a new event waiter builder, waiting for the specified event to occur.
      *
      * @param eventType The JDA event to wait after
-     * @param <T>       Type of the JDA event
+     * @param T         Type of the JDA event
      *
      * @return A new event waiter builder
      */
-    <T extends Event> EventWaiterBuilder<T> of(@NotNull Class<T> eventType);
+    fun <T : Event> of(eventType: Class<T>): EventWaiterBuilder<T>
 }
+
+suspend fun <T : Event> EventWaiterBuilder<T>.await(): T = submit().await()
