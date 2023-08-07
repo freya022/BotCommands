a365525fd6d77ad931c8031b0816bb4c0fe402ad
freya02 <41875020+freya022@users.noreply.github.com>
2023-08-07 22:55:58 +0200

    Add function to suppress message content intent warning

--- src/main/kotlin/com/freya02/botcommands/api/core/utils/JDA.kt
+++ src/main/kotlin/com/freya02/botcommands/api/core/utils/JDA.kt
@@ -12,6 +12,7 @@ import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback
 import net.dv8tion.jda.api.requests.ErrorResponse
 import net.dv8tion.jda.api.utils.messages.MessageCreateData
 import net.dv8tion.jda.api.utils.messages.MessageEditData
+import net.dv8tion.jda.internal.entities.ReceivedMessage
 
 suspend fun Guild.retrieveMemberOrNull(userId: Long): Member? = retrieveMemberOrNull(UserSnowflake.fromId(userId))
 suspend fun Guild.retrieveMemberOrNull(user: UserSnowflake): Member? = try {
@@ -28,6 +29,22 @@ suspend fun JDA.retrieveUserOrNull(userId: Long): User? = try {
     if (e.errorResponse != ErrorResponse.UNKNOWN_MEMBER)
         throw e
     null
+}
+
+/**
+ * Temporarily suppresses message content intent warnings
+ *
+ * **Note:** This applies to all threads while the current code is inside this function
+ */
+inline fun <R> suppressContentWarning(block: () -> R): R {
+    val oldFlag = ReceivedMessage.didContentIntentWarning
+    ReceivedMessage.didContentIntentWarning = true
+
+    return try {
+        block()
+    } finally {
+        ReceivedMessage.didContentIntentWarning = oldFlag
+    }
 }
 
 /**
