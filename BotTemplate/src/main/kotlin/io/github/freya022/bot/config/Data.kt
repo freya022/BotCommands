ea3abfb99630b389246b13c4d9e5d70e1490d200
freya02 <41875020+freya022@users.noreply.github.com>
2023-08-07 23:51:35 +0200

    BotTemplate - Move user variables into constants

--- BotTemplate/src/main/kotlin/io/github/freya022/bot/config/Data.kt
+++ BotTemplate/src/main/kotlin/io/github/freya022/bot/config/Data.kt
@@ -1,10 +1,22 @@
 package io.github.freya022.bot.config
 
+import java.io.FileNotFoundException
 import java.nio.file.Path
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.notExists
 
 object Data {
     /**
      * Where your bot can write data if needed
      */
     val folder: Path = Environment.folder.resolve(if (Environment.isDev) "dev-data" else "data")
+
+    /**
+     * Checks whether the path exists, throwing if not.
+     */
+    private fun validatedPath(desc: String, p: Path): Path {
+        if (p.notExists())
+            throw FileNotFoundException("$desc at ${p.absolutePathString()} does not exist.")
+        return p
+    }
 }
