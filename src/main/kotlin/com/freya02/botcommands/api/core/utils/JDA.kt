58e321d3e932a2c5e1571b0120726a0a32c8dda9
freya02 <41875020+freya022@users.noreply.github.com>
2023-08-07 23:07:41 +0200

    Add namedDefaultScope to easily create named coroutines scopes

--- src/main/kotlin/com/freya02/botcommands/api/core/utils/JDA.kt
+++ src/main/kotlin/com/freya02/botcommands/api/core/utils/JDA.kt
@@ -1,6 +1,8 @@
 package com.freya02.botcommands.api.core.utils
 
 import dev.minn.jda.ktx.coroutines.await
+import dev.minn.jda.ktx.events.getDefaultScope
+import kotlinx.coroutines.*
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Member
@@ -13,6 +15,11 @@ import net.dv8tion.jda.api.requests.ErrorResponse
 import net.dv8tion.jda.api.utils.messages.MessageCreateData
 import net.dv8tion.jda.api.utils.messages.MessageEditData
 import net.dv8tion.jda.internal.entities.ReceivedMessage
+import java.util.concurrent.Executors
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 suspend fun Guild.retrieveMemberOrNull(userId: Long): Member? = retrieveMemberOrNull(UserSnowflake.fromId(userId))
 suspend fun Guild.retrieveMemberOrNull(user: UserSnowflake): Member? = try {
@@ -45,6 +52,34 @@ inline fun <R> suppressContentWarning(block: () -> R): R {
     } finally {
         ReceivedMessage.didContentIntentWarning = oldFlag
     }
+}
+
+/**
+ * Creates a [CoroutineScope] with incremental thread naming, uses [getDefaultScope] under the hood.
+ *
+ * @param job The parent job used for coroutines which can be used to cancel all children, uses [SupervisorJob] by default
+ * @param errorHandler The [CoroutineExceptionHandler] used for handling uncaught exceptions,
+ *                     uses a logging handler which cancels the parent job on [Error] by default
+ * @param context Any additional context to add to the scope, uses [EmptyCoroutineContext] by default
+ */
+fun namedDefaultScope(
+    name: String,
+    poolSize: Int,
+    job: Job? = null,
+    errorHandler: CoroutineExceptionHandler? = null,
+    context: CoroutineContext = EmptyCoroutineContext
+): CoroutineScope {
+    val lock = ReentrantLock()
+    var count = 0
+    val executor = Executors.newScheduledThreadPool(poolSize) {
+        Thread(it).apply {
+            lock.withLock {
+                this.name = "$name ${++count}"
+            }
+        }
+    }
+
+    return getDefaultScope(pool = executor, context = CoroutineName(name) + context, job = job, errorHandler = errorHandler)
 }
 
 /**
