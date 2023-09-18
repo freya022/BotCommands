f4d4095f14d3afb527fdda337392795162ef5613
freya02 <41875020+freya022@users.noreply.github.com>
2023-09-18 13:07:25 +0200

    Remove deprecated DBResult stuff

--- src/main/kotlin/com/freya02/botcommands/api/core/db/DBResult.kt
+++ src/main/kotlin/com/freya02/botcommands/api/core/db/DBResult.kt
@@ -4,10 +4,6 @@ import java.sql.ResultSet
 import java.sql.SQLException
 import kotlin.reflect.full.isSubclassOf
 
-// TODO remove in alpha 6
-@Deprecated("For removal")
-typealias ResultFunction<R> = (DBResult) -> R
-
 class DBResult internal constructor(resultSet: ResultSet) : Iterable<DBResult>, ResultSet by resultSet {
     override fun iterator(): Iterator<DBResult> = object : Iterator<DBResult> {
         override fun hasNext(): Boolean {
@@ -40,25 +36,13 @@ class DBResult internal constructor(resultSet: ResultSet) : Iterable<DBResult>, 
 
     fun read(): DBResult = readOrNull() ?: throw NoSuchElementException("There are no elements in this result set")
 
-    // TODO remove in alpha 6
-    @Deprecated("Replaced by readOrNull", ReplaceWith("readOrNull()"), DeprecationLevel.ERROR)
-    fun readOnce(): DBResult? = readOrNull()
-
     fun readOrNull(): DBResult? = when {
         !next() -> null
         else -> this
     }
 
-    // TODO remove in alpha 6
-    @Deprecated("Replaced by readOrNull", ReplaceWith("readOrNull(resultFunction)"), DeprecationLevel.ERROR)
-    fun <R> readOnce(resultFunction: (DBResult) -> R): R? = readOrNull(resultFunction)
-
     fun <R> readOrNull(resultFunction: (DBResult) -> R): R? = when {
         !next() -> null
         else -> resultFunction(this)
     }
-
-    // TODO remove in alpha 6
-    @Deprecated("Replaced by Iterable#map", ReplaceWith("map(resultFunction)"), DeprecationLevel.ERROR)
-    fun <R> transformEach(resultFunction: (DBResult) -> R): List<R> = map(resultFunction)
 }
