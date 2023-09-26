6fc28c27fb9877bbe86379dcd2f553a751d5d072
freya02 <41875020+freya022@users.noreply.github.com>
2023-09-26 21:04:31 +0200

    Fix test log inconsistency

--- src/test/kotlin/com/freya02/botcommands/test_kt/listeners/DisabledTextListener.kt
+++ src/test/kotlin/com/freya02/botcommands/test_kt/listeners/DisabledTextListener.kt
@@ -2,6 +2,7 @@ package com.freya02.botcommands.test_kt.listeners
 
 import com.freya02.botcommands.api.core.annotations.BEventListener
 import com.freya02.botcommands.api.core.service.annotations.BService
+import com.freya02.botcommands.api.core.utils.simpleNestedName
 import mu.KotlinLogging
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
@@ -11,6 +12,6 @@ private val logger = KotlinLogging.logger { }
 class DisabledTextListener {
     @BEventListener
     fun onMessage(event: MessageReceivedEvent) {
-        logger.debug( "Received message in DisabledTextListener" )
+        logger.trace("Received message in ${this.javaClass.simpleNestedName}" )
     }
 }
--- src/test/kotlin/com/freya02/botcommands/test_kt/listeners/RequiredIntentsTextListener.kt
+++ src/test/kotlin/com/freya02/botcommands/test_kt/listeners/RequiredIntentsTextListener.kt
@@ -3,6 +3,7 @@ package com.freya02.botcommands.test_kt.listeners
 import com.freya02.botcommands.api.core.annotations.BEventListener
 import com.freya02.botcommands.api.core.conditions.RequiredIntents
 import com.freya02.botcommands.api.core.service.annotations.BService
+import com.freya02.botcommands.api.core.utils.simpleNestedName
 import mu.KotlinLogging
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.requests.GatewayIntent
@@ -14,6 +15,6 @@ private val logger = KotlinLogging.logger { }
 class RequiredIntentsTextListener {
     @BEventListener
     fun onMessage(event: MessageReceivedEvent) {
-        logger.trace( "Received a message" )
+        logger.trace("Received message in ${this.javaClass.simpleNestedName}" )
     }
 }
