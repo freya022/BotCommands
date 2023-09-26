05ff5a95bbad80cf7233b99ba5f026d11ae78f9a
freya02 <41875020+freya022@users.noreply.github.com>
2023-09-26 20:12:02 +0200

    Improve docs

--- src/main/kotlin/com/freya02/botcommands/api/core/utils/ErrorResponses.kt
+++ src/main/kotlin/com/freya02/botcommands/api/core/utils/ErrorResponses.kt
@@ -16,6 +16,9 @@ inline fun <T> Result<T>.onErrorResponseException(block: (ErrorResponseException
  * Runs the function [block] if the result is an [error response][ErrorResponse].
  *
  * This does not clear the exception.
+ *
+ * @see ignore
+ * @see handle
  */
 inline fun <T> Result<T>.onErrorResponse(block: (ErrorResponse) -> Unit): Result<T> {
     return onErrorResponseException { block(it.errorResponse) }
@@ -25,6 +28,9 @@ inline fun <T> Result<T>.onErrorResponse(block: (ErrorResponse) -> Unit): Result
  * Runs the function [block] if the result is the specified [error response][ErrorResponse].
  *
  * This does not clear the exception.
+ *
+ * @see ignore
+ * @see handle
  */
 inline fun <T> Result<T>.onErrorResponse(error: ErrorResponse, block: (ErrorResponseException) -> Unit): Result<T> {
     return onErrorResponseException { if (it.errorResponse == error) block(it) }
@@ -32,7 +38,12 @@ inline fun <T> Result<T>.onErrorResponse(error: ErrorResponse, block: (ErrorResp
 
 /**
  * Dismisses the encapsulated [error response][ErrorResponse]
- * if it corresponds to any of the ignored responses, making the [Result] a success.
+ * if it corresponds to an ignored response, making the [Result] a success.
+ *
+ * @see handle
+ * @see runCatchingResponse
+ * @see runIgnoringResponse
+ * @see runIgnoringResponseOrNull
  */
 fun Result<Unit>.ignore(vararg responses: ErrorResponse): Result<Unit> = recoverCatching {
     if (it is ErrorResponseException && it.errorResponse in responses) {
@@ -45,6 +56,10 @@ fun Result<Unit>.ignore(vararg responses: ErrorResponse): Result<Unit> = recover
 /**
  * Maps the encapsulated [error response][ErrorResponse] using the given function [block]
  * if it corresponds to an ignored response.
+ *
+ * Any thrown exception will be encapsulated in the returned [Result].
+ *
+ * @see ignore
  */
 inline fun <T : R, R> Result<T>.handle(vararg responses: ErrorResponse, block: (ErrorResponseException) -> R): Result<R> = recoverCatching {
     if (it is ErrorResponseException && it.errorResponse in responses) {
@@ -56,14 +71,22 @@ inline fun <T : R, R> Result<T>.handle(vararg responses: ErrorResponse, block: (
 
 /**
  * Encapsulates the result of the specified function [block] and dismisses [error responses][ErrorResponse]
- * that corresponds to any of the ignored responses.
+ * that corresponds to an ignored response, making the [Result] a success.
+ *
+ * @see runIgnoringResponse
+ * @see runIgnoringResponseOrNull
  */
 inline fun runCatchingResponse(vararg ignoredResponses: ErrorResponse, block: () -> Unit): Result<Unit> =
     runCatching(block).ignore(*ignoredResponses)
 
 /**
  * Runs the specified function [block] and dismisses [error responses][ErrorResponse]
- * that corresponds to any of the ignored responses.
+ * that corresponds to an ignored response.
+ *
+ * Any other exception is still thrown
+ *
+ * @see ignore
+ * @see runIgnoringResponseOrNull
  */
 inline fun runIgnoringResponse(vararg ignoredResponses: ErrorResponse, block: () -> Unit) {
     try {
@@ -77,7 +100,10 @@ inline fun runIgnoringResponse(vararg ignoredResponses: ErrorResponse, block: ()
 
 /**
  * Runs the specified function [block] and returns `null` on [error responses][ErrorResponse]
- * that corresponds to any of the ignored responses.
+ * that corresponds to an ignored response.
+ *
+ * @see ignore
+ * @see runIgnoringResponse
  */
 inline fun <R> runIgnoringResponseOrNull(vararg ignoredResponses: ErrorResponse, block: () -> R): R? {
     return try {
