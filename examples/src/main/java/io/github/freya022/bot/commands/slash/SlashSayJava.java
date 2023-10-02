7378349870fd2ec70e71947c495dc9f1faf89b77
freya02 <41875020+freya022@users.noreply.github.com>
2023-10-02 10:46:47 +0200

    Add /say example

---
+++ examples/src/main/java/io/github/freya022/bot/commands/slash/SlashSayJava.java
@@ -0,0 +1,19 @@
+package io.github.freya022.bot.commands.slash;
+
+import com.freya02.botcommands.api.commands.annotations.Command;
+import com.freya02.botcommands.api.commands.application.ApplicationCommand;
+import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent;
+import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand;
+import com.freya02.botcommands.api.commands.application.slash.annotations.SlashOption;
+import io.github.freya022.bot.commands.WikiProfile;
+
+@WikiProfile(WikiProfile.Profile.JAVA)
+// --8<-- [start:say_java]
+@Command
+public class SlashSayJava extends ApplicationCommand {
+    @JDASlashCommand(name = "say", description = "Says something")
+    public void onSlashSay(GuildSlashEvent event, @SlashOption(description = "What to say") String content) {
+        event.reply(content).queue();
+    }
+}
+// --8<-- [end:say_java]
---
+++ examples/src/main/kotlin/io/github/freya022/bot/commands/slash/SlashSay.kt
@@ -0,0 +1,42 @@
+package io.github.freya022.bot.commands.slash
+
+import com.freya02.botcommands.api.commands.annotations.Command
+import com.freya02.botcommands.api.commands.application.ApplicationCommand
+import com.freya02.botcommands.api.commands.application.GlobalApplicationCommandManager
+import com.freya02.botcommands.api.commands.application.annotations.AppDeclaration
+import com.freya02.botcommands.api.commands.application.slash.GuildSlashEvent
+import com.freya02.botcommands.api.commands.application.slash.annotations.JDASlashCommand
+import com.freya02.botcommands.api.commands.application.slash.annotations.SlashOption
+import io.github.freya022.bot.commands.WikiProfile
+
+@WikiProfile(WikiProfile.Profile.KOTLIN)
+// --8<-- [start:say_kotlin]
+@Command
+class SlashSayKotlin : ApplicationCommand() {
+    @JDASlashCommand(name = "say", description = "Says something")
+    fun onSlashSay(event: GuildSlashEvent, @SlashOption(description = "What to say") content: String) {
+        event.reply(content).queue()
+    }
+}
+// --8<-- [end:say_kotlin]
+
+@WikiProfile(WikiProfile.Profile.KOTLIN_DSL)
+// --8<-- [start:say_kotlin_dsl]
+@Command
+class SlashSayKotlinDsl {
+    fun onSlashSay(event: GuildSlashEvent, content: String) {
+        event.reply(content).queue()
+    }
+
+    @AppDeclaration
+    fun declare(manager: GlobalApplicationCommandManager) {
+        manager.slashCommand("say", function = ::onSlashSay) {
+            description = "Says something"
+
+            option("content") {
+                description = "What to say"
+            }
+        }
+    }
+}
+// --8<-- [end:say_kotlin_dsl]
