6e4d98fe8ceb6b3f676ff15fc991111b88cece68
freya02 <41875020+freya022@users.noreply.github.com>
2023-10-08 21:08:50 +0200

    Use write lock instead of a synchronized map when setting live commands

--- src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/ApplicationCommandsContextImpl.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/ApplicationCommandsContextImpl.kt
@@ -1,6 +1,5 @@
 package io.github.freya022.botcommands.internal.commands.application
 
-import gnu.trove.TCollections
 import gnu.trove.map.hash.TLongObjectHashMap
 import io.github.freya022.botcommands.api.commands.CommandPath
 import io.github.freya022.botcommands.api.commands.application.ApplicationCommandMap
@@ -14,10 +13,13 @@ import io.github.freya022.botcommands.internal.core.BContextImpl
 import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.entities.Guild
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 //TODO internal
 class ApplicationCommandsContextImpl internal constructor(private val context: BContextImpl) : ApplicationCommandsContext {
-    private val liveApplicationCommandInfoMap = TCollections.synchronizedMap(TLongObjectHashMap<ApplicationCommandMap>())
+    private val writeLock = ReentrantLock()
+    private val liveApplicationCommandInfoMap = TLongObjectHashMap<ApplicationCommandMap>()
 
     override fun findLiveSlashCommand(guild: Guild?, path: CommandPath): SlashCommandInfo? =
         getLiveApplicationCommandsMap(guild).findSlashCommand(path)
@@ -40,7 +42,7 @@ class ApplicationCommandsContextImpl internal constructor(private val context: B
         else -> getLiveApplicationCommandsMap(null) + getLiveApplicationCommandsMap(guild)
     }
 
-    fun putLiveApplicationCommandsMap(guild: Guild?, map: ApplicationCommandMap) {
+    fun putLiveApplicationCommandsMap(guild: Guild?, map: ApplicationCommandMap): Unit = writeLock.withLock {
         liveApplicationCommandInfoMap.put(getGuildKey(guild), map)
     }
 
