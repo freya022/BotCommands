1bde0a7e7a62b86f3cfeba9f379c186cf2c7f959
freya02 <41875020+freya022@users.noreply.github.com>
2023-10-19 10:29:55 +0200

    Test singleton list argument using slash parameter resolver

--- src/test/kotlin/io/github/freya022/botcommands/test/commands/slash/SlashDynamicTypedResolver.kt
+++ src/test/kotlin/io/github/freya022/botcommands/test/commands/slash/SlashDynamicTypedResolver.kt
@@ -6,12 +6,21 @@ import io.github.freya022.botcommands.api.commands.annotations.Command
 import io.github.freya022.botcommands.api.commands.application.ApplicationCommand
 import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashEvent
 import io.github.freya022.botcommands.api.commands.application.slash.annotations.JDASlashCommand
+import io.github.freya022.botcommands.api.commands.application.slash.annotations.SlashOption
 
 @Command
 class SlashDynamicTypedResolver : ApplicationCommand() {
-    // The point is this shouldn't work for any other type of Map, see MapResolverFactory, StringDoubleMapResolver
     @JDASlashCommand(name = "dynamic_typed_resolver")
-    suspend fun onSlashDynamicTypedResolver(event: GuildSlashEvent, map: Map<String, Double>) {
-        event.reply_("$map", ephemeral = true).await()
+    suspend fun onSlashDynamicTypedResolver(
+        event: GuildSlashEvent,
+        // The point is this shouldn't work for any other type of Map, see MapResolverFactory, StringDoubleMapResolver
+        map: Map<String, Double>,
+        // This one gets transformed from a single NUMBER option into a singleton list
+        @SlashOption list: List<Double>
+    ) {
+        event.reply_("""
+            map: $map
+            list: $list
+            """.trimIndent(), ephemeral = true).await()
     }
 }
---
+++ src/test/kotlin/io/github/freya022/botcommands/test/resolvers/ListResolver.kt
@@ -0,0 +1,30 @@
+package io.github.freya022.botcommands.test.resolvers
+
+import io.github.freya022.botcommands.api.core.service.annotations.ResolverFactory
+import io.github.freya022.botcommands.api.parameters.ParameterWrapper
+import io.github.freya022.botcommands.api.parameters.SlashParameterResolver
+import io.github.freya022.botcommands.api.parameters.TypedParameterResolver
+import io.github.freya022.botcommands.api.parameters.TypedParameterResolverFactory
+import io.github.freya022.botcommands.internal.commands.application.slash.SlashCommandInfo
+import net.dv8tion.jda.api.interactions.commands.CommandInteractionPayload
+import net.dv8tion.jda.api.interactions.commands.OptionMapping
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import kotlin.reflect.typeOf
+
+object ListResolver :
+    TypedParameterResolver<ListResolver, List<Double>>(typeOf<List<Double>>()),
+    SlashParameterResolver<ListResolver, List<Double>> {
+
+    override val optionType: OptionType = OptionType.NUMBER
+
+    override suspend fun resolveSuspend(
+        info: SlashCommandInfo,
+        event: CommandInteractionPayload,
+        optionMapping: OptionMapping
+    ): List<Double> = listOf(optionMapping.asDouble * 2)
+}
+
+@ResolverFactory
+object ListResolverFactory : TypedParameterResolverFactory<ListResolver>(ListResolver::class, typeOf<List<Double>>()) {
+    override fun get(parameter: ParameterWrapper): ListResolver = ListResolver
+}
