a8daa4adf7a9d16feb666c8ab9acf0a18ed593e3
freya02 <41875020+freya022@users.noreply.github.com>
2023-10-25 13:01:20 +0200

    Add description to filters

--- src/main/kotlin/io/github/freya022/botcommands/api/core/Filter.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/Filter.kt
@@ -1,7 +1,11 @@
 package io.github.freya022.botcommands.api.core
 
+import io.github.freya022.botcommands.api.core.utils.simpleNestedName
+
 interface Filter {
     val global: Boolean get() = true
+
+    val description: String get() = this.javaClass.simpleNestedName
 }
 
 internal inline fun <T : Filter> checkFilters(globalFilters: List<T>, commandFilters: List<T>, block: (filter: T) -> Unit) {
