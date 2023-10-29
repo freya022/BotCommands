330e5dac6fb21986ab00041db2f7688fb37ef146
freya02 <41875020+freya022@users.noreply.github.com>
2023-10-29 10:49:51 +0100

    Rework supported parameter resolver documentation using tables

--- src/main/kotlin/io/github/freya022/botcommands/api/parameters/ParameterResolver.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/parameters/ParameterResolver.kt
@@ -3,33 +3,37 @@ package io.github.freya022.botcommands.api.parameters
 import io.github.freya022.botcommands.api.core.entities.InputUser
 import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService
 import io.github.freya022.botcommands.api.parameters.resolvers.*
-import net.dv8tion.jda.api.entities.Member
-import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.entities.Role
-import net.dv8tion.jda.api.entities.User
+import net.dv8tion.jda.api.entities.*
+import net.dv8tion.jda.api.entities.Message.Attachment
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel
 import net.dv8tion.jda.api.entities.emoji.Emoji
 
+// NOTE: you can edit the built-in resolvers docs using https://www.tablesgenerator.com/markdown_tables
+// see the File > Paste table data
 /**
  * Base class for parameter resolvers used in text commands, application commands, and component callbacks.
  *
  * You need to extend [ClassParameterResolver] or [TypedParameterResolver] instead.
  *
  * ### Default parameter resolvers
- * - [String]
  *
- * - [Boolean]
- * - [Long]
- * - [Double]
- *
- * - [Emoji]
- *
- * - [Role]
- * - [User]
- * - [Member]
- * - [InputUser]
- * - [All guild channels subtypes][GuildChannel]
- * - [Message] (only message context commands)
+ * |                                                                 | Text              | Slash           | Message context    | User context    | Components | Modals |
+ * |-----------------------------------------------------------------|-------------------|-----------------|--------------------|-----------------|------------|--------|
+ * | [String]                                                        | ✓ (can be quoted) | ✓               |                    |                 | ✓          | ✓      |
+ * | [Boolean]                                                       | ✓                 | ✓               |                    |                 | ✓          |        |
+ * | [Int]                                                           | ✓                 | ✓               |                    |                 | ✓          |        |
+ * | [Long]                                                          | ✓                 | ✓               |                    |                 | ✓          |        |
+ * | [Double]                                                        | ✓                 | ✓               |                    |                 | ✓          |        |
+ * | [Emoji]                                                         | ✓                 | ✓               |                    |                 | ✓          |        |
+ * | [IMentionable]                                                  | ✓ (only mentions) | ✓               |                    |                 |            |        |
+ * | [Role]                                                          | ✓                 | ✓               |                    |                 | ✓          |        |
+ * | [User]                                                          | ✓                 | ✓               |                    | ✓ (target user) | ✓          |        |
+ * | [Member]                                                        | ✓                 | ✓               |                    | ✓ (target user) | ✓          |        |
+ * | [InputUser]                                                     | ✓                 | ✓               |                    | ✓ (target user) | ✓          |        |
+ * | [All guild channels subtypes (includes Category)][GuildChannel] | ✓                 | ✓               |                    |                 | ✓          |        |
+ * | [Guild]                                                         | ✓                 | ✓ (as a String) |                    |                 | ✓          |        |
+ * | [Message]                                                       |                   |                 | ✓ (target message) |                 |            |        |
+ * | [Attachment]                                                    |                   | ✓               |                    |                 |            |        |
  *
  * You can also check loaded parameter resolvers in the logs on the `trace` level.
  *
