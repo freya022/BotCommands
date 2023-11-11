eb573bcfeb9b5425c3c910f730ae129e994c7ee7
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-11 11:08:58 +0100

    Update EmojiUtils

--- src/main/java/io/github/freya022/botcommands/api/utils/EmojiUtils.java
+++ src/main/java/io/github/freya022/botcommands/api/utils/EmojiUtils.java
@@ -1,30 +1,30 @@
 package io.github.freya022.botcommands.api.utils;
 
-import com.vdurmont.emoji.EmojiManager;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.fellbaum.jemoji.EmojiManager;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.NoSuchElementException;
 
 public class EmojiUtils {
 	/**
-	 * Resolves a shortcode/alias emoji (e.g: :joy:) into a unicode emoji for JDA to use (on reactions for example)
+	 * Resolves a shortcode/alias emoji (e.g. :joy:) into an unicode emoji for JDA to use (on reactions, for example)
 	 * <br>This will return itself if the input was a valid unicode emoji
 	 *
 	 * @param input An emoji shortcode
 	 * @return The unicode string of this emoji
 	 */
 	@NotNull
-	public static String resolveEmojis(String input) {
-		com.vdurmont.emoji.Emoji emoji = EmojiManager.getForAlias(input);
+	public static String resolveEmoji(@NotNull String input) {
+		var emoji = EmojiManager.getByDiscordAlias(input);
 
-		if (emoji == null) emoji = EmojiManager.getByUnicode(input);
-		if (emoji == null) throw new NoSuchElementException("No emoji for input: " + input);
-		return emoji.getUnicode();
+		if (emoji.isEmpty()) emoji = EmojiManager.getEmoji(input);
+		if (emoji.isEmpty()) throw new NoSuchElementException("No emoji for input: " + input);
+		return emoji.get().getUnicode();
 	}
 
 	@NotNull
-	public static Emoji resolveJDAEmoji(String input) {
-		return Emoji.fromUnicode(resolveEmojis(input));
+	public static Emoji resolveJDAEmoji(@NotNull String input) {
+		return Emoji.fromUnicode(resolveEmoji(input));
 	}
 }
