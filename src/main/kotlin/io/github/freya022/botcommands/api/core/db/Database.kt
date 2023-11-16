85ee085fa66fb030531c5114c5d900d949f9838d
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-16 18:32:48 +0100

    Do not use KPreparedStatement for BlockingDatabase

--- src/main/kotlin/io/github/freya022/botcommands/api/core/db/Database.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/db/Database.kt
@@ -60,10 +60,15 @@ internal fun Database.fetchConnectionJava(readOnly: Boolean = false): Connection
 internal fun <R> Database.withTransactionJava(readOnly: Boolean = false, block: TransactionFunction<R, *>): R =
     runBlocking { transactional(readOnly) { block.apply(connection) } }
 
+@Suppress("SqlSourceToSinkFlow")
 @JvmOverloads
 @Throws(SQLException::class)
 internal fun <R> Database.withStatementJava(sql: String, readOnly: Boolean = false, block: StatementFunction<R, *>): R =
-    runBlocking { preparedStatement(sql, readOnly) { block.apply(connection) } }
+    runBlocking {
+        fetchConnection(readOnly).use { connection ->
+            connection.prepareStatement(sql).use { block.apply(it) }
+        }
+    }
 
 @PublishedApi
 internal val dbLeakScope = namedDefaultScope("Connection leak watcher", 1)
