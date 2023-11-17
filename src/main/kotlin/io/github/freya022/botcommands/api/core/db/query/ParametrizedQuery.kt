9f27f8207faa30fc114567ff16140ffbfb3f871a
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-17 21:46:47 +0100

    Document ParametrizedQuery(Factory)

--- src/main/kotlin/io/github/freya022/botcommands/api/core/db/query/ParametrizedQuery.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/db/query/ParametrizedQuery.kt
@@ -1,8 +1,28 @@
 package io.github.freya022.botcommands.api.core.db.query
 
+/**
+ * Represents an SQL query with parameters.
+ *
+ * This is used for logging purposes.
+ *
+ * @see ParametrizedQueryFactory
+ */
 interface ParametrizedQuery {
+    /**
+     * Removes all parameters.
+     */
     fun clear()
+
+    /**
+     * Adds a parameter to the query.
+     *
+     * @param index the index of the parameter, starts at 1
+     * @param value `null`-able value of the parameter
+     */
     fun addValue(index: Int, value: Any?)
 
+    /**
+     * Constructs the SQL query out of the previously registered parameters.
+     */
     fun toSql(): String
 }
--- src/main/kotlin/io/github/freya022/botcommands/api/core/db/query/ParametrizedQueryFactory.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/db/query/ParametrizedQueryFactory.kt
@@ -1,12 +1,44 @@
 package io.github.freya022.botcommands.api.core.db.query
 
+import io.github.freya022.botcommands.api.core.config.BConfig
+import io.github.freya022.botcommands.api.core.config.BServiceConfigBuilder
+import io.github.freya022.botcommands.api.core.service.annotations.BService
 import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService
 import java.sql.Connection
+import java.sql.DatabaseMetaData
 import java.sql.PreparedStatement
 
+/**
+ * Factory for [ParametrizedQuery].
+ *
+ * A parametrized query is only used if all of these conditions are met:
+ * - [BConfig.logQueries] is enabled **OR** [BConfig.queryLogThreshold] is configured
+ * - The logger of the class that created the prepared statement has its TRACE logs enabled
+ *
+ * ### Usage
+ * Register your instance as a service with [BService]
+ * or [any annotation that enables your class for dependency injection][BServiceConfigBuilder.serviceAnnotations].
+ *
+ * ### Built-in RDBMS support
+ * - PostgreSQL
+ *
+ * If no compatible factory is found, a generic parametrized query is used.
+ *
+ * @see ParametrizedQuery
+ * @see InterfacedService @InterfacedService
+ */
 @InterfacedService(acceptMultiple = true)
 interface ParametrizedQueryFactory<R : ParametrizedQuery> {
+    /**
+     * Returns whether this connection is supporter by this parametrized query factory.
+     *
+     * @param connection          the database connection
+     * @param databaseProductName the [database product name][DatabaseMetaData.getDatabaseProductName]
+     */
     fun isSupported(connection: Connection, databaseProductName: String): Boolean
 
+    /**
+     * Returns a [ParametrizedQuery] for this statement.
+     */
     fun get(preparedStatement: PreparedStatement, sql: String): R
 }
