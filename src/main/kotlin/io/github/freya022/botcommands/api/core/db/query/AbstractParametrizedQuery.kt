63bdad2449cc16e1e31c133f16a3f7b878379258
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-18 21:10:09 +0100

    Use ISO formats in AbstractParametrizedQuery#formatParameter

--- src/main/kotlin/io/github/freya022/botcommands/api/core/db/query/AbstractParametrizedQuery.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/db/query/AbstractParametrizedQuery.kt
@@ -1,9 +1,12 @@
 package io.github.freya022.botcommands.api.core.db.query
 
 import java.sql.PreparedStatement
-import java.text.SimpleDateFormat
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.sql.Date as SqlDate
+import java.sql.Time as SqlTime
+import java.sql.Timestamp as SqlTimestamp
 
-private val dateFormat = SimpleDateFormat("MM/dd/yyyy HH:mm:ss.SSS")
 private val commentRegex = Regex("""--(?!.* ')""")
 
 abstract class AbstractParametrizedQuery(protected val preparedStatement: PreparedStatement) : ParametrizedQuery {
@@ -13,7 +16,14 @@ abstract class AbstractParametrizedQuery(protected val preparedStatement: Prepar
             is String -> preparedStatement.enquoteLiteral(value)
             is Number -> value.toString()
             is Boolean -> value.toString()
-            is java.util.Date -> dateFormat.format(value)
+            is SqlDate -> DateTimeFormatter.ISO_LOCAL_DATE.format(value.toLocalDate())
+            is SqlTimestamp -> DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(value.toLocalDateTime())
+            is SqlTime -> DateTimeFormatter.ISO_LOCAL_TIME.format(value.toLocalTime())
+            is LocalTime -> DateTimeFormatter.ISO_LOCAL_TIME.format(value)
+            is LocalDate -> DateTimeFormatter.ISO_LOCAL_DATE.format(value)
+            is OffsetTime -> DateTimeFormatter.ISO_OFFSET_TIME.format(value)
+            is LocalDateTime -> DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(value)
+            is OffsetDateTime -> DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(value)
             else -> value.toString()
         }
     }
