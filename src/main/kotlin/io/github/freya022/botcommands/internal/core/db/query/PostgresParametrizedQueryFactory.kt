40b498254b668368dc93017d34ea89f6cb0f6e80
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-19 14:34:43 +0100

    Use ServiceStart.LAZY on PostgresParametrizedQueryFactory

--- src/main/kotlin/io/github/freya022/botcommands/internal/core/db/query/PostgresParametrizedQueryFactory.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/core/db/query/PostgresParametrizedQueryFactory.kt
@@ -2,12 +2,13 @@ package io.github.freya022.botcommands.internal.core.db.query
 
 import io.github.freya022.botcommands.api.core.db.query.ParametrizedQuery
 import io.github.freya022.botcommands.api.core.db.query.ParametrizedQueryFactory
+import io.github.freya022.botcommands.api.core.service.ServiceStart
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import java.sql.Connection
 import java.sql.DatabaseMetaData
 import java.sql.PreparedStatement
 
-@BService
+@BService(start = ServiceStart.LAZY)
 internal object PostgresParametrizedQueryFactory : ParametrizedQueryFactory<PostgresParametrizedQueryFactory.PostgresParametrizedQuery> {
     internal class PostgresParametrizedQuery internal constructor(private val preparedStatement: PreparedStatement) : ParametrizedQuery {
         override fun clear() { }
