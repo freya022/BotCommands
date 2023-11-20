7587b5da0db31939e8642951c032ee9d422a20e3
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-20 09:38:11 +0100

    Fix CHANGELOG.md links

--- examples/src/main/kotlin/io/github/freya022/wiki/switches/WikiLanguage.kt
+++ examples/src/main/kotlin/io/github/freya022/wiki/switches/WikiLanguage.kt
@@ -4,6 +4,7 @@ import io.github.freya022.botcommands.api.core.BContext
 import io.github.freya022.botcommands.api.core.service.CustomConditionChecker
 import io.github.freya022.botcommands.api.core.service.annotations.Condition
 
+// Singleton which checks if a service annotated with @WikiLanguage can be instantiated
 object WikiLanguageChecker : CustomConditionChecker<WikiLanguage> {
     // NOTE: When changing wiki source code, the wiki downloads snippets at build time,
     // and must be rebuilt for the changes to be taken into account
@@ -22,6 +23,8 @@ object WikiLanguageChecker : CustomConditionChecker<WikiLanguage> {
 }
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER)
+// Custom annotation which acts like a conditional service annotation,
+// The checks are made with WikiLanguageChecker
 @Condition(WikiLanguageChecker::class, fail = false)
 annotation class WikiLanguage(@get:JvmName("value") val language: Language) {
     enum class Language {
