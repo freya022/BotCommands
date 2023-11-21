c5feb9f49a7456b1f16fb89909d4b93bc6094803
freya02 <41875020+freya022@users.noreply.github.com>
2023-11-21 09:40:49 +0100

    Fix rebase

--- src/main/kotlin/io/github/freya022/botcommands/api/components/builder/PersistentHandlerBuilder.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/components/builder/PersistentHandlerBuilder.kt
@@ -1,6 +1,6 @@
 package io.github.freya022.botcommands.api.components.builder
 
-import io.github.freya022.botcommands.api.parameters.ComponentParameterResolver
+import io.github.freya022.botcommands.api.parameters.resolvers.ComponentParameterResolver
 import io.github.freya022.botcommands.internal.components.ComponentDSL
 import io.github.freya022.botcommands.internal.components.PersistentHandler
 import net.dv8tion.jda.api.entities.ISnowflake
