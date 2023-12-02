46548c67db8819b3171452135c10da6242869afd
freya02 <41875020+freya022@users.noreply.github.com>
2023-12-02 21:29:12 +0100

    Replace emoji-java with JEmoji (#132)
    * Replace emoji-java with JEmojis, remove org.json
    * Add nullabilities to RichTextParser
    * Update jackson-databind
    
    NOTE: Regional indicators are not recognized by JEmojis yet: https://github.com/felldo/JEmoji/issues/44

--- src/main/java/io/github/freya022/botcommands/api/utils/RichTextFinder.java
+++ src/main/java/io/github/freya022/botcommands/api/utils/RichTextFinder.java
@@ -1,11 +1,11 @@
 package io.github.freya022.botcommands.api.utils;
 
-import com.vdurmont.emoji.EmojiParser;
+import net.fellbaum.jemoji.Emoji;
+import net.fellbaum.jemoji.EmojiManager;
+import net.fellbaum.jemoji.IndexedEmoji;
+import org.jspecify.annotations.NullMarked;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,186 +21,184 @@ import java.util.regex.Pattern;
  *     <li>{@code @here} and {@code @everyone} mentions</li>
  *     <li>URLs</li>
  * </ul>
- *
- * This class takes your input and tokenises it as it finds what you're asking it to find.
+ * <p>
+ * This class takes your input and tokenizes it as it finds what you're asking it to find.
  * <p>
  * You can then take the output using {@link #getResults()} or consume it directly using {@link #processResults(RichTextConsumer)}.
  */
-public class RichTextFinder extends EmojiParser {
-	private static final Pattern urlPattern = Pattern.compile("https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
-	private static final Pattern EMPTY_PATTERN = Pattern.compile("");
+@NullMarked
+public class RichTextFinder {
+    private static final Pattern URL_PATTERN = Pattern.compile("https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+    private static final Pattern EMPTY_PATTERN = Pattern.compile("");
 
-	private final String input;
-	private final Matcher matcher;
-	private final Map<Integer, RichText> normalMentionMap = new TreeMap<>();
-	private final Map<Integer, String> addedStrs = new TreeMap<>();
+    private final String input;
+    private final Matcher matcher;
+    private final Map<Integer, RichText> normalMentionMap = new TreeMap<>();
+    private final Map<Integer, String> addedStrs = new TreeMap<>();
 
-	/**
-	 * Parses the input for what you're asking
-	 *
-	 * @param input The input to parse
-	 * @param getIMentionable Whether to take Users/Channels/Emotes/Roles
-	 * @param getGlobalMentions Whether to take {@code @here} and {@code @everyone} mentions
-	 * @param getEmojis Whether to take Unicode/shortcode emojis
-	 * @param getUrls Whether to take URLs
-	 */
-	public RichTextFinder(String input, boolean getIMentionable, boolean getGlobalMentions, boolean getEmojis, boolean getUrls) {
-		this.input = input.replace("\uFE0F", "");
-		this.matcher = EMPTY_PATTERN.matcher(this.input);
+    /**
+     * Parses the input for what you're asking
+     *
+     * @param input             The input to parse
+     * @param getIMentionable   Whether to take Users/Channels/Emotes/Roles
+     * @param getGlobalMentions Whether to take {@code @here} and {@code @everyone} mentions
+     * @param getEmojis         Whether to take Unicode/shortcode emojis
+     * @param getUrls           Whether to take URLs
+     */
+    public RichTextFinder(String input, boolean getIMentionable, boolean getGlobalMentions, boolean getEmojis, boolean getUrls) {
+        this.input = input.replace("\uFE0F", "");
+        this.matcher = EMPTY_PATTERN.matcher(this.input);
 
-		if (getIMentionable) {
-			findAllMentions(RichTextType.USER, RichTextType.USER.getPattern());
-			findAllMentions(RichTextType.CHANNEL, RichTextType.CHANNEL.getPattern());
-			findAllMentions(RichTextType.EMOJI, RichTextType.EMOJI.getPattern());
-			findAllMentions(RichTextType.ROLE, RichTextType.ROLE.getPattern());
-		}
+        if (getIMentionable) {
+            findAllMentions(RichTextType.USER, RichTextType.USER.getPattern());
+            findAllMentions(RichTextType.CHANNEL, RichTextType.CHANNEL.getPattern());
+            findAllMentions(RichTextType.EMOJI, RichTextType.EMOJI.getPattern());
+            findAllMentions(RichTextType.ROLE, RichTextType.ROLE.getPattern());
+        }
 
-		if (getGlobalMentions) {
-			findAllMentions(RichTextType.HERE, RichTextType.HERE.getPattern());
-			findAllMentions(RichTextType.EVERYONE, RichTextType.EVERYONE.getPattern());
-		}
+        if (getGlobalMentions) {
+            findAllMentions(RichTextType.HERE, RichTextType.HERE.getPattern());
+            findAllMentions(RichTextType.EVERYONE, RichTextType.EVERYONE.getPattern());
+        }
 
-		if (getEmojis) {
-			resolveEmojis();
-		}
+        if (getEmojis) {
+            extractAliasedEmojis();
+            extractUnicodeEmojis();
+        }
 
-		if (getUrls) {
-			findAllMentions(RichTextType.URL, urlPattern);
-		}
+        if (getUrls) {
+            findAllMentions(RichTextType.URL, URL_PATTERN);
+        }
 
-		if (!addedStrs.isEmpty()) {
-			Iterator<String> it = addedStrs.values().iterator();
-			String next = it.next();
-			int startIndex = 0;
-			int endIndex = input.indexOf(next, startIndex);
-			final String startSubstring = this.input.substring(startIndex, endIndex).trim();
-			if (!startSubstring.isEmpty()) {
-				normalMentionMap.put(startIndex, new RichText(startSubstring, RichTextType.TEXT));
-			}
+        if (!addedStrs.isEmpty()) {
+            Iterator<String> it = addedStrs.values().iterator();
+            String next = it.next();
+            int startIndex = 0;
+            int endIndex = input.indexOf(next, startIndex);
+            final String startSubstring = this.input.substring(startIndex, endIndex).trim();
+            if (!startSubstring.isEmpty()) {
+                normalMentionMap.put(startIndex, new RichText(startSubstring, RichTextType.TEXT));
+            }
 
-			while (it.hasNext()) {
-				startIndex = endIndex + next.length();
+            while (it.hasNext()) {
+                startIndex = endIndex + next.length();
 
-				next = it.next();
-				endIndex = input.indexOf(next, startIndex);
+                next = it.next();
+                endIndex = input.indexOf(next, startIndex);
 
-				final String middleSubstring = this.input.substring(startIndex, endIndex).trim();
-				if (!middleSubstring.isEmpty()) {
-					normalMentionMap.put(startIndex, new RichText(middleSubstring, RichTextType.TEXT));
-				}
-			}
+                final String middleSubstring = this.input.substring(startIndex, endIndex).trim();
+                if (!middleSubstring.isEmpty()) {
+                    normalMentionMap.put(startIndex, new RichText(middleSubstring, RichTextType.TEXT));
+                }
+            }
 
-			startIndex = endIndex + next.length();
-			final String endSubstring = this.input.substring(startIndex).trim();
-			if (!endSubstring.isEmpty()) {
-				normalMentionMap.put(startIndex, new RichText(endSubstring, RichTextType.TEXT));
-			}
+            startIndex = endIndex + next.length();
+            final String endSubstring = this.input.substring(startIndex).trim();
+            if (!endSubstring.isEmpty()) {
+                normalMentionMap.put(startIndex, new RichText(endSubstring, RichTextType.TEXT));
+            }
 
-			addedStrs.clear();
-		} else {
-			normalMentionMap.put(0, new RichText(this.input, RichTextType.TEXT));
-		}
-	}
+            addedStrs.clear();
+        } else {
+            normalMentionMap.put(0, new RichText(this.input, RichTextType.TEXT));
+        }
+    }
 
-	/**
-	 * @return The tokens parsed as rich text
-	 */
-	public List<RichText> getResults() {
-		return List.copyOf(normalMentionMap.values());
-	}
+    /**
+     * @return The tokens parsed as rich text
+     */
+    public List<RichText> getResults() {
+        return List.copyOf(normalMentionMap.values());
+    }
 
-	private void resolveEmojis() {
-		//Find emoji aliases
-		final int inputLength = input.length();
-		for (int last = 0; last < inputLength; last++) {
-			AliasCandidate alias = getAliasAt(input, last);
+    private void extractAliasedEmojis() {
+        int aliasBegin = 0;
+        while ((aliasBegin = input.indexOf(':', aliasBegin)) >= 0) {
+            int aliasEnd = input.indexOf(':', aliasBegin + 2);  // Alias must be at least 1 char in length
+            if (aliasEnd == -1) {
+                aliasBegin += 1; // Do not find back the same alias
+                continue; // No alias end found
+            }
 
-			if (alias != null) {
-				last = alias.endIndex;
+            final Optional<Emoji> optEmoji = EmojiManager.getByDiscordAlias(input.substring(aliasBegin, aliasEnd + 1));
+            if (optEmoji.isEmpty()) {
+                aliasBegin += 1; // Do not find back the same alias
+                continue;
+            }
 
-				final String substring;
-				if (alias.fitzpatrick != null) {
-					substring = alias.emoji.getUnicode() + alias.fitzpatrick.unicode;
-				} else {
-					substring = alias.emoji.getUnicode();
-				}
+            final Emoji emoji = optEmoji.get();
+            if (!isInCustomEmote(emoji)) {
+                normalMentionMap.put(aliasBegin, new RichText(emoji.getUnicode(), RichTextType.UNICODE_EMOTE));
+                addedStrs.put(aliasBegin, input.substring(aliasBegin, aliasEnd + 1));
+            }
 
-				final int beginIndex = last;
+            aliasBegin += 1; // Do not find back the same alias
+        }
+    }
 
-				final boolean isInsideCustomEmote = normalMentionMap.values().stream().filter(r -> r.type == RichTextType.EMOJI).anyMatch(r -> {
-					final List<String> aliases = alias.emoji.getAliases();
-					for (String aliasItem : aliases) {
-						final boolean customEmoteHasEmoji = r.substring.startsWith(':' + aliasItem + ':', 1);
+    private boolean isInCustomEmote(Emoji emoji) {
+        for (RichText richText : normalMentionMap.values()) {
+            if (richText.type != RichTextType.EMOJI) continue;
 
-						if (customEmoteHasEmoji) {
-							return true;
-						}
-					}
+            for (String aliasItem : emoji.getDiscordAliases()) {
+                final boolean customEmoteHasEmoji = richText.substring.startsWith(aliasItem, 1);
 
-					return false;
-				});
+                if (customEmoteHasEmoji) {
+                    return true;
+                }
+            }
+        }
 
-				if (!isInsideCustomEmote) {
-//					System.out.println("added emoji " + substring);
-					normalMentionMap.put(beginIndex, new RichText(substring, RichTextType.UNICODE_EMOTE));
-					addedStrs.put(beginIndex, input.substring(alias.startIndex, alias.endIndex + 1));
-				}
-			}
-		}
+        return false;
+    }
 
-		//Find unicode emojis
-		char[] inputCharArray = input.toCharArray();
-		UnicodeCandidate next;
-		for (int i = 0; (next = getNextUnicodeCandidate(inputCharArray, i)) != null; i = next.getFitzpatrickEndIndex()) {
-			if (next.hasFitzpatrick()) {
-				normalMentionMap.put(next.getEmojiStartIndex(), new RichText(next.getEmoji().getUnicode(next.getFitzpatrick()), RichTextType.UNICODE_EMOTE));
-				addedStrs.put(next.getEmojiStartIndex(), next.getEmoji().getUnicode(next.getFitzpatrick()));
-			} else {
-				normalMentionMap.put(next.getEmojiStartIndex(), new RichText(next.getEmoji().getUnicode(), RichTextType.UNICODE_EMOTE));
-				addedStrs.put(next.getEmojiStartIndex(), next.getEmoji().getUnicode());
-			}
-		}
-	}
+    private void extractUnicodeEmojis() {
+        for (IndexedEmoji indexedEmoji : EmojiManager.extractEmojisInOrderWithIndex(input)) {
+            normalMentionMap.put(indexedEmoji.getCharIndex(), new RichText(indexedEmoji.getEmoji().getUnicode(), RichTextType.UNICODE_EMOTE));
+            addedStrs.put(indexedEmoji.getCharIndex(), indexedEmoji.getEmoji().getUnicode());
+        }
+    }
 
-	private void findAllMentions(RichTextType type, Pattern pattern) {
-		matcher.usePattern(pattern);
-		matcher.reset();
-		while (matcher.find()) {
-			final String group = matcher.group();
-			normalMentionMap.put(matcher.start(), new RichText(group, type));
-			addedStrs.put(matcher.start(), group);
-		}
-	}
+    private void findAllMentions(RichTextType type, Pattern pattern) {
+        matcher.usePattern(pattern);
+        matcher.reset();
+        while (matcher.find()) {
+            final String group = matcher.group();
+            normalMentionMap.put(matcher.start(), new RichText(group, type));
+            addedStrs.put(matcher.start(), group);
+        }
+    }
 
-	/**
-	 * Processes each rich text token
-	 * @param consumer The consumer accepting a substring and a rich text type
-	 */
-	public void processResults(RichTextConsumer consumer) {
-		for (RichText richText : normalMentionMap.values()) {
-			consumer.consume(richText.substring, richText.type);
-		}
-	}
+    /**
+     * Processes each rich text token
+     *
+     * @param consumer The consumer accepting a substring and a rich text type
+     */
+    public void processResults(RichTextConsumer consumer) {
+        for (RichText richText : normalMentionMap.values()) {
+            consumer.consume(richText.substring, richText.type);
+        }
+    }
 
-	public Map<Integer, RichText> getNormalMentionMap() {
-		return normalMentionMap;
-	}
+    public Map<Integer, RichText> getNormalMentionMap() {
+        return normalMentionMap;
+    }
 
-	public static class RichText {
-		private final String substring;
-		private final RichTextType type;
+    public static class RichText {
+        private final String substring;
+        private final RichTextType type;
 
-		RichText(String substring, RichTextType type) {
-			this.substring = substring;
-			this.type = type;
-		}
+        RichText(String substring, RichTextType type) {
+            this.substring = substring;
+            this.type = type;
+        }
 
-		public String getSubstring() {
-			return substring;
-		}
+        public String getSubstring() {
+            return substring;
+        }
 
-		public RichTextType getType() {
-			return type;
-		}
-	}
+        public RichTextType getType() {
+            return type;
+        }
+    }
 }
