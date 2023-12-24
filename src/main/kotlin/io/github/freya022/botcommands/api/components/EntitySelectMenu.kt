e0c711129d147c60f7e3b1581c2675e0c68c607e
freya02 <41875020+freya022@users.noreply.github.com>
2023-12-24 19:00:35 +0100

    Add equals, hashCode and toString to components
    Useful for printing them out in exceptions

--- src/main/kotlin/io/github/freya022/botcommands/api/components/EntitySelectMenu.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/components/EntitySelectMenu.kt
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.interactions.components.selections.EntitySelectMenu a
 
 class EntitySelectMenu internal constructor(
     private val componentController: ComponentController,
-    selectMenu: JDAEntitySelectMenu
+    private val selectMenu: JDAEntitySelectMenu
 ) : JDAEntitySelectMenu by selectMenu, IdentifiableComponent {
     override fun withDisabled(disabled: Boolean): EntitySelectMenu {
         return EntitySelectMenu(componentController, super.withDisabled(disabled))
@@ -14,4 +14,21 @@ class EntitySelectMenu internal constructor(
 
     @JvmSynthetic
     override suspend fun await(): EntitySelectEvent = componentController.awaitComponent(this)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as EntitySelectMenu
+
+        return selectMenu == other.selectMenu
+    }
+
+    override fun hashCode(): Int {
+        return selectMenu.hashCode()
+    }
+
+    override fun toString(): String {
+        return selectMenu.toString()
+    }
 }
