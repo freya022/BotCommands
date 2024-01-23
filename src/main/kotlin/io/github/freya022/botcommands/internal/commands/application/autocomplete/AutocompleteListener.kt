6fd987b50ff6d5d9b04980d1c2b5924fbe9ae43d
freya02 <41875020+freya022@users.noreply.github.com>
2024-01-23 23:01:34 +0100

    Run autocomplete on the application command scope

--- src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/autocomplete/AutocompleteListener.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/autocomplete/AutocompleteListener.kt
@@ -1,19 +1,23 @@
 package io.github.freya022.botcommands.internal.commands.application.autocomplete
 
 import io.github.freya022.botcommands.api.commands.CommandPath
+import io.github.freya022.botcommands.api.core.BContext
 import io.github.freya022.botcommands.api.core.annotations.BEventListener
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import io.github.freya022.botcommands.internal.commands.application.slash.SlashCommandOption
-import io.github.freya022.botcommands.internal.core.BContextImpl
 import io.github.freya022.botcommands.internal.core.options.OptionType
 import io.github.freya022.botcommands.internal.utils.ReflectionUtils.function
 import io.github.freya022.botcommands.internal.utils.throwUser
+import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
 
 @BService
-internal class AutocompleteListener(private val context: BContextImpl) {
+internal class AutocompleteListener(private val context: BContext) {
+    private val applicationContext = context.applicationCommandsContext
+    private val scope = context.coroutineScopesConfig.applicationCommandsScope
+
     @BEventListener
-    internal suspend fun onAutocomplete(event: CommandAutoCompleteInteractionEvent) {
+    internal suspend fun onAutocomplete(event: CommandAutoCompleteInteractionEvent) = scope.launch {
         val slashCommand = CommandPath.of(event.fullCommandName).let {
             context.applicationCommandsContext.findLiveSlashCommand(event.guild, it)
                 ?: throwUser("A slash command could not be found: ${event.fullCommandName}")
@@ -27,7 +31,7 @@ internal class AutocompleteListener(private val context: BContextImpl) {
                 val autocompleteHandler = option.autocompleteHandler
                     ?: throwUser(option.kParameter.function, "Autocomplete handler was not found on parameter '${option.declaredName}'")
 
-                return event.replyChoices(autocompleteHandler.handle(event)).queue()
+                return@launch event.replyChoices(autocompleteHandler.handle(event)).queue()
             }
         }
     }
