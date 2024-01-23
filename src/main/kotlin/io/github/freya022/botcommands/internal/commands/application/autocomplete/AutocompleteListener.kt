6c2edc4f565b7d93561d8ad1cc4210bd6781a471
freya02 <41875020+freya022@users.noreply.github.com>
2024-01-23 23:03:14 +0100

    Send no autocomplete choice when a command is not found
    Also log if the application command maps are absent

--- src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/autocomplete/AutocompleteListener.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/autocomplete/AutocompleteListener.kt
@@ -8,8 +8,11 @@ import io.github.freya022.botcommands.internal.commands.application.slash.SlashC
 import io.github.freya022.botcommands.internal.core.options.OptionType
 import io.github.freya022.botcommands.internal.utils.ReflectionUtils.function
 import io.github.freya022.botcommands.internal.utils.throwUser
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
+
+private val logger = KotlinLogging.logger { }
 
 @BService
 internal class AutocompleteListener(private val context: BContext) {
@@ -19,8 +22,10 @@ internal class AutocompleteListener(private val context: BContext) {
     @BEventListener
     internal suspend fun onAutocomplete(event: CommandAutoCompleteInteractionEvent) = scope.launch {
         val slashCommand = CommandPath.of(event.fullCommandName).let {
-            context.applicationCommandsContext.findLiveSlashCommand(event.guild, it)
-                ?: throwUser("A slash command could not be found: ${event.fullCommandName}")
+            applicationContext.findLiveSlashCommand(event.guild, it)
+                // Ignore, if the user tries to use a command we don't know,
+                // it's going to be handled by the slash command handler
+                ?: return@launch onCommandNotFound(event)
         }
 
         for (option in slashCommand.parameters.flatMap { it.allOptions }) {
@@ -34,5 +39,19 @@ internal class AutocompleteListener(private val context: BContext) {
                 return@launch event.replyChoices(autocompleteHandler.handle(event)).queue()
             }
         }
+    }
+
+    private fun onCommandNotFound(event: CommandAutoCompleteInteractionEvent) {
+        // In rare cases where a user sends an autocomplete request before the commands have been registered
+        // Log on DEBUG as the exception is going to be more apparent when the user executes the command
+        val guildMap = applicationContext.getLiveApplicationCommandsMap(event.guild)
+        val globalMap = applicationContext.getLiveApplicationCommandsMap(null)
+        if (guildMap == null || globalMap == null) {
+            logger.debug { "Ignoring autocomplete request for '${event.fullCommandName}' in guild '${event.guild?.name}' (${event.guild?.id}) as the commands haven't loaded yet" }
+        } else {
+            logger.debug { "Ignoring autocomplete request for '${event.fullCommandName}' in guild '${event.guild?.name}' (${event.guild?.id}) as the command does not exist" }
+        }
+
+        return event.replyChoices().queue()
     }
 }
