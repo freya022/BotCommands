0c34cc5c7cc4d6a475562cd1539e2c22f136aa0e
freya02 <41875020+freya022@users.noreply.github.com>
2024-02-21 18:00:30 +0100

    Return built modal inputs

--- src/main/kotlin/io/github/freya022/botcommands/api/modals/Modals.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/modals/Modals.kt
@@ -2,6 +2,7 @@ package io.github.freya022.botcommands.api.modals
 
 import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService
 import io.github.freya022.botcommands.api.modals.annotations.ModalInput
+import net.dv8tion.jda.api.interactions.components.text.TextInput
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle
 
 /**
@@ -26,20 +27,20 @@ interface Modals {
     fun createTextInput(inputName: String, label: String, style: TextInputStyle): TextInputBuilder
 }
 
-fun Modals.create(title: String, block: context(Modals) ModalBuilder.() -> Unit) =
+fun Modals.create(title: String, block: context(Modals) ModalBuilder.() -> Unit): Modal =
     create(title).apply { block(this@create, this) }.build()
 
 context(Modals)
-fun ModalBuilder.textInput(inputName: String, label: String, inputStyle: TextInputStyle, block: TextInputBuilder.() -> Unit = {}) {
-    addActionRow(createTextInput(inputName, label, inputStyle).apply(block).build())
-}
+fun ModalBuilder.textInput(inputName: String, label: String, inputStyle: TextInputStyle, block: TextInputBuilder.() -> Unit = {}): TextInput =
+    createTextInput(inputName, label, inputStyle)
+        .apply(block)
+        .build()
+        .also { addActionRow(it) }
 
 context(Modals)
-fun ModalBuilder.shortTextInput(inputName: String, label: String, block: TextInputBuilder.() -> Unit = {}) {
+fun ModalBuilder.shortTextInput(inputName: String, label: String, block: TextInputBuilder.() -> Unit = {}): TextInput =
     textInput(inputName, label, TextInputStyle.SHORT, block)
-}
 
 context(Modals)
-fun ModalBuilder.paragraphTextInput(inputName: String, label: String, block: TextInputBuilder.() -> Unit = {}) {
+fun ModalBuilder.paragraphTextInput(inputName: String, label: String, block: TextInputBuilder.() -> Unit = {}): TextInput =
     textInput(inputName, label, TextInputStyle.PARAGRAPH, block)
-}
