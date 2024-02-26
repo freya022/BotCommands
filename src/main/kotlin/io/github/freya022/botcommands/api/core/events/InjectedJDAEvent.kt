20745d50cf9fa478dea8192488e7804db884a4a1
freya02 <41875020+freya022@users.noreply.github.com>
2024-02-26 17:47:14 +0100

    Make `JDAService` mandatory (#154)
    * Make JDAService required by other services
    * Add the ShardManager service, if available

--- src/main/kotlin/io/github/freya022/botcommands/api/core/events/InjectedJDAEvent.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/events/InjectedJDAEvent.kt
@@ -4,9 +4,10 @@ import io.github.freya022.botcommands.api.core.BContext
 import io.github.freya022.botcommands.api.core.service.ServiceContainer
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent
+import net.dv8tion.jda.api.sharding.ShardManager
 
 /**
- * Indicates that a JDA instance was acquired and injected in the [ServiceContainer].
+ * Indicates that a [JDA] instance was acquired and injected in the [ServiceContainer], alongside its [ShardManager].
  *
  * The JDA instance is not fully initialized, as this event is sent as fast as possible,
  * when the first JDA instance is retrievable.
--- src/main/kotlin/io/github/freya022/botcommands/internal/core/JDAServiceMismatchChecker.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/core/JDAServiceMismatchChecker.kt
@@ -1,11 +1,9 @@
 package io.github.freya022.botcommands.internal.core
 
-import io.github.freya022.botcommands.api.core.BContext
 import io.github.freya022.botcommands.api.core.JDAService
 import io.github.freya022.botcommands.api.core.annotations.BEventListener
 import io.github.freya022.botcommands.api.core.events.InjectedJDAEvent
 import io.github.freya022.botcommands.api.core.service.annotations.BService
-import io.github.freya022.botcommands.api.core.service.getServiceOrNull
 import io.github.freya022.botcommands.internal.utils.reference
 import io.github.oshai.kotlinlogging.KotlinLogging
 
@@ -14,32 +12,30 @@ private val logger = KotlinLogging.logger { }
 @BService
 internal object JDAServiceMismatchChecker {
     @BEventListener
-    internal fun onJDA(event: InjectedJDAEvent, context: BContext) {
-        context.getServiceOrNull<JDAService>()?.let { jdaService ->
-            val jdaIntents = event.jda.gatewayIntents
-            val jdaServiceIntents = jdaService.intents
-            if (jdaIntents != jdaServiceIntents) {
-                logger.warn {
-                    """
-                        The intents given in JDAService and JDA should be the same!
-                        JDA intents: $jdaIntents
-                        JDAService intents: $jdaServiceIntents
-                        Hint: you should pass ${JDAService::intents.reference} to your builder
-                    """.trimIndent()
-                }
+    internal fun onJDA(event: InjectedJDAEvent, jdaService: JDAService) {
+        val jdaIntents = event.jda.gatewayIntents
+        val jdaServiceIntents = jdaService.intents
+        if (jdaIntents != jdaServiceIntents) {
+            logger.warn {
+                """
+                    The intents given in JDAService and JDA should be the same!
+                    JDA intents: $jdaIntents
+                    JDAService intents: $jdaServiceIntents
+                    Hint: you should pass ${JDAService::intents.reference} to your builder
+                """.trimIndent()
             }
+        }
 
-            val jdaCacheFlags = event.jda.cacheFlags
-            val jdaServiceCacheFlags = jdaService.cacheFlags
-            if (!jdaCacheFlags.containsAll(jdaServiceCacheFlags)) {
-                logger.warn {
-                    """
-                        The cache flags given in JDAService should at least be a subset of the JDA cache flags!
-                        JDA cache flags: $jdaCacheFlags
-                        JDAService cache flags: $jdaServiceCacheFlags
-                        Hint: you should pass ${JDAService::cacheFlags.reference} to your builder
-                    """.trimIndent()
-                }
+        val jdaCacheFlags = event.jda.cacheFlags
+        val jdaServiceCacheFlags = jdaService.cacheFlags
+        if (!jdaCacheFlags.containsAll(jdaServiceCacheFlags)) {
+            logger.warn {
+                """
+                    The cache flags given in JDAService should at least be a subset of the JDA cache flags!
+                    JDA cache flags: $jdaCacheFlags
+                    JDAService cache flags: $jdaServiceCacheFlags
+                    Hint: you should pass ${JDAService::cacheFlags.reference} to your builder
+                """.trimIndent()
             }
         }
     }
