e60452fae3454fb7c9170830f1066c93cafd87d8
freya02 <41875020+freya022@users.noreply.github.com>
2024-03-12 11:55:25 +0100

    Fix cast exception on timeout

--- src/main/kotlin/io/github/freya022/botcommands/api/pagination/AbstractPagination.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/pagination/AbstractPagination.kt
@@ -90,7 +90,7 @@ abstract class AbstractPagination<T : AbstractPagination<T>> protected construct
             timeoutJob = paginationTimeoutScope.launchCatchingDelayed(timeout.timeout, { onTimeoutHandlerException(it) }) {
                 timeoutPassed = true
                 @Suppress("UNCHECKED_CAST")
-                timeout.onTimeout?.invoke(this as T)
+                timeout.onTimeout?.invoke(this@AbstractPagination as T)
             }
         }
     }
