c3c0589e2ac4b10c45c682e922990d9ae8030374
freya02 <41875020+freya022@users.noreply.github.com>
2024-03-15 16:40:49 +0100

    Recreate paginator buttons
    They caused warnings due to the clean after refresh

--- src/main/kotlin/io/github/freya022/botcommands/api/pagination/paginator/AbstractPaginator.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/pagination/paginator/AbstractPaginator.kt
@@ -1,10 +1,9 @@
 package io.github.freya022.botcommands.api.pagination.paginator
 
 import io.github.freya022.botcommands.api.components.event.ButtonEvent
+import io.github.freya022.botcommands.api.components.utils.ButtonContent
 import io.github.freya022.botcommands.api.core.BContext
 import io.github.freya022.botcommands.api.pagination.AbstractPagination
-import net.dv8tion.jda.api.interactions.components.ActionRow
-import net.dv8tion.jda.api.interactions.components.buttons.Button
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder
 
@@ -26,7 +25,7 @@ abstract class AbstractPaginator<T : AbstractPaginator<T>> protected constructor
     var page: Int = 0
         set(value) {
             // 0 <= value < maxPages
-            check(value in 0..<maxPages) {
+            require(value in 0..<maxPages) {
                 "Page needs to be between 0 and $maxPages (excluded)"
             }
             field = value
@@ -37,56 +36,11 @@ abstract class AbstractPaginator<T : AbstractPaginator<T>> protected constructor
     val isLastPage: Boolean
         get() = page >= maxPages - 1
 
-    private var firstButton: Button
-    private var previousButton: Button
-    private var nextButton: Button
-    private var lastButton: Button
-    private var deleteButton: Button? = null
-
-    init {
-        firstButton = this.componentsService.ephemeralButton(ButtonStyle.PRIMARY, builder.firstContent)
-            .bindTo { e: ButtonEvent ->
-                page = 0
-                e.editMessage(getCurrentMessage()).queue()
-            }
-            .constraints(constraints)
-            .build()
-
-        previousButton = this.componentsService.ephemeralButton(ButtonStyle.PRIMARY, builder.previousContent)
-            .bindTo { e: ButtonEvent ->
-                page = (page - 1).coerceAtLeast(0)
-                e.editMessage(getCurrentMessage()).queue()
-            }
-            .constraints(constraints)
-            .build()
-
-        nextButton = this.componentsService.ephemeralButton(ButtonStyle.PRIMARY, builder.nextContent)
-            .bindTo { e: ButtonEvent ->
-                page = (page + 1).coerceAtMost(maxPages - 1)
-                e.editMessage(getCurrentMessage()).queue()
-            }
-            .constraints(constraints)
-            .build()
-
-        lastButton = this.componentsService.ephemeralButton(ButtonStyle.PRIMARY, builder.lastContent)
-            .bindTo { e: ButtonEvent ->
-                page = maxPages - 1
-                e.editMessage(getCurrentMessage()).queue()
-            }
-            .constraints(constraints)
-            .build()
-
-        if (builder.hasDeleteButton) {
-            //Unique use in the case the message isn't ephemeral
-            this.deleteButton = this.componentsService.ephemeralButton(ButtonStyle.DANGER, builder.deleteContent)
-                .bindTo(::onDeleteClicked)
-                .constraints(constraints)
-                .oneUse(true)
-                .build()
-        } else {
-            this.deleteButton = null
-        }
-    }
+    private val firstButton: ButtonContent = builder.firstContent
+    private val previousButton: ButtonContent = builder.previousContent
+    private val nextButton: ButtonContent = builder.nextContent
+    private val lastButton: ButtonContent = builder.lastContent
+    private val deleteButton: ButtonContent? = builder.deleteContent.takeIf { builder.hasDeleteButton }
 
     private suspend fun onDeleteClicked(e: ButtonEvent) {
         e.deferEdit().queue()
@@ -101,28 +55,30 @@ abstract class AbstractPaginator<T : AbstractPaginator<T>> protected constructor
     }
 
     protected open fun putComponents(builder: MessageCreateBuilder) {
-        if (isFirstPage) {
-            previousButton = previousButton.asDisabled()
-            firstButton = firstButton.asDisabled()
-        } else {
-            previousButton = previousButton.asEnabled()
-            firstButton = firstButton.asEnabled()
+        val buttons = buildList {
+            this += firstButton.toPageButton(targetPage = 0).withDisabled(isFirstPage)
+            this += previousButton.toPageButton(targetPage = page - 1).withDisabled(isFirstPage)
+
+            this += nextButton.toPageButton(targetPage = page + 1).withDisabled(isLastPage)
+            this += lastButton.toPageButton(targetPage = maxPages - 1).withDisabled(isLastPage)
+
+            if (deleteButton != null) {
+                this += componentsService.ephemeralButton(ButtonStyle.DANGER, deleteButton)
+                    .bindTo(::onDeleteClicked)
+                    .constraints(constraints)
+                    .build()
+            }
         }
 
-        if (isLastPage) {
-            nextButton = nextButton.asDisabled()
-            lastButton = lastButton.asDisabled()
-        } else {
-            nextButton = nextButton.asEnabled()
-            lastButton = lastButton.asEnabled()
-        }
-
-        val deleteButton = deleteButton
-        val row = if (deleteButton != null) {
-            ActionRow.of(firstButton, previousButton, nextButton, lastButton, deleteButton)
-        } else {
-            ActionRow.of(firstButton, previousButton, nextButton, lastButton)
-        }
-        builder.addComponents(row)
+        builder.addActionRow(buttons)
     }
+
+    private fun ButtonContent.toPageButton(targetPage: Int) =
+        componentsService.ephemeralButton(ButtonStyle.PRIMARY, this)
+            .bindTo { e: ButtonEvent ->
+                page = targetPage.coerceIn(0, maxPages - 1)
+                e.editMessage(getCurrentMessage()).queue()
+            }
+            .constraints(constraints)
+            .build()
 }
