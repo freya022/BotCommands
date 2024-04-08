b6e443883a41309a5a41302643cb9105d31d403f
freya02 <41875020+freya022@users.noreply.github.com>
2024-04-08 21:47:08 +0200

    Document `@EnableBotCommands`, allow as a meta annotation

--- src/main/kotlin/io/github/freya022/botcommands/api/core/annotations/EnableBotCommands.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/annotations/EnableBotCommands.kt
@@ -4,7 +4,31 @@ import io.github.freya022.botcommands.internal.core.BotCommandsInitializer
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 
-@Target(AnnotationTarget.CLASS)
+/**
+ * Entry point annotation for Spring projects.
+ *
+ * This annotation can only be used once in your main class.
+ *
+ * **Note:** Your main class must **not** have [@ComponentScan][ComponentScan],
+ * prefer using your own annotation meta-annotated with the scanner, example:
+ * ```kt
+ * @ComponentScan("my.bot.package")
+ * annotation class EnableMyBot
+ *
+ * @EnableMyBot
+ * @EnableBotCommands
+ * @EnableAutoConfiguration
+ * @ConfigurationPropertiesScan
+ * class MyBotApplication
+ * ```
+ *
+ * This is due to [this issue](https://github.com/spring-projects/spring-framework/issues/31957#issuecomment-1880081941):
+ * > The recommendation is that you either declare one or more local `@ComponentScan` annotations OR one or more `@ComponentScan` meta-annotations.
+ * > If you combine local and meta-annotations for `@ComponentScan`, only the local `@ComponentScan` annotations will be honored.
+ *
+ * If you have a better solution, please suggest it.
+ */
+@Target(AnnotationTarget.CLASS, AnnotationTarget.ANNOTATION_CLASS)
 @Import(BotCommandsInitializer::class)
 @Retention(AnnotationRetention.RUNTIME)
 @ComponentScan(
