2f19f68e2558f3dccdfc979e99a88bb95c6370f3
freya02 <41875020+freya022@users.noreply.github.com>
2024-04-15 21:59:52 +0200

    Add support for `@RequiredIntents`

--- src/main/kotlin/io/github/freya022/botcommands/api/core/conditions/RequiredIntents.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/conditions/RequiredIntents.kt
@@ -3,13 +3,18 @@ package io.github.freya022.botcommands.api.core.conditions
 import io.github.freya022.botcommands.api.core.JDAService
 import io.github.freya022.botcommands.api.core.service.annotations.Condition
 import io.github.freya022.botcommands.internal.conditions.RequiredIntentsChecker
+import io.github.freya022.botcommands.internal.conditions.SpringRequiredIntentsChecker
 import net.dv8tion.jda.api.requests.GatewayIntent
+import org.springframework.context.annotation.Conditional
 
 /**
  * Prevents usage of the annotated service if the required intents are not present in [JDAService.intents].
+ *
+ * **Note:** If you are using Spring, this checks against the `jda.intents` property.
  *
  * @see JDAService
  */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER)
 @Condition(RequiredIntentsChecker::class, fail = false)
+@Conditional(SpringRequiredIntentsChecker::class)
 annotation class RequiredIntents(@get:JvmName("value") vararg val intents: GatewayIntent)
--- src/main/kotlin/io/github/freya022/botcommands/internal/conditions/RequiredIntentsChecker.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/conditions/RequiredIntentsChecker.kt
@@ -5,7 +5,13 @@ import io.github.freya022.botcommands.api.core.conditions.RequiredIntents
 import io.github.freya022.botcommands.api.core.service.CustomConditionChecker
 import io.github.freya022.botcommands.api.core.service.ServiceContainer
 import io.github.freya022.botcommands.api.core.service.getService
+import io.github.freya022.botcommands.api.core.utils.enumSetOf
 import io.github.freya022.botcommands.api.core.utils.simpleNestedName
+import net.dv8tion.jda.api.requests.GatewayIntent
+import org.springframework.context.annotation.Condition
+import org.springframework.context.annotation.ConditionContext
+import org.springframework.core.env.getProperty
+import org.springframework.core.type.AnnotatedTypeMetadata
 
 internal object RequiredIntentsChecker : CustomConditionChecker<RequiredIntents> {
     override val annotationType: Class<RequiredIntents> = RequiredIntents::class.java
@@ -22,5 +28,17 @@ internal object RequiredIntentsChecker : CustomConditionChecker<RequiredIntents>
         }
 
         return null
+    }
+}
+
+internal object SpringRequiredIntentsChecker : Condition {
+    override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+        val currentIntents = context.environment
+            .getProperty<List<String>>("jda.intents")
+            ?.mapTo(enumSetOf<GatewayIntent>(), GatewayIntent::valueOf)
+            ?: JDAService.defaultIntents
+        val requiredIntents = metadata.annotations.get(RequiredIntentsChecker.annotationType).getEnumArray("value", GatewayIntent::class.java).toSet()
+        val missingIntents = requiredIntents - currentIntents
+        return missingIntents.isEmpty()
     }
 }
