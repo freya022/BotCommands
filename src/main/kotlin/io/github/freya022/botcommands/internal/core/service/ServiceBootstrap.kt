ed5324c9e30ce001dafb442f41057da765ea91ee
freya02 <41875020+freya022@users.noreply.github.com>
2024-04-17 13:17:30 +0200

    Remove todo

--- src/main/kotlin/io/github/freya022/botcommands/internal/core/service/ServiceBootstrap.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/core/service/ServiceBootstrap.kt
@@ -3,9 +3,7 @@ package io.github.freya022.botcommands.internal.core.service
 import io.github.freya022.botcommands.api.core.service.ClassGraphProcessor
 import io.github.freya022.botcommands.api.core.service.ServiceContainer
 
-//TODO rename to BotCommandsBootstrapper
-// make this an abstract class, move default init logic in it
-// so both default and spring initialize there instead of separate classes
+
 internal interface ServiceBootstrap {
     val classGraphProcessors: Set<ClassGraphProcessor>
     val stagingClassAnnotations: StagingClassAnnotations
