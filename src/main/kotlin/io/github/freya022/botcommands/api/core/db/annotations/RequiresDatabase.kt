8e825e07dcbbd6ba6a0d5b0dc9e20b21499cc25e
freya02 <41875020+freya022@users.noreply.github.com>
2024-04-18 12:07:16 +0200

    Replace botcommands.database.enable with ConnectionSupplier bean check

--- src/main/kotlin/io/github/freya022/botcommands/api/core/db/annotations/RequiresDatabase.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/db/annotations/RequiresDatabase.kt
@@ -1,9 +1,10 @@
 package io.github.freya022.botcommands.api.core.db.annotations
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import io.github.freya022.botcommands.api.core.db.ConnectionSupplier
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 
 //TODO when BC supports reading inherited annotations,
 // add @Dependencies(ConnectionSupplier::class)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.ANNOTATION_CLASS)
-@ConditionalOnProperty("botcommands.database.enable")
+@ConditionalOnBean(ConnectionSupplier::class)
 annotation class RequiresDatabase
