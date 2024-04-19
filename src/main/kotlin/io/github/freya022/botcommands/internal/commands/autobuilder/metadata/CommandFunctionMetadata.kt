ffce4906b3b23ca19d6bed71a73c5b62b482ac81
freya02 <41875020+freya022@users.noreply.github.com>
2024-04-19 12:59:10 +0200

    Check required command superclasses at init time

--- src/main/kotlin/io/github/freya022/botcommands/internal/commands/autobuilder/metadata/CommandFunctionMetadata.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/commands/autobuilder/metadata/CommandFunctionMetadata.kt
@@ -1,20 +1,26 @@
 package io.github.freya022.botcommands.internal.commands.autobuilder.metadata
 
 import io.github.freya022.botcommands.api.commands.CommandPath
+import io.github.freya022.botcommands.api.core.utils.isSubclassOf
 import io.github.freya022.botcommands.internal.core.ClassPathFunction
-import io.github.freya022.botcommands.internal.utils.throwUser
+import io.github.freya022.botcommands.internal.utils.requireUser
 import kotlin.reflect.KClass
-import kotlin.reflect.safeCast
 
 internal abstract class CommandFunctionMetadata<T : Any, A : Annotation>(
     private val classPathFunction: ClassPathFunction,
-    private val instanceType: KClass<T>,
+    instanceType: KClass<T>,
     val annotation: A,
     val path: CommandPath
 ) : MetadataFunctionHolder {
-    override val func get() = classPathFunction.function
+    final override val func get() = classPathFunction.function
 
+    init {
+        requireUser(classPathFunction.clazz.isSubclassOf(instanceType), func) {
+            "Declaring class must extend ${instanceType.simpleName}"
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
     val instance: T
-        get() = instanceType.safeCast(classPathFunction.instance)
-            ?: throwUser(func, "Declaring class must extend ${instanceType.simpleName}")
+        get() = classPathFunction.instance as T
 }
