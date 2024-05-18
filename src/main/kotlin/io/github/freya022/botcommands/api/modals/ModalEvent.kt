c7f16831e76c893c67fc1f4583b8b6f3b71a0702
freya02 <41875020+freya022@users.noreply.github.com>
2024-05-18 18:52:38 +0200

    Accept ActionComponent in ModalEvent#get

--- src/main/kotlin/io/github/freya022/botcommands/api/modals/ModalEvent.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/modals/ModalEvent.kt
@@ -2,7 +2,7 @@ package io.github.freya022.botcommands.api.modals
 
 import io.github.freya022.botcommands.api.core.BContext
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
-import net.dv8tion.jda.api.interactions.components.text.TextInput
+import net.dv8tion.jda.api.interactions.components.ActionComponent
 import net.dv8tion.jda.api.interactions.modals.ModalMapping
 
 class ModalEvent internal constructor(
@@ -13,6 +13,12 @@ class ModalEvent internal constructor(
     override fun getRawData() = event.rawData
 
     @JvmName("getValue")
-    operator fun get(input: TextInput): ModalMapping = event.values.first { it.id == input.id }
-        ?: throw IllegalArgumentException("No value found, you likely passed an input from another modal, or haven't attached it")
+    operator fun get(component: ActionComponent): ModalMapping {
+        require(component.isModalCompatible) {
+            "Can only get modal mapping for modal-compatible components, provided: $component"
+        }
+
+        return event.values.first { it.id == component.id }
+            ?: throw IllegalArgumentException("No value found, you likely passed an input from another modal, or haven't attached it")
+    }
 }
