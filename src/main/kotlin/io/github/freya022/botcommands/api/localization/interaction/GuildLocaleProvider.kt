d7c565fb5e001664f3f231d8043ecbcd45a31628
freya02 <41875020+freya022@users.noreply.github.com>
2024-05-30 18:15:28 +0200

    Add User/GuildLocaleProvider and default implementations

---
+++ src/main/kotlin/io/github/freya022/botcommands/api/localization/interaction/GuildLocaleProvider.kt
@@ -0,0 +1,22 @@
+package io.github.freya022.botcommands.api.localization.interaction
+
+import io.github.freya022.botcommands.api.core.config.BServiceConfigBuilder
+import io.github.freya022.botcommands.api.core.service.annotations.BService
+import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService
+import net.dv8tion.jda.api.interactions.DiscordLocale
+import net.dv8tion.jda.api.interactions.Interaction
+
+/**
+ * Provides the locale of a guild in a Discord interaction,
+ * may be useful if the guild has set its own locale, for example.
+ *
+ * This returns [Interaction.getGuildLocale] by default.
+ *
+ * ### Usage
+ * Register your instance as a service with [@BService][BService]
+ * or [any annotation that enables your class for dependency injection][BServiceConfigBuilder.serviceAnnotations].
+ */
+@InterfacedService(acceptMultiple = false)
+fun interface GuildLocaleProvider {
+    fun getLocale(interaction: Interaction): DiscordLocale
+}
---
+++ src/main/kotlin/io/github/freya022/botcommands/internal/localization/interaction/DefaultGuildLocaleProvider.kt
@@ -0,0 +1,33 @@
+package io.github.freya022.botcommands.internal.localization.interaction
+
+import io.github.freya022.botcommands.api.core.service.ConditionalServiceChecker
+import io.github.freya022.botcommands.api.core.service.ServiceContainer
+import io.github.freya022.botcommands.api.core.service.annotations.BService
+import io.github.freya022.botcommands.api.core.service.annotations.ConditionalService
+import io.github.freya022.botcommands.api.core.service.getInterfacedServices
+import io.github.freya022.botcommands.api.core.utils.simpleNestedName
+import io.github.freya022.botcommands.api.localization.interaction.GuildLocaleProvider
+import io.github.freya022.botcommands.internal.utils.classRef
+import net.dv8tion.jda.api.interactions.DiscordLocale
+import net.dv8tion.jda.api.interactions.Interaction
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+
+@BService
+@ConditionalService(DefaultGuildLocaleProvider.ExistingSupplierChecker::class)
+@ConditionalOnMissingBean(GuildLocaleProvider::class)
+internal class DefaultGuildLocaleProvider internal constructor() : GuildLocaleProvider {
+    override fun getLocale(interaction: Interaction): DiscordLocale = interaction.guildLocale
+
+    internal object ExistingSupplierChecker : ConditionalServiceChecker {
+        override fun checkServiceAvailability(serviceContainer: ServiceContainer, checkedClass: Class<*>): String? {
+            // Try to get GuildLocaleProvider interfaced services, except ours
+            // If empty, then the user didn't provide one, in which case we can allow
+            //Won't take DefaultGuildLocaleProvider into account
+            val providers = serviceContainer.getInterfacedServices<GuildLocaleProvider>()
+            if (providers.isNotEmpty())
+                return "An user supplied ${classRef<GuildLocaleProvider>()} is already active (${providers.first().javaClass.simpleNestedName})"
+
+            return null
+        }
+    }
+}
