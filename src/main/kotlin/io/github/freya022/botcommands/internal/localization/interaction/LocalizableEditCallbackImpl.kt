1531b7190dd2262c3a4ad4cec87d94baeed02366
freya02 <41875020+freya022@users.noreply.github.com>
2024-06-06 14:18:45 +0200

    Implement LocalizableInteraction on reply/edit callbacks

--- src/main/kotlin/io/github/freya022/botcommands/internal/localization/interaction/LocalizableEditCallbackImpl.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/localization/interaction/LocalizableEditCallbackImpl.kt
@@ -2,30 +2,28 @@ package io.github.freya022.botcommands.internal.localization.interaction
 
 import io.github.freya022.botcommands.api.localization.Localization
 import io.github.freya022.botcommands.api.localization.interaction.LocalizableEditCallback
-import io.github.freya022.botcommands.api.localization.interaction.LocalizableInteractionHook
+import io.github.freya022.botcommands.api.localization.interaction.LocalizableInteraction
 import net.dv8tion.jda.api.interactions.callbacks.IMessageEditCallback
 import net.dv8tion.jda.api.requests.restaction.interactions.MessageEditCallbackAction
 import java.util.*
 
 internal class LocalizableEditCallbackImpl internal constructor(
     private val editCallback: IMessageEditCallback,
-    private val interaction: LocalizableInteractionImpl
-) : LocalizableEditCallback {
-    override fun getHook(): LocalizableInteractionHook {
-        return interaction.getHook()
-    }
+    interaction: LocalizableInteractionImpl
+) : LocalizableEditCallback,
+    LocalizableInteraction by interaction {
 
     override fun editUser(localizationPath: String, vararg entries: Localization.Entry): MessageEditCallbackAction =
-        editCallback.editMessage(interaction.getUserMessage(localizationPath, *entries))
+        editCallback.editMessage(getUserMessage(localizationPath, *entries))
 
     override fun editGuild(localizationPath: String, vararg entries: Localization.Entry): MessageEditCallbackAction =
-        editCallback.editMessage(interaction.getGuildMessage(localizationPath, *entries))
+        editCallback.editMessage(getGuildMessage(localizationPath, *entries))
 
     override fun editLocalized(
         locale: Locale,
         localizationPath: String,
         vararg entries: Localization.Entry,
     ): MessageEditCallbackAction {
-        return editCallback.editMessage(interaction.getLocalizedMessage(locale, localizationPath, *entries))
+        return editCallback.editMessage(getLocalizedMessage(locale, localizationPath, *entries))
     }
 }
--- src/main/kotlin/io/github/freya022/botcommands/internal/localization/interaction/LocalizableReplyCallbackImpl.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/localization/interaction/LocalizableReplyCallbackImpl.kt
@@ -1,7 +1,7 @@
 package io.github.freya022.botcommands.internal.localization.interaction
 
 import io.github.freya022.botcommands.api.localization.Localization
-import io.github.freya022.botcommands.api.localization.interaction.LocalizableInteractionHook
+import io.github.freya022.botcommands.api.localization.interaction.LocalizableInteraction
 import io.github.freya022.botcommands.api.localization.interaction.LocalizableReplyCallback
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback
 import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction
@@ -9,23 +9,21 @@ import java.util.*
 
 internal class LocalizableReplyCallbackImpl internal constructor(
     private val replyCallback: IReplyCallback,
-    private val interaction: LocalizableInteractionImpl
-) : LocalizableReplyCallback {
-    override fun getHook(): LocalizableInteractionHook {
-        return interaction.getHook()
-    }
+    interaction: LocalizableInteractionImpl
+) : LocalizableReplyCallback,
+    LocalizableInteraction by interaction {
 
     override fun replyUser(localizationPath: String, vararg entries: Localization.Entry): ReplyCallbackAction =
-        replyCallback.reply(interaction.getUserMessage(localizationPath, *entries))
+        replyCallback.reply(getUserMessage(localizationPath, *entries))
 
     override fun replyGuild(localizationPath: String, vararg entries: Localization.Entry): ReplyCallbackAction =
-        replyCallback.reply(interaction.getGuildMessage(localizationPath, *entries))
+        replyCallback.reply(getGuildMessage(localizationPath, *entries))
 
     override fun replyLocalized(
         locale: Locale,
         localizationPath: String,
         vararg entries: Localization.Entry,
     ): ReplyCallbackAction {
-        return replyCallback.reply(interaction.getLocalizedMessage(locale, localizationPath, *entries))
+        return replyCallback.reply(getLocalizedMessage(locale, localizationPath, *entries))
     }
 }
