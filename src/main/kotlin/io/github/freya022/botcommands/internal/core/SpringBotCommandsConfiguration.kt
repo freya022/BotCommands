c294947fe5ed22f23295fb54c2cc537787edb249
freya02 <41875020+freya022@users.noreply.github.com>
2024-07-23 19:38:16 +0200

    Apparently this is how you do it

--- src/main/kotlin/io/github/freya022/botcommands/internal/core/SpringBotCommandsConfiguration.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/core/SpringBotCommandsConfiguration.kt
@@ -2,11 +2,11 @@ package io.github.freya022.botcommands.internal.core
 
 import io.github.freya022.botcommands.api.core.config.JDAConfiguration
 import io.github.freya022.botcommands.internal.core.config.BotCommandsCoreConfiguration
-import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
-@AutoConfiguration
+@Configuration
 @ComponentScan(
     basePackages = [
         "io.github.freya022.botcommands.api",
@@ -14,4 +14,4 @@ import org.springframework.context.annotation.ComponentScan
     ]
 )
 @ConfigurationPropertiesScan(basePackageClasses = [JDAConfiguration::class, BotCommandsCoreConfiguration::class])
-open class BotCommandsAutoConfiguration
+internal open class SpringBotCommandsConfiguration
