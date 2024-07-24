4829c905bf61396ef19260b033984902cc65fc05
freya02 <41875020+freya022@users.noreply.github.com>
2024-07-24 19:44:59 +0200

    Document requirement annotations

--- src/main/kotlin/io/github/freya022/botcommands/api/commands/application/annotations/RequiresApplicationCommands.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/commands/application/annotations/RequiresApplicationCommands.kt
@@ -1,9 +1,15 @@
 package io.github.freya022.botcommands.api.commands.application.annotations
 
+import io.github.freya022.botcommands.api.core.config.BApplicationConfig
 import io.github.freya022.botcommands.api.core.service.annotations.Condition
 import io.github.freya022.botcommands.internal.commands.application.RequiresApplicationCommandsChecker
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 
+/**
+ * Prevents usage of the annotated service if application commands are [not enabled][BApplicationConfig.enable].
+ *
+ * @see BApplicationConfig.enable
+ */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.ANNOTATION_CLASS)
 @Condition(RequiresApplicationCommandsChecker::class)
 @ConditionalOnProperty("botcommands.application.enable")
--- src/main/kotlin/io/github/freya022/botcommands/api/commands/text/annotations/RequiresTextCommands.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/commands/text/annotations/RequiresTextCommands.kt
@@ -1,9 +1,15 @@
 package io.github.freya022.botcommands.api.commands.text.annotations
 
+import io.github.freya022.botcommands.api.core.config.BTextConfig
 import io.github.freya022.botcommands.api.core.service.annotations.Condition
 import io.github.freya022.botcommands.internal.commands.text.RequiresTextCommandsChecker
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 
+/**
+ * Prevents usage of the annotated service if text commands are [not enabled][BTextConfig.enable].
+ *
+ * @see BTextConfig.enable
+ */
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.ANNOTATION_CLASS)
 @Condition(RequiresTextCommandsChecker::class)
 @ConditionalOnProperty("botcommands.text.enable")
