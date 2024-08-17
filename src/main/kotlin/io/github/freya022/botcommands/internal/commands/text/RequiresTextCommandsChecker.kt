ea6df11b3be45cb8c23b81d0843f5bc0ed745b44
freya02 <41875020+freya022@users.noreply.github.com>
2024-08-17 18:23:05 +0200

    Only check status on RequiresTextCommandsChecker
    Reduces risks of circular dependencies
    This shouldn't check for everything anyway

--- src/main/kotlin/io/github/freya022/botcommands/internal/commands/text/RequiresTextCommandsChecker.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/commands/text/RequiresTextCommandsChecker.kt
@@ -1,14 +1,10 @@
 package io.github.freya022.botcommands.internal.commands.text
 
-import io.github.freya022.botcommands.api.commands.text.TextPrefixSupplier
 import io.github.freya022.botcommands.api.commands.text.annotations.RequiresTextCommands
 import io.github.freya022.botcommands.api.core.config.BTextConfig
 import io.github.freya022.botcommands.api.core.service.CustomConditionChecker
 import io.github.freya022.botcommands.api.core.service.ServiceContainer
 import io.github.freya022.botcommands.api.core.service.getService
-import io.github.freya022.botcommands.api.core.service.getServiceOrNull
-import io.github.freya022.botcommands.api.core.utils.simpleNestedName
-import io.github.freya022.botcommands.internal.commands.text.TextCommandsListener.Status
 import io.github.freya022.botcommands.internal.utils.reference
 
 internal object RequiresTextCommandsChecker : CustomConditionChecker<RequiresTextCommands> {
@@ -19,16 +15,10 @@ internal object RequiresTextCommandsChecker : CustomConditionChecker<RequiresTex
         checkedClass: Class<*>,
         annotation: RequiresTextCommands
     ): String? {
-        fun prefixSupplierName() = serviceContainer.getService<TextPrefixSupplier>().javaClass.simpleNestedName
-        return when (Status.check(serviceContainer.getService(), serviceContainer.getService(), serviceContainer.getServiceOrNull())) {
-            Status.DISABLED -> "Text commands needs to be enabled, see ${BTextConfig::enable.reference}"
-            Status.ENABLED -> null
-            Status.USES_PREFIX_SUPPLIER -> null
-            Status.CAN_READ_PING -> null
-            Status.MISSING_CONTENT_INTENT_WITH_PREFIX_SUPPLIER_WITH_PING -> null
-            Status.MISSING_CONTENT_INTENT_WITH_PREFIX_SUPPLIER_WITHOUT_PING -> "Text prefixes supplied by ${prefixSupplierName()} can't be used without GatewayIntent.MESSAGE_CONTENT, and ${BTextConfig::usePingAsPrefix.reference} is disabled"
-            Status.MISSING_CONTENT_INTENT_WITH_PREFIX_WITH_PING -> null
-            Status.MISSING_CONTENT_INTENT_WITH_PREFIX_WITHOUT_PING -> "Text prefixes can't be used without GatewayIntent.MESSAGE_CONTENT, and ${BTextConfig::usePingAsPrefix.reference} is disabled"
+        return if (serviceContainer.getService<BTextConfig>().enable) {
+            null
+        } else {
+            "Text commands needs to be enabled, see ${BTextConfig::enable.reference}"
         }
     }
 }
