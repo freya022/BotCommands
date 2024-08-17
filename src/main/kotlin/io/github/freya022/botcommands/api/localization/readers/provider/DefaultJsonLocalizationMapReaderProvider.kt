838f768eaf23d06194a655a1e1fec7c370e2eb9a
freya02 <41875020+freya022@users.noreply.github.com>
2024-08-17 14:05:41 +0200

    Make DefaultJsonLocalizationMapReaderProvider a service
    Required by the service factory, can't be an object due to Spring

--- src/main/kotlin/io/github/freya022/botcommands/api/localization/readers/provider/DefaultJsonLocalizationMapReaderProvider.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/localization/readers/provider/DefaultJsonLocalizationMapReaderProvider.kt
@@ -1,14 +1,13 @@
 package io.github.freya022.botcommands.api.localization.readers.provider
 
 import io.github.freya022.botcommands.api.core.BContext
-import io.github.freya022.botcommands.api.core.service.annotations.BConfiguration
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import io.github.freya022.botcommands.api.localization.readers.DefaultJsonLocalizationMapReader
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@BConfiguration
+@BService
 internal open class DefaultJsonLocalizationMapReaderProvider {
     @Bean("builtinDefaultJsonLocalizationMapReader")
     @BService(name = "builtinDefaultJsonLocalizationMapReader")
