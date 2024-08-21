b132709540322b4878abcc845f3eed014a78051b
freya02 <41875020+freya022@users.noreply.github.com>
2024-08-21 16:49:19 +0200

    Adapt ApplicationCommandsCacheFactoryProvider to use configs

--- src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/cache/provider/ApplicationCommandsCacheFactoryProvider.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/commands/application/cache/provider/ApplicationCommandsCacheFactoryProvider.kt
@@ -1,6 +1,9 @@
 package io.github.freya022.botcommands.internal.commands.application.cache.provider
 
 import io.github.freya022.botcommands.api.core.config.BApplicationConfig
+import io.github.freya022.botcommands.api.core.config.BApplicationConfigBuilder
+import io.github.freya022.botcommands.api.core.config.application.cache.DatabaseApplicationCommandsCacheConfig
+import io.github.freya022.botcommands.api.core.config.application.cache.FileApplicationCommandsCacheConfig
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import io.github.freya022.botcommands.api.core.service.annotations.Lazy
 import io.github.freya022.botcommands.api.core.utils.loggerOf
@@ -9,20 +12,17 @@ import io.github.freya022.botcommands.internal.commands.application.cache.factor
 import io.github.freya022.botcommands.internal.commands.application.cache.factory.FileApplicationCommandsCacheFactory
 import io.github.freya022.botcommands.internal.commands.application.cache.factory.MemoryApplicationCommandsCacheFactory
 import io.github.freya022.botcommands.internal.commands.application.cache.factory.NullApplicationCommandsCacheFactory
-import io.github.freya022.botcommands.internal.utils.reference
+import io.github.freya022.botcommands.internal.utils.shortSignatureNoSrc
+import io.github.freya022.botcommands.internal.utils.throwInternal
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.dv8tion.jda.api.JDA
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import kotlin.io.path.Path
 import kotlin.io.path.isWritable
 import kotlin.io.path.pathString
 
 private val logger = KotlinLogging.loggerOf<ApplicationCommandsCache>()
 
-//TODO in-memory/in-file/in-database storage
-// Perhaps the configuration would look like `commandCache = FileCommandCache(baseDir)`
-// In-memory storage should not be configurable, as it is used as a fallback for missing write permissions
 @BService
 @Configuration
 internal open class ApplicationCommandsCacheFactoryProvider {
@@ -30,24 +30,21 @@ internal open class ApplicationCommandsCacheFactoryProvider {
     @Bean
     @BService
     internal open fun applicationCommandsCacheFactory(jda: JDA, applicationConfig: BApplicationConfig): ApplicationCommandsCacheFactory {
-        if (!applicationConfig.enableCaching) {
-            return NullApplicationCommandsCacheFactory
-        }
+        val cacheConfig = applicationConfig.cache ?: return NullApplicationCommandsCacheFactory
 
-        val dataDirectory = applicationConfig.commandCachePath ?: run {
-            val appDataDirectory = when {
-                "Windows" in System.getProperty("os.name") -> System.getenv("appdata")
-                else -> "/var/tmp"
+        when (cacheConfig) {
+            is FileApplicationCommandsCacheConfig -> {
+                val dataDirectory = cacheConfig.path
+                if (!dataDirectory.isWritable()) {
+                    // Don't use absolutePathString in case it also produces an exception
+                    logger.warn { "Cannot write to '${dataDirectory.pathString}', try setting a different path in ${BApplicationConfigBuilder::fileCache.shortSignatureNoSrc}, falling back to an in-memory store" }
+                    return MemoryApplicationCommandsCacheFactory(cacheConfig)
+                }
+
+                return FileApplicationCommandsCacheFactory(cacheConfig, jda.selfUser.applicationIdLong)
             }
-            Path(appDataDirectory).resolve("BotCommands")
+            is DatabaseApplicationCommandsCacheConfig -> TODO()
+            else -> throwInternal("Unsupported cache config: $cacheConfig")
         }
-
-        if (!dataDirectory.isWritable()) {
-            // Don't use absolutePathString in case it also produces an exception
-            logger.warn { "Cannot write to '${dataDirectory.pathString}', try setting a different path in ${BApplicationConfig::commandCachePath.reference}, falling back to an in-memory store" }
-            return MemoryApplicationCommandsCacheFactory()
-        }
-
-        return FileApplicationCommandsCacheFactory(dataDirectory.resolve("ApplicationCommands-${jda.selfUser.applicationId}"))
     }
 }
