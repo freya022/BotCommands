3c3bb6b7421ded0070ee1148a7c4043b21d2da13
freya02 <41875020+freya022@users.noreply.github.com>
2024-08-28 18:12:07 +0200

    Use PostgreSQL as the test database
    Improves test startup
    Only use H2 if database schema or queries are changed
    Fallback to H2 if credentials are missing

--- src/test/kotlin/io/github/freya022/botcommands/test/switches/TestDatabase.kt
+++ src/test/kotlin/io/github/freya022/botcommands/test/switches/TestDatabase.kt
@@ -3,28 +3,52 @@ package io.github.freya022.botcommands.test.switches
 import io.github.freya022.botcommands.api.core.service.CustomConditionChecker
 import io.github.freya022.botcommands.api.core.service.ServiceContainer
 import io.github.freya022.botcommands.api.core.service.annotations.Condition
+import io.github.freya022.botcommands.test.config.Config
 import org.springframework.context.annotation.ConditionContext
 import org.springframework.context.annotation.Conditional
 import org.springframework.core.type.AnnotatedTypeMetadata
 import org.springframework.context.annotation.Condition as SpringCondition
 
 object TestDatabaseChecker : CustomConditionChecker<TestDatabase>, SpringCondition {
-    private val databaseType = TestDatabase.DatabaseType.H2
+    private val databaseType = TestDatabase.DatabaseType.POSTGRESQL
 
     override val annotationType: Class<TestDatabase> = TestDatabase::class.java
 
     override fun checkServiceAvailability(serviceContainer: ServiceContainer, checkedClass: Class<*>, annotation: TestDatabase): String? {
-        if (databaseType != annotation.databaseType) {
-            return "${annotation.databaseType} is disabled, $databaseType is used instead"
+        val implDatabaseType = annotation.databaseType
+
+        if (databaseType == TestDatabase.DatabaseType.POSTGRESQL && !hasPostgresCredentials()) {
+            // Fallback to H2 if that's what we're scanning
+            if (implDatabaseType == TestDatabase.DatabaseType.H2) {
+                return null
+            }
+
+            return "No PostgreSQL database name, or user, or password is set, falling back to H2."
+        }
+
+        if (databaseType != implDatabaseType) {
+            return "$implDatabaseType is disabled, $databaseType is used instead"
         }
 
         return null
     }
 
     override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
-        return databaseType == metadata.annotations
-                .get(TestDatabase::class.java)
-                .synthesize().databaseType
+        val implDatabaseType = metadata.annotations
+            .get(TestDatabase::class.java)
+            .synthesize().databaseType
+
+        if (databaseType == TestDatabase.DatabaseType.POSTGRESQL && !hasPostgresCredentials()) {
+            // Fallback to H2 if that's what we're scanning
+            return implDatabaseType == TestDatabase.DatabaseType.H2
+        }
+
+        return databaseType == implDatabaseType
+    }
+
+    private fun hasPostgresCredentials(): Boolean {
+        val databaseConfig = Config.instance.databaseConfig
+        return databaseConfig.name.isNotBlank() && databaseConfig.user.isNotBlank() && databaseConfig.password.isNotBlank()
     }
 }
 
