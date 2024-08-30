7deb53f6275452e3c7704a39ba824dc0bbff882f
freya02 <41875020+freya022@users.noreply.github.com>
2024-08-30 11:52:10 +0200

    Specify configurers only work with Spring

--- src/main/kotlin/io/github/freya022/botcommands/api/core/config/ConfigConfigurers.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/config/ConfigConfigurers.kt
@@ -1,16 +1,23 @@
 package io.github.freya022.botcommands.api.core.config
 
+/**
+ * Base interface for runtime configuration, only usable with Spring, see sub-interfaces.
+ */
 sealed interface BConfigurer<T : Any> {
     fun configure(builder: T)
 }
 
 /**
  * Configurer for [BConfig].
+ * 
+ * Only usable with Spring.
  */
 interface BConfigConfigurer : BConfigurer<BConfigBuilder>
 
 /**
- * Configurer for [BConfig].
+ * Configurer for [BDebugConfig].
+ *
+ * Only usable with Spring.
  */
 @Suppress("DEPRECATION")
 @Deprecated("For removal")
@@ -18,41 +25,57 @@ interface BDebugConfigConfigurer : BConfigurer<BDebugConfigBuilder>
 
 /**
  * Configurer for [BServiceConfig].
+ *
+ * Only usable with Spring.
  */
 @Deprecated("For removal")
 interface BServiceConfigConfigurer : BConfigurer<BServiceConfigBuilder>
 
 /**
  * Configurer for [BDatabaseConfig].
+ *
+ * Only usable with Spring.
  */
 interface BDatabaseConfigConfigurer : BConfigurer<BDatabaseConfigBuilder>
 
 /**
  * Configurer for [BLocalizationConfig].
+ *
+ * Only usable with Spring.
  */
 interface BLocalizationConfigConfigurer : BConfigurer<BLocalizationConfigBuilder>
 
 /**
  * Configurer for [BTextConfig].
+ *
+ * Only usable with Spring.
  */
 interface BTextConfigConfigurer : BConfigurer<BTextConfigBuilder>
 
 /**
  * Configurer for [BApplicationConfig].
+ *
+ * Only usable with Spring.
  */
 interface BApplicationConfigConfigurer : BConfigurer<BApplicationConfigBuilder>
 
 /**
  * Configurer for [BModalsConfig].
+ *
+ * Only usable with Spring.
  */
 interface BModalsConfigConfigurer : BConfigurer<BModalsConfigBuilder>
 
 /**
  * Configurer for [BComponentsConfig].
+ *
+ * Only usable with Spring.
  */
 interface BComponentsConfigConfigurer : BConfigurer<BComponentsConfigBuilder>
 
 /**
  * Configurer for [BCoroutineScopesConfig].
+ *
+ * Only usable with Spring.
  */
 interface BCoroutineScopesConfigConfigurer : BConfigurer<BCoroutineScopesConfigBuilder>
