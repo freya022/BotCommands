70c6209943dc71db0e9c1d6821f05c3682a491f3
freya02 <41875020+freya022@users.noreply.github.com>
2024-08-30 11:24:57 +0200

    Add missing requirement in tests

--- src/test/kotlin/io/github/freya022/botcommands/test/MyTextPrefixSupplier.kt
+++ src/test/kotlin/io/github/freya022/botcommands/test/MyTextPrefixSupplier.kt
@@ -2,10 +2,12 @@ package io.github.freya022.botcommands.test
 
 import io.github.freya022.botcommands.api.commands.text.TextCommandsContext
 import io.github.freya022.botcommands.api.commands.text.TextPrefixSupplier
+import io.github.freya022.botcommands.api.commands.text.annotations.RequiresTextCommands
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel
 
 @BService
+@RequiresTextCommands
 class MyTextPrefixSupplier(private val textCommandsContext: TextCommandsContext) : TextPrefixSupplier {
     override fun getPrefixes(channel: GuildMessageChannel): List<String> = when {
         channel.idLong == 1274378050154270720L -> emptyList() // No text commands
