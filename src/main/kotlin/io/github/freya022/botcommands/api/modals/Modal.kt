b1325288fade5ca48c5454fd29c77ba44cde8875
freya02 <41875020+freya022@users.noreply.github.com>
2024-09-29 14:14:20 +0200

    Add Modal#awaitOrNull

--- src/main/kotlin/io/github/freya022/botcommands/api/modals/Modal.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/modals/Modal.kt
@@ -1,10 +1,17 @@
 package io.github.freya022.botcommands.api.modals
 
 import io.github.freya022.botcommands.internal.modals.ModalMaps
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.suspendCancellableCoroutine
 import net.dv8tion.jda.api.interactions.modals.Modal as JDAModal
 
 class Modal internal constructor(modal: JDAModal, private val modalMaps: ModalMaps) : JDAModal by modal {
+    /**
+     * Suspends until the modal is submitted, and returns the event.
+     *
+     * @throws TimeoutCancellationException If the timeout set in the modal builder has been reached
+     */
+    @JvmSynthetic
     suspend fun await(): ModalEvent {
         return suspendCancellableCoroutine { continuation ->
             val internalId = ModalMaps.parseModalId(id)
@@ -14,5 +21,16 @@ class Modal internal constructor(modal: JDAModal, private val modalMaps: ModalMa
                 modalMaps.removeContinuation(internalId, continuation)
             }
         }
+    }
+
+    /**
+     * Suspends until the modal is submitted, and returns the event,
+     * or `null` if the timeout has been reached.
+     */
+    @JvmSynthetic
+    suspend fun awaitOrNull(): ModalEvent? = try {
+        await()
+    } catch (e: TimeoutCancellationException) {
+        null
     }
 }
