f3c2513a6d1ea5eb031dd62194fe754410d9a85b
freya02 <41875020+freya022@users.noreply.github.com>
2024-09-30 21:41:12 +0200

    Adapt ServiceMethodOption

--- src/main/kotlin/io/github/freya022/botcommands/internal/parameters/ServiceMethodOption.kt
+++ src/main/kotlin/io/github/freya022/botcommands/internal/parameters/ServiceMethodOption.kt
@@ -1,5 +1,6 @@
 package io.github.freya022.botcommands.internal.parameters
 
+import io.github.freya022.botcommands.api.core.service.LazyService
 import io.github.freya022.botcommands.api.core.service.ServiceContainer
 import io.github.freya022.botcommands.api.parameters.AggregatedParameter
 import io.github.freya022.botcommands.internal.core.options.OptionImpl
@@ -18,7 +19,7 @@ internal class ServiceMethodOption internal constructor(
 
     // Caches the service if:
     // 1. Is non-null
-    // 2. Is not Lazy/List
+    // 2. Is not LazyService/List
     internal fun getService(): Any? {
         if (::cachedService.isInitialized) return cachedService
 
@@ -32,8 +33,8 @@ internal class ServiceMethodOption internal constructor(
         // Always retry getting lazy services and lists as they may be updated later
         if (service is List<*>)
             return service
-        if (service is Lazy<*>) {
-            if (service.isInitialized() && service.value != null)
+        if (service is LazyService<*>) {
+            if (service.isInitialized())
                 cachedService = service
             return service
         } else {
