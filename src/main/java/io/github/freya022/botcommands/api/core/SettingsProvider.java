edf6ebaf493a9f46bb9f27a27fb8e644ac98d59e
freya02 <41875020+freya022@users.noreply.github.com>
2024-09-30 21:58:01 +0200

    Remove SettingsProvider#getLocale

--- src/main/java/io/github/freya022/botcommands/api/core/SettingsProvider.java
+++ src/main/java/io/github/freya022/botcommands/api/core/SettingsProvider.java
@@ -6,19 +6,13 @@ import io.github.freya022.botcommands.api.commands.application.annotations.Decla
 import io.github.freya022.botcommands.api.commands.text.TextPrefixSupplier;
 import io.github.freya022.botcommands.api.core.service.annotations.BService;
 import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService;
-import io.github.freya022.botcommands.api.localization.DefaultMessages;
-import io.github.freya022.botcommands.api.localization.interaction.GuildLocaleProvider;
-import io.github.freya022.botcommands.api.localization.interaction.UserLocaleProvider;
-import io.github.freya022.botcommands.api.localization.text.TextCommandLocaleProvider;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
-import net.dv8tion.jda.api.interactions.DiscordLocale;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 import java.util.function.Predicate;
 
 /**
@@ -65,26 +59,5 @@ public interface SettingsProvider {
     @Deprecated
     default CommandList getGuildCommands(@NotNull Guild guild) {
         return CommandList.all();
-    }
-
-    /**
-     * Returns the {@link Locale} of the given {@link Guild}, will be null for a global context
-     * <br>This might be used for localization such as in default messages or application commands
-     *
-     * @param guild The target {@link Guild} to get the {@link Locale} from
-     *
-     * @return The {@link Locale} of the specified guild
-     *
-     * @see DefaultMessages
-     *
-     * @deprecated Replaced by {@link TextCommandLocaleProvider} / {@link UserLocaleProvider} / {@link GuildLocaleProvider}
-     */
-    @NotNull
-    @Deprecated
-    default DiscordLocale getLocale(@Nullable Guild guild) {
-        if (guild != null) return guild.getLocale();
-
-        //Discord default locale is US english
-        return DiscordLocale.ENGLISH_US;
     }
 }
