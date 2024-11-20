9c85119775a23a6a3217f2e8544229459f8b2afb
freya02 <41875020+freya022@users.noreply.github.com>
2024-11-20 18:33:21 +0100

    Add nullability info to LocalizationMapReader#appendPath

--- src/main/java/io/github/freya022/botcommands/api/localization/readers/LocalizationMapReader.java
+++ src/main/java/io/github/freya022/botcommands/api/localization/readers/LocalizationMapReader.java
@@ -7,6 +7,7 @@ import io.github.freya022.botcommands.api.localization.LocalizationMapRequest;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 
 /**
@@ -29,7 +30,8 @@ public interface LocalizationMapReader {
      *
      * @return The new path
      */
-    default String appendPath(String path, String other) {
+    @Nonnull
+    default String appendPath(@Nonnull String path, @Nonnull String other) {
         if (path.isBlank()) return other;
 
         return path + '.' + other;
