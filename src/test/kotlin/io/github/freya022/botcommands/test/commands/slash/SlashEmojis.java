e2e2dd597ea077cb232c7fe269de00c7a16aed13
freya02 <41875020+freya022@users.noreply.github.com>
2025-02-04 11:49:00 +0100

    Add showcase test for jda-emojis

---
+++ src/test/kotlin/io/github/freya022/botcommands/test/commands/slash/SlashEmojis.java
@@ -0,0 +1,41 @@
+package io.github.freya022.botcommands.test.commands.slash;
+
+import dev.freya02.jda.emojis.Emojis;
+import io.github.freya022.botcommands.api.commands.annotations.Command;
+import io.github.freya022.botcommands.api.commands.application.ApplicationCommand;
+import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashEvent;
+import io.github.freya022.botcommands.api.commands.application.slash.annotations.JDASlashCommand;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.interactions.components.selections.SelectOption;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+
+@Command
+public class SlashEmojis extends ApplicationCommand {
+
+    @JDASlashCommand(name = "emojis")
+    public void onSlashEmojis(GuildSlashEvent event) {
+        // For jda-emojis demonstration purposes, this does not work with BC
+        Button approve = Button.primary("approve:data", "Approve").withEmoji(Emojis.WHITE_CHECK_MARK);
+        StringSelectMenu choices = StringSelectMenu.create("choices:data")
+                .addOptions(
+                        SelectOption.of("First choice", "1").withEmoji(Emojis.ONE)
+                )
+                .setDefaultValues("1")
+                .build();
+
+        MessageCreateData messageData = new MessageCreateBuilder()
+                .addActionRow(approve)
+                .addActionRow(choices)
+                .build();
+
+        event.reply(messageData)
+                // Since we reply to a slash command, we need to retrieve the message back
+                .flatMap(InteractionHook::retrieveOriginal)
+                // This emoji's name is slightly different as fields can't start with digits
+                .flatMap(message -> message.addReaction(Emojis.HUNDRED_POINTS))
+                .queue();
+    }
+}
