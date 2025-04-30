2a00324454327d619c1c7913b2df93fa5ad6896c
freya02 <41875020+freya022@users.noreply.github.com>
2025-04-30 21:50:24 +0200

    tests: Simplify SpringMain.kt

--- src/test/kotlin/io/github/freya022/botcommands/spring/test/SpringMain.kt
+++ src/test/kotlin/io/github/freya022/botcommands/spring/test/SpringMain.kt
@@ -1,13 +1,10 @@
 package io.github.freya022.botcommands.spring.test
 
-import dev.reformator.stacktracedecoroutinator.jvm.DecoroutinatorJvmApi
 import io.github.freya022.botcommands.test.config.Environment
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.lang.management.ManagementFactory
 import kotlin.io.path.absolutePathString
-import kotlin.system.exitProcess
 
 @SpringBootApplication(
         scanBasePackages = [
@@ -23,27 +20,9 @@ fun main(args: Array<String>) {
     System.setProperty("logging.config", Environment.logbackConfigPath.absolutePathString())
     logger.info { "Loading logback configuration at ${Environment.logbackConfigPath.absolutePathString()}" }
 
-    // I use hotswap agent to update my code without restarting the bot
-    // Of course this only supports modifying existing code
-    // Refer to https://github.com/HotswapProjects/HotswapAgent#readme on how to use hotswap
+    // No need to install stacktrace-decoroutinator
+    // The stack traces are already enhanced in debug mode by the Java agent provided by IntelliJ
+    // The IntelliJ debug agent also doesn't cause errors when exceptions happen after transforming reloaded classes
 
-    // stacktrace-decoroutinator has issues when reloading with hotswap agent
-    if ("-XX:+AllowEnhancedClassRedefinition" in ManagementFactory.getRuntimeMXBean().inputArguments) {
-        logger.info { "Skipping stacktrace-decoroutinator as enhanced hotswap is active" }
-    } else if ("--no-decoroutinator" in args) {
-        logger.info { "Skipping stacktrace-decoroutinator as --no-decoroutinator is specified" }
-    } else {
-        DecoroutinatorJvmApi.install()
-    }
-
-    try {
-        runApplication<SpringMain>(*args)
-    } catch (e: Exception) {
-        // Don't handle the exception sent by DevTools
-        if (e.javaClass.name == "org.springframework.boot.devtools.restart.SilentExitExceptionHandler\$SilentExitException")
-            return
-
-        logger.catching(e)
-        exitProcess(1)
-    }
+    runApplication<SpringMain>(*args)
 }
