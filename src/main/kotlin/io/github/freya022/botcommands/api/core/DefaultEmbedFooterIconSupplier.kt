ad6a804571dadbf70f267369c72aba2ba709e55a
freya02 <41875020+freya022@users.noreply.github.com>
2025-06-25 18:19:44 +0200

    Convert DefaultEmbed* suppliers to Kotlin
    Konsist can't read Java sources

--- src/main/kotlin/io/github/freya022/botcommands/api/core/DefaultEmbedFooterIconSupplier.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/DefaultEmbedFooterIconSupplier.kt
@@ -1,25 +1,21 @@
-package io.github.freya022.botcommands.api.core;
+package io.github.freya022.botcommands.api.core
 
-import io.github.freya022.botcommands.api.commands.text.BaseCommandEvent;
-import io.github.freya022.botcommands.api.core.service.annotations.BService;
-import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.InputStream;
+import io.github.freya022.botcommands.api.commands.text.BaseCommandEvent
+import io.github.freya022.botcommands.api.core.service.annotations.BService
+import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService
+import java.io.InputStream
 
 /**
- * Interface for embed footer icons requested by {@link BaseCommandEvent#getDefaultIconStream()}.
+ * Interface for embed footer icons requested by [BaseCommandEvent.getDefaultIconStream].
  *
- * <p>Returns {@code null} by default.
+ * Returns `null` by default.
  *
- * <p>
- * <b>Usage</b>: Register your instance as a service with {@link BService}.
+ * **Usage**: Register your instance as a service with [BService].
  *
  * @see InterfacedService @InterfacedService
  */
 @InterfacedService(acceptMultiple = false)
-public interface DefaultEmbedFooterIconSupplier {
+fun interface DefaultEmbedFooterIconSupplier {
 
-    @Nullable
-    InputStream get();
+    fun get(): InputStream?
 }
--- src/main/kotlin/io/github/freya022/botcommands/api/core/DefaultEmbedSupplier.kt
+++ src/main/kotlin/io/github/freya022/botcommands/api/core/DefaultEmbedSupplier.kt
@@ -1,24 +1,22 @@
-package io.github.freya022.botcommands.api.core;
+package io.github.freya022.botcommands.api.core
 
-import io.github.freya022.botcommands.api.commands.text.BaseCommandEvent;
-import io.github.freya022.botcommands.api.core.service.annotations.BService;
-import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService;
-import net.dv8tion.jda.api.EmbedBuilder;
-import org.jetbrains.annotations.NotNull;
+import io.github.freya022.botcommands.api.commands.text.BaseCommandEvent
+import io.github.freya022.botcommands.api.core.service.annotations.BService
+import io.github.freya022.botcommands.api.core.service.annotations.InterfacedService
+import net.dv8tion.jda.api.EmbedBuilder
 
 /**
- * Interface for embeds requested by {@link BaseCommandEvent#getDefaultEmbed()}, aiming to reduce boilerplate.
- * <br>This embed is also used in the default help command.
+ * Interface for embeds requested by [BaseCommandEvent.getDefaultEmbed], aiming to reduce boilerplate.
+ * This embed is also used in the default help command.
  *
- * <p>Returns an empty {@link EmbedBuilder} by default.
+ * Returns an empty [EmbedBuilder] by default.
  *
- * <p>
- * <b>Usage</b>: Register your instance as a service with {@link BService}.
+ * **Usage**: Register your instance as a service with [BService].
  *
  * @see InterfacedService @InterfacedService
  */
 @InterfacedService(acceptMultiple = false)
-public interface DefaultEmbedSupplier {
-    @NotNull
-    EmbedBuilder get();
+fun interface DefaultEmbedSupplier {
+
+    fun get(): EmbedBuilder
 }
