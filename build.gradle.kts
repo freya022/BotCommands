2e513d6cd295c9f8114ae459de40848d6b9f8392
freya02 <41875020+freya022@users.noreply.github.com>
2025-07-13 20:35:32 +0200

    Use version catalog update plugin (#238)
    * Add plugin
    * Format libs.versions.toml
    * Use version for all bucket4j deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
+import nl.littlerobots.vcu.plugin.resolver.VersionSelectors
+
 plugins {
     id("BotCommands-repositories-conventions")
     id("BotCommands-publish-conventions")
+
+    alias(libs.plugins.version.catalog.update)
+}
+
+versionCatalogUpdate {
+    versionSelector(VersionSelectors.STABLE)
 }
 
 // The root project script is used to produce an aggregated POM
