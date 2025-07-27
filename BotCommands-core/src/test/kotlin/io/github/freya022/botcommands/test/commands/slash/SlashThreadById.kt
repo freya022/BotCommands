def6ef40cbd0e3913646fda3bc2595ceeb64ad38
freya02 <41875020+freya022@users.noreply.github.com>
2025-07-27 17:41:21 +0200

    tests: Fix deprecated usages in SlashThreadById

--- BotCommands-core/src/test/kotlin/io/github/freya022/botcommands/test/commands/slash/SlashThreadById.kt
+++ BotCommands-core/src/test/kotlin/io/github/freya022/botcommands/test/commands/slash/SlashThreadById.kt
@@ -1,7 +1,7 @@
 package io.github.freya022.botcommands.test.commands.slash
 
 import dev.freya02.botcommands.jda.ktx.messages.reply_
-import dev.freya02.botcommands.jda.ktx.retrieve.retrieveThreadChannelOrNull
+import dev.freya02.botcommands.jda.ktx.retrieve.retrieveThreadChannelByIdOrNull
 import io.github.freya022.botcommands.api.commands.annotations.Command
 import io.github.freya022.botcommands.api.commands.application.ApplicationCommand
 import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashEvent
@@ -12,7 +12,7 @@ import io.github.freya022.botcommands.api.commands.application.slash.annotations
 class SlashThreadById : ApplicationCommand() {
     @JDASlashCommand(name = "thread_by_id")
     suspend fun execute(event: GuildSlashEvent, @SlashOption id: String) {
-        val threadChannel = event.guild.retrieveThreadChannelOrNull(id.toLong())
+        val threadChannel = event.guild.retrieveThreadChannelByIdOrNull(id.toLong())
         event.reply_(threadChannel?.asMention.toString(), ephemeral = true).queue()
     }
 }
