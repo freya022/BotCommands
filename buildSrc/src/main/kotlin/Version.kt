9a312dbc714f1d8c1fc8066a0208166062581b69
freya02 <41875020+freya022@users.noreply.github.com>
2025-08-25 17:51:16 +0200

    Remove unused buildSrc code

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -21,19 +21,4 @@ data class Version(
     private fun readObject(out: ObjectInputStream) {
         out.defaultReadObject()
     }
-
-    companion object {
-        private val versionPattern = Regex("""(\d+)\.(\d+)\.(\d+)(?:-(\w+\.\d+))?(?:_DEV)?""")
-
-        fun parseOrNull(version: String): Version? {
-            val groups = versionPattern.matchEntire(version)?.groups ?: return null
-
-            val major = groups[1]?.value ?: return null
-            val minor = groups[2]?.value ?: return null
-            val revision = groups[3]?.value ?: return null
-            val classifier = groups[4]?.value
-
-            return Version(major, minor, revision, classifier, version.endsWith("_DEV"))
-        }
-    }
 }
