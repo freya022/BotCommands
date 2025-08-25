eae76293ceb95b47c9c2aac7bf88e28845c6cd72
freya02 <41875020+freya022@users.noreply.github.com>
2025-08-25 17:58:08 +0200

    Add `@JvmStatic` on defaults of application command managers

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/application/provider/GlobalApplicationCommandManager.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/application/provider/GlobalApplicationCommandManager.kt
@@ -16,16 +16,18 @@ class GlobalApplicationCommandManager internal constructor(context: BContext): A
 
     object Defaults {
         /**
-         * Default value of [TopLevelApplicationCommandBuilder.contexts].
+         * Default value of [TopLevelApplicationCommandBuilder.contexts] and annotated application commands.
          *
          * Defaults to [InteractionContextType.GUILD], can be edited.
          */
+        @JvmStatic
         var contexts: Set<InteractionContextType> = setOf(InteractionContextType.GUILD)
         /**
-         * Default value of [TopLevelApplicationCommandBuilder.integrationTypes].
+         * Default value of [TopLevelApplicationCommandBuilder.integrationTypes] and annotated application commands.
          *
          * Defaults to [IntegrationType.GUILD_INSTALL], can be edited.
          */
+        @JvmStatic
         var integrationTypes: Set<IntegrationType> = setOf(IntegrationType.GUILD_INSTALL)
     }
 }
--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/application/provider/GuildApplicationCommandManager.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/application/provider/GuildApplicationCommandManager.kt
@@ -17,16 +17,18 @@ class GuildApplicationCommandManager internal constructor(context: BContext, val
 
     object Defaults {
         /**
-         * Default value of [TopLevelApplicationCommandBuilder.contexts].
+         * Default value of [TopLevelApplicationCommandBuilder.contexts] and annotated application commands.
          *
          * Defaults to [InteractionContextType.GUILD], can be edited.
          */
+        @JvmStatic
         var contexts: Set<InteractionContextType> = setOf(InteractionContextType.GUILD)
         /**
-         * Default value of [TopLevelApplicationCommandBuilder.integrationTypes].
+         * Default value of [TopLevelApplicationCommandBuilder.integrationTypes] and annotated application commands.
          *
          * Defaults to [IntegrationType.GUILD_INSTALL], can be edited.
          */
+        @JvmStatic
         var integrationTypes: Set<IntegrationType> = setOf(IntegrationType.GUILD_INSTALL)
     }
 }
