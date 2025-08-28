cf1deed3eb2c7ec8f01ebe8a07a9ac08034a2c9c
freya02 <41875020+freya022@users.noreply.github.com>
2025-08-28 19:00:42 +0200

    Remove `data` modifier from `TimeoutInfo`

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/pagination/TimeoutInfo.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/pagination/TimeoutInfo.kt
@@ -2,7 +2,7 @@ package io.github.freya022.botcommands.api.pagination
 
 import kotlin.time.Duration
 
-data class TimeoutInfo<T : AbstractPagination<T>>(
+class TimeoutInfo<T : AbstractPagination<T>>(
     val timeout: Duration,
     val onTimeout: (suspend (T) -> Unit)?
 ) 
