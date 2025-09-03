d61789a66f5ccb60f862cec615bf0c16e9bb0613
freya02 <41875020+freya022@users.noreply.github.com>
2025-09-04 00:00:13 +0200

    examples: Extend `/format` to have a `StringSelectMenu` for languages

--- BotCommands-core/src/examples/kotlin/io/github/freya022/bot/commands/slash/SlashFormat.kt
+++ BotCommands-core/src/examples/kotlin/io/github/freya022/bot/commands/slash/SlashFormat.kt
@@ -1,5 +1,6 @@
 package io.github.freya022.bot.commands.slash
 
+import dev.freya02.botcommands.jda.ktx.components.SelectOption
 import dev.freya02.botcommands.jda.ktx.components.TextInput
 import dev.freya02.botcommands.jda.ktx.messages.reply_
 import io.github.freya022.botcommands.api.commands.annotations.Command
@@ -9,32 +10,47 @@ import io.github.freya022.botcommands.api.commands.application.slash.annotations
 import io.github.freya022.botcommands.api.modals.Modals
 import io.github.freya022.botcommands.api.modals.annotations.RequiresModals
 import io.github.freya022.botcommands.api.modals.create
+import net.dv8tion.jda.api.components.selections.StringSelectMenu
 import net.dv8tion.jda.api.components.textinput.TextInputStyle
 
-private const val codeInputName = "SlashModal: codeInput"
+private const val codeInputId = "SlashModal: codeInput"
+private const val languageInputId = "SlashModal: languageInput"
 
 @Command
 @RequiresModals
-class SlashModal(private val modals: Modals) : ApplicationCommand() {
+class SlashFormat(private val modals: Modals) : ApplicationCommand() {
+
     @JDASlashCommand(name = "format", description = "Formats your code")
     suspend fun onSlashFormat(event: GuildSlashEvent) {
         val modal = modals.create("Format your code") {
             label("Code") {
-                child = TextInput(codeInputName, TextInputStyle.PARAGRAPH) {
+                child = TextInput(codeInputId, TextInputStyle.PARAGRAPH) {
                     minLength = 3
                 }
+            }
+
+            label("Language") {
+                // TODO add InlineStringSelectMenu + others
+                child = StringSelectMenu.create(languageInputId)
+                    .addOptions(
+                        SelectOption("Kotlin", "kt"),
+                        SelectOption("Java", "java"),
+                    )
+                    .build()
             }
         }
         event.replyModal(modal).queue()
 
         val modalEvent = modal.await()
-        val code = modalEvent.values.first().asString
+        val code = modalEvent.values[0].asString
+        val language = modalEvent.values[1].asStringList[0]
 
-        modalEvent.reply_(
-            """
+        modalEvent.reply_(ephemeral = true) {
+            content = """
                 Here is your formatted code:
-                ```kt
+                ```$language
                 $code```
-            """.trimIndent(), ephemeral = true).queue()
+            """.trimIndent()
+        }.queue()
     }
 }
