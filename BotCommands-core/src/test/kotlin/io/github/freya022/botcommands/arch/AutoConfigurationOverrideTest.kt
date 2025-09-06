6a3765fb68b19a8a5f26c87d3d1646a39df51956
freya02 <41875020+freya022@users.noreply.github.com>
2025-09-06 18:07:32 +0200

    Move unit tests to packages

---
+++ BotCommands-core/src/test/kotlin/io/github/freya022/botcommands/arch/AutoConfigurationOverrideTest.kt
@@ -0,0 +1,75 @@
+package io.github.freya022.botcommands.arch
+
+import io.github.classgraph.AnnotationClassRef
+import io.github.classgraph.AnnotationInfo
+import io.github.classgraph.ClassGraph
+import io.github.freya022.botcommands.api.core.BotCommands
+import io.github.freya022.botcommands.api.core.service.annotations.ConditionalOnMissingService
+import io.github.freya022.botcommands.api.core.service.getService
+import io.github.freya022.botcommands.helpers.utils.createTest
+import io.github.freya022.botcommands.internal.core.service.annotations.InternalAutoConfiguration
+import io.mockk.mockkClass
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.FieldSource
+import kotlin.reflect.KClass
+import kotlin.test.assertSame
+
+object AutoConfigurationOverrideTest {
+
+    private lateinit var autoconfiguredTypes: Set<KClass<*>>
+
+    @JvmStatic
+    @BeforeAll
+    fun setup() {
+        val autoconfiguredTypes = hashSetOf<KClass<*>>()
+
+        ClassGraph()
+            .acceptPackages("io.github.freya022.botcommands", "dev.freya02.botcommands")
+            .enableClassInfo()
+            .enableMethodInfo()
+            .enableAnnotationInfo()
+            .scan()
+            .use { scan ->
+                val autoConfigurationClasses = scan.getClassesWithAnnotation(InternalAutoConfiguration::class.java)
+                autoConfigurationClasses.forEach { autoConfigurationClass ->
+                    @Suppress("UNCHECKED_CAST")
+                    fun AnnotationInfo.getRequiredMissingBeans(): List<KClass<*>> {
+                        val requiredMissingBeans = parameterValues["value"].value as Array<*>
+                        return requiredMissingBeans.map { (it as AnnotationClassRef).loadClass().kotlin }
+                    }
+
+                    autoConfigurationClass
+                        .getAnnotationInfo(ConditionalOnMissingService::class.java)
+                        ?.also { conditionalOnMissingBean ->
+                            autoconfiguredTypes += conditionalOnMissingBean.getRequiredMissingBeans()
+                        }
+
+                    autoConfigurationClass.declaredMethodInfo.forEach { method ->
+                        method
+                            .getAnnotationInfo(ConditionalOnMissingService::class.java)
+                            ?.also { conditionalOnMissingBean ->
+                                autoconfiguredTypes += conditionalOnMissingBean.getRequiredMissingBeans()
+                            }
+                    }
+                }
+            }
+
+        this.autoconfiguredTypes = autoconfiguredTypes
+    }
+
+    @ParameterizedTest
+    @FieldSource("autoconfiguredTypes")
+    fun `Can override auto configured services`(autoconfiguredType: KClass<Any>) {
+        val expected = mockkClass(autoconfiguredType, relaxed = true)
+        val context = BotCommands.createTest {
+            services {
+                registerServiceSupplier(primaryType = autoconfiguredType) { expected }
+            }
+        }
+
+        val actual = assertDoesNotThrow { context.getService(autoconfiguredType) }
+        assertSame(expected, actual)
+    }
+}
---
+++ BotCommands-core/src/test/kotlin/io/github/freya022/botcommands/components/ComponentTests.kt
@@ -0,0 +1,59 @@
+package io.github.freya022.botcommands.components
+
+import ch.qos.logback.classic.ClassicConstants
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.LoggerContext
+import io.github.freya022.botcommands.api.components.Buttons
+import io.github.freya022.botcommands.api.core.BContext
+import io.github.freya022.botcommands.api.core.BotCommands
+import io.github.freya022.botcommands.api.core.service.getService
+import io.github.freya022.botcommands.helpers.config.Environment
+import io.github.freya022.botcommands.helpers.db.TestH2Source
+import io.github.freya022.botcommands.helpers.utils.createTest
+import io.github.freya022.botcommands.internal.components.controller.ComponentController
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import kotlin.io.path.absolutePathString
+
+object ComponentTests {
+    private lateinit var context: BContext
+
+    private val buttons: Buttons by lazy { context.getService() }
+    private val componentController: ComponentController by lazy { context.getService() }
+
+    @JvmStatic
+    @BeforeAll
+    fun setup() {
+        System.setProperty(ClassicConstants.CONFIG_FILE_PROPERTY, Environment.logbackConfigPath.absolutePathString())
+        (LoggerFactory.getILoggerFactory() as LoggerContext).loggerList.forEach { it.level = Level.WARN }
+
+        context = BotCommands.createTest(components = true) {
+            addClass<TestH2Source>()
+        }
+    }
+
+    @Test
+    fun `Group delete results in owned components deleted`(): Unit = runBlocking {
+        val button = buttons.primary("test").persistent { }
+        val group = buttons.group(button).ephemeral { }
+
+        buttons.deleteComponents(group)
+
+        assertNull(componentController.getActiveComponent(group.internalId))
+        assertNull(componentController.getActiveComponent(button.internalId))
+    }
+
+    @Test
+    fun `Component delete results in owner group deleted`(): Unit = runBlocking {
+        val button = buttons.primary("test").persistent { }
+        val group = buttons.group(button).ephemeral { }
+
+        buttons.deleteComponents(button)
+
+        assertNull(componentController.getActiveComponent(group.internalId))
+        assertNull(componentController.getActiveComponent(button.internalId))
+    }
+}
