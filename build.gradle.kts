64c939370add021a4b42f001602cc4c3b11fa86d
freya02 <41875020+freya022@users.noreply.github.com>
2025-09-15 22:58:22 +0200

    Move test bot sources to module
    As Gradle publishes its own metadata saying the root module requires Java 24, which is false.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import nl.littlerobots.vcu.plugin.resolver.VersionSelectors
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     id("BotCommands-conventions")
@@ -10,10 +9,6 @@ plugins {
 
 versionCatalogUpdate {
     versionSelector(VersionSelectors.STABLE)
-}
-
-sourceSets {
-    register("testBot")
 }
 
 // The root project script is used to produce an aggregated POM
@@ -29,60 +24,11 @@ dependencies {
     testImplementation(libs.konsist)
     testImplementation(libs.kotlin.metadata)
 
-    // ---------------------------- TEST BOT DEPENDENCIES ---------------------------
-
-    "testBotImplementation"(projects.botCommandsCore)
-    "testBotImplementation"(libs.kotlin.logging)
-    "testBotImplementation"(projects.botCommandsJdaKtx)
-
-    // Logging
-    "testBotImplementation"(libs.logback.classic)
-
-    // Coroutines
-    "testBotImplementation"(libs.stacktrace.decoroutinator)
-
-    // Database
-    "testBotRuntimeOnly"(libs.postgresql)
-    "testBotRuntimeOnly"(libs.h2)
-    "testBotImplementation"(libs.flyway.core)
-    "testBotRuntimeOnly"(libs.flyway.database.postgresql)
-    "testBotImplementation"(libs.hikaricp)
-
-    // Persistent rate limiting
-    "testBotImplementation"(libs.bucket4j.jdk17.postgresql)
-
-    // Upgrade because kotlinx-coroutines-debug somehow has an ANCIENT version
-    "testBotRuntimeOnly"(libs.bytebuddy)
-    "testBotRuntimeOnly"(libs.bytebuddy.agent)
-
-    "testBotRuntimeOnly"(projects.botCommandsMethodAccessors.classfile)
-
-    // ---------------------------- SPRING TEST BOT DEPENDENCIES ---------------------------
-
-    // Spring module
-    "testBotImplementation"(projects.botCommandsSpring)
-
-    // Spring Boot
-    "testBotImplementation"(libs.spring.boot.starter)
-    "testBotRuntimeOnly"(libs.spring.boot.devtools)
-
     // ---------------------- AGGREGATED DOCS DEPENDENCIES ----------------------
 
     dokka(projects.botCommandsCore)
     dokka(projects.botCommandsSpring)
     dokka(projects.botCommandsJdaKtx)
-}
-
-// This does not mean the library requires 24+, since the root module only has project-wide tests
-java {
-    sourceCompatibility = JavaVersion.VERSION_24
-    targetCompatibility = JavaVersion.VERSION_24
-}
-
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_24
-    }
 }
 
 kotlin {
---
+++ test-bot/build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("BotCommands-conventions")
+}
+
+// The root project script is used to produce an aggregated POM
+
+dependencies {
+    implementation(projects.botCommandsCore)
+    implementation(libs.kotlin.logging)
+    implementation(projects.botCommandsJdaKtx)
+
+    // Logging
+    implementation(libs.logback.classic)
+
+    // Coroutines
+    implementation(libs.stacktrace.decoroutinator)
+
+    // Database
+    runtimeOnly(libs.postgresql)
+    runtimeOnly(libs.h2)
+    implementation(libs.flyway.core)
+    runtimeOnly(libs.flyway.database.postgresql)
+    implementation(libs.hikaricp)
+
+    // Persistent rate limiting
+    implementation(libs.bucket4j.jdk17.postgresql)
+
+    // Upgrade because kotlinx-coroutines-debug somehow has an ANCIENT version
+    runtimeOnly(libs.bytebuddy)
+    runtimeOnly(libs.bytebuddy.agent)
+
+    runtimeOnly(projects.botCommandsMethodAccessors.classfile)
+
+    // ---------------------------- SPRING TEST BOT DEPENDENCIES ---------------------------
+
+    // Spring module
+    implementation(projects.botCommandsSpring)
+
+    // Spring Boot
+    implementation(libs.spring.boot.starter)
+    runtimeOnly(libs.spring.boot.devtools)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_24
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            "-Xcontext-parameters",
+        )
+    }
+}
