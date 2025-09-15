c3c2a5b0a04bd9faf37835edf0a159554e267dcc
freya02 <41875020+freya022@users.noreply.github.com>
2025-09-15 17:48:33 +0200

    Support equals/hashCode on InputUser

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/entities/InputUserImpl.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/entities/InputUserImpl.kt
@@ -11,6 +11,23 @@ internal class InputUserImpl internal constructor(
     constructor(member: Member) : this(member.user, member)
     constructor(user: User) : this(user, null)
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is InputUserImpl) return false
+
+        return when {
+            member != null || other.member != null -> member == other.member
+            else -> user == other.user
+        }
+    }
+
+    override fun hashCode(): Int {
+        return when {
+            member != null -> member.hashCode()
+            else -> user.hashCode()
+        }
+    }
+
     override fun toString(): String {
         return when {
             member != null -> "InputUser($member)"
