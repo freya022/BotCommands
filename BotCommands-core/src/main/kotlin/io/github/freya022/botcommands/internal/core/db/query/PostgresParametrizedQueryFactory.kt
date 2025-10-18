471d14e579ee98a4d28ca24a11a7ae3c6e34b9c6
freya02 <41875020+freya022@users.noreply.github.com>
2025-10-18 20:04:54 +0200

    db: Only use pgjdbc's query representation for PostgreSQL query logs
    It does not seem possible to reliably replace the values pgjdbc could not replace itself,
    as we can't know when the pgjdbc-replaced value will end, we risk seeking into it.

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/db/query/PostgresParametrizedQueryFactory.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/db/query/PostgresParametrizedQueryFactory.kt
@@ -6,15 +6,13 @@ import io.github.freya022.botcommands.api.core.db.query.AbstractParametrizedQuer
 import io.github.freya022.botcommands.api.core.db.query.ParametrizedQueryFactory
 import io.github.freya022.botcommands.api.core.service.annotations.BService
 import io.github.freya022.botcommands.api.core.service.annotations.Lazy
-import io.github.freya022.botcommands.internal.core.exceptions.internalErrorMessage
-import io.github.oshai.kotlinlogging.KotlinLogging
 import java.sql.Connection
 import java.sql.DatabaseMetaData
 import java.sql.PreparedStatement
 
 /**
  * This lets pgjdbc fill out the parameters by itself,
- * then we replace those it hasn't replaced.
+ * if a parameter is not replaced, it should be contributed to pgjdbc.
  */
 @Lazy
 @BService
@@ -35,65 +33,7 @@ internal object PostgresParametrizedQueryFactory : ParametrizedQueryFactory<Post
             if (values.isEmpty)
                 return removeCommentsAndInline(rawSql)
 
-            val queryParts = splitByQueryParameter(rawSql)
-            val postgresQuery = preparedStatement.toString()
-
-            // Rebuild the query using the split parts,
-            // but for each part, check if there is a '?' after it,
-            // if absent, it means postgres replaced it
-            return removeCommentsAndInline(buildString {
-                var lastIndex = 0
-                // Drop last part as it has no query parameter after it
-                for ((i, part) in queryParts.dropLast(1).withIndex()) {
-                    val partIndex = postgresQuery.indexOf(part, lastIndex)
-                    if (partIndex == -1) {
-                        logger.error { internalErrorMessage("Could not find part #$i '${part}' in '$postgresQuery'") }
-                        return removeCommentsAndInline(postgresQuery)
-                    }
-
-                    append(part)
-                    if (postgresQuery[partIndex + part.length] == '?') {
-                        // The driver did not replace the value, use our own replacement
-                        append(formatParameter(values[i + 1]))
-                        lastIndex = partIndex + part.length + 1
-                    } else {
-                        // The driver replaced the value, find where it stops based on the next part
-                        val nextPart = queryParts[i + 1]
-                        if (nextPart.isBlank()) { // When the query ends with a parameter, the last part is empty
-                            append(postgresQuery.substring(partIndex + part.length))
-                            break
-                        }
-                        val nextPartIndex = postgresQuery.indexOf(nextPart, partIndex)
-                        append(postgresQuery.substring(partIndex + part.length, nextPartIndex))
-                        lastIndex = nextPartIndex
-                    }
-                }
-
-                append(queryParts.last())
-            })
-        }
-
-        private fun splitByQueryParameter(query: String): List<String> = buildList {
-            val builder = StringBuilder()
-            var inComment = false
-            for ((index, char) in query.withIndex()) {
-                if (char == '?' && !inComment) {
-                    add(builder.toString())
-                    builder.clear()
-                } else {
-                    builder.append(char)
-                    if (char == '-' && query.getOrNull(index + 1) == '-') // -- comment
-                        inComment = true
-                    if (char == '\n')
-                        inComment = false
-                }
-            }
-            add(builder.toString())
-        }
-
-        private companion object {
-
-            private val logger = KotlinLogging.logger { }
+            return removeCommentsAndInline(preparedStatement.toString())
         }
     }
 
