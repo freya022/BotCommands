935c4df888696d349dac7e7686c3ac595f21cc5c
freya02 <41875020+freya022@users.noreply.github.com>
2025-10-30 16:59:41 +0100

    Add `method`, `declaringKClass` and `declaringClass` to `Executable`
    Should help with command introspection,
    especially reading annotations on classes
    and Java reflect objects

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/core/Executable.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/core/Executable.kt
@@ -3,7 +3,11 @@ package io.github.freya022.botcommands.api.core
 import io.github.freya022.botcommands.api.core.options.Option
 import io.github.freya022.botcommands.api.core.utils.*
 import io.github.freya022.botcommands.api.parameters.AggregatedParameter
+import io.github.freya022.botcommands.internal.utils.ReflectionUtils.declaringClass
+import io.github.freya022.botcommands.internal.utils.javaMethodInternal
+import java.lang.reflect.Method
 import kotlin.collections.flatMap
+import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 
 /**
@@ -23,6 +27,26 @@ interface Executable {
      * This is strictly for introspection purposes, do not call this function manually.
      */
     val function: KFunction<*>
+
+    /**
+     * The target method of this executable.
+     *
+     * This is strictly for introspection purposes, do not call this method manually.
+     */
+    val method: Method
+        get() = function.javaMethodInternal
+
+    /**
+     * The class which declares the function of this executable.
+     */
+    val declaringKClass: KClass<*>
+        get() = function.declaringClass
+
+    /**
+     * The class which declares the method of this executable.
+     */
+    val declaringClass: Class<*>
+        get() = method.declaringClass
 
     /**
      * The parameters of this executable.
