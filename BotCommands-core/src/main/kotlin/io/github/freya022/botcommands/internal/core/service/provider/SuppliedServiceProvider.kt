2dfcbd01ae3728d6a2bd6d45c5203f42ebfb29e3
freya02 <41875020+freya022@users.noreply.github.com>
2025-11-23 16:26:53 +0100

    Apply common conditional annotations on supplied services

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/service/provider/SuppliedServiceProvider.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/service/provider/SuppliedServiceProvider.kt
@@ -27,7 +27,7 @@ internal class SuppliedServiceProvider internal constructor(
     override val priority = serviceSupplier.priority
 
     override fun canInstantiate(serviceContainer: BCServiceContainerImpl): ServiceError? {
-        return null
+        return commonCanInstantiate(serviceContainer, clazz, clazz)
     }
 
     override fun createInstance(serviceContainer: BCServiceContainerImpl): TimedInstantiation<*> {
