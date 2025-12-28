710333b5ce8638330b9cc02209389835fa1c949e
freya02 <41875020+freya022@users.noreply.github.com>
2025-12-28 14:15:28 +0100

    Add non-inline overload of `FunctionAnnotationsMap#getWithClassAnnotation`
    As inline functions cannot be mocked

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/service/FunctionAnnotationsMap.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/service/FunctionAnnotationsMap.kt
@@ -49,10 +49,13 @@ internal class FunctionAnnotationsMap(
     internal inline fun <reified A : Annotation> get(): Collection<ClassPathFunction> =
         get(A::class)
 
-    internal inline fun <reified CLASS_A : Annotation, reified FUNCTION_A : Annotation> getWithClassAnnotation(): List<ClassPathFunction> {
-        val classes = classAnnotationsMap.getOrNull<CLASS_A>() ?: return emptyList()
-        val functions = get<FUNCTION_A>()
+    internal fun <CLASS_A : Annotation, FUNCTION_A : Annotation> getWithClassAnnotation(classAnnotation: KClass<CLASS_A>, functionAnnotation: KClass<FUNCTION_A>): List<ClassPathFunction> {
+        val classes = classAnnotationsMap.getOrNull(classAnnotation) ?: return emptyList()
+        val functions = get(functionAnnotation)
 
         return functions.filter { it.clazz in classes }
     }
+
+    internal inline fun <reified CLASS_A : Annotation, reified FUNCTION_A : Annotation> getWithClassAnnotation() =
+        getWithClassAnnotation(CLASS_A::class, FUNCTION_A::class)
 }
