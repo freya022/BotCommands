eac47a4d423a115edb2232cd791a06daac6a957c
freya02 <41875020+freya022@users.noreply.github.com>
2026-02-07 16:34:34 +0100

    Added an internal application start event
    So the restarter can hook into it

---
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/internal/core/events/BApplicationStartEvent.kt
@@ -0,0 +1,5 @@
+package io.github.freya022.botcommands.internal.core.events
+
+import io.github.freya022.botcommands.api.core.config.BConfig
+
+class BApplicationStartEvent internal constructor(val config: BConfig)
