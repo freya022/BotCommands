a95655f341001bcd8c09e6e4aa0508d4e7daee76
freya02 <41875020+freya022@users.noreply.github.com>
2026-02-07 21:50:20 +0100

    Remove `KotlinTypeToken` failure tests

--- BotCommands-core/src/test/kotlin/io/github/freya022/botcommands/core/reflect/KotlinTypeTokenTest.kt
+++ BotCommands-core/src/test/kotlin/io/github/freya022/botcommands/core/reflect/KotlinTypeTokenTest.kt
@@ -3,7 +3,6 @@ package io.github.freya022.botcommands.core.reflect
 import io.github.freya022.botcommands.api.core.reflect.KotlinTypeToken
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
 
 object KotlinTypeTokenTest {
     @Test
@@ -11,21 +10,5 @@ object KotlinTypeTokenTest {
         assertDoesNotThrow { object : KotlinTypeToken<String>() {} }
         assertDoesNotThrow { object : KotlinTypeToken<List<String>>() {} }
         assertDoesNotThrow { object : KotlinTypeToken<List<*>>() {} }
-    }
-
-    @Test
-    fun `Invalid KotlinTypeToken`() {
-        assertThrows<IllegalArgumentException> { genericFunction<String>() }
-        assertThrows<IllegalArgumentException> { reifiedGenericFunction<String>() }
-    }
-
-    private fun <T> genericFunction() {
-        object : KotlinTypeToken<T>() {}
-    }
-
-    // KType#classifier returns null, but KType#javaType works,
-    // while it should be supported, the APIs use KType, and you can't get one from a Type.
-    private inline fun <reified T> reifiedGenericFunction(): KotlinTypeToken<T> {
-        return object : KotlinTypeToken<T>() {}
     }
 }
