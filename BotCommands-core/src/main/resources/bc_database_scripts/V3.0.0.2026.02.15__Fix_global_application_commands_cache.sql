149d60047583127434b46b14ed94dfb534b608a0
freya02 <41875020+freya022@users.noreply.github.com>
2026-02-15 22:27:21 +0100

    Fix H2 compat of 6ab2feb92e4169541bc8e6b4e743dbda854b002a

--- BotCommands-core/src/main/resources/bc_database_scripts/V3.0.0.2026.02.15__Fix_global_application_commands_cache.sql
+++ BotCommands-core/src/main/resources/bc_database_scripts/V3.0.0.2026.02.15__Fix_global_application_commands_cache.sql
@@ -7,10 +7,15 @@ UPDATE bc_version
 SET version = '3.0.0-beta.9'
 WHERE one_row = true;
 
+-- PostgreSQL
 ALTER TABLE application_commands_cache
-    DROP CONSTRAINT application_commands_cache_application_id_guild_id_key;
+    DROP IF EXISTS application_commands_cache_application_id_guild_id_key;
 
-TRUNCATE application_commands_cache;
+-- H2
+ALTER TABLE application_commands_cache
+    DROP IF EXISTS CONSTRAINT_INDEX_A;
+
+TRUNCATE TABLE application_commands_cache;
 
 ALTER TABLE application_commands_cache
-    ADD UNIQUE NULLS NOT DISTINCT (application_id, guild_id);
+    ADD CONSTRAINT app_commands_cache_unique_key UNIQUE NULLS NOT DISTINCT (application_id, guild_id);
