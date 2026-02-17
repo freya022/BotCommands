75d3e62e940738ff2990344ed8e633eaf57d62d2
freya02 <41875020+freya022@users.noreply.github.com>
2026-02-17 13:12:19 +0100

    Rewrite "Types supported by default" of `ModalParameterResolver`

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/parameters/resolvers/ModalParameterResolver.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/parameters/resolvers/ModalParameterResolver.kt
@@ -24,14 +24,36 @@ import kotlin.reflect.KType
  * Needs to be implemented alongside a [ParameterResolver] subclass.
  *
  * ### Types supported by default
- * - [TextInput] : `String`, when not filled by the user, empty or `null` if the parameter is explicitly nullable
- * - [StringSelectMenu] : `String` (can be `null`), `List<String>` (can be empty)
- * - [EntitySelectMenu] : [Mentions], `T` (can be `null`) and `List<T>` (can be empty) where `T` is one of:
+ * **Note:** For `null` to be supported, the parameter must be explicitly nullable.
+ *
+ * #### [TextInput]
+ * - `String` (can be empty, supports `null` when empty)
+ *
+ * #### [StringSelectMenu]
+ * - `String` when a single value can be selected (supports `null` when none selected)
+ * - `List<String>` (can be empty)
+ *
+ * #### [EntitySelectMenu]
+ * - [Mentions]
+ * - `T` (supports `null` when none selected)
+ * - `List<T>` (can be empty)
+ *
+ * Where `T` is one of:
  * [IMentionable], [Role], [User], [InputUser], [Member], [GuildChannel]
- * - [AttachmentUpload] : `List` of [Message.Attachment], can be empty, [Message.Attachment]
- * - [RadioGroup] : `String`, `null` when none selected
- * - [CheckboxGroup] : `List<String>`, can be empty
- * - [Checkbox] : (primitive)`Boolean`
+ *
+ * #### [AttachmentUpload]
+ * - `List` of [Message.Attachment] (can be empty)
+ * - [Message.Attachment] (supports `null` when none selected)
+ *
+ * #### [RadioGroup]
+ * - `String` (supports `null` when none selected)
+ *
+ * #### [CheckboxGroup]
+ * - `List<String>` (can be empty)
+ * - `String` when a single value can be selected
+ *
+ * #### [Checkbox]
+ * - (primitive) `Boolean`
  *
  * @param T Type of the implementation
  * @param R Type of the returned resolved objects
