b3f3ba97d36f14ca3ad79d2a9d9838f18c23ddaf
freya02 <41875020+freya022@users.noreply.github.com>
2026-03-01 19:10:28 +0100

    Use same docs model for opt-in annotation

--- BotCommands-restarter/src/main/kotlin/dev/freya02/botcommands/restarter/api/ExperimentalRestartApi.kt
+++ BotCommands-restarter/src/main/kotlin/dev/freya02/botcommands/restarter/api/ExperimentalRestartApi.kt
@@ -1,17 +1,31 @@
 package dev.freya02.botcommands.restarter.api
 
+import kotlin.annotation.AnnotationTarget.*
+
 /**
- * Opt-in marker annotation for the hot restart feature.
+ * Opt-in marker annotation for "hot restart" APIs that are considered experimental and are not subject to compatibility guarantees:
+ * The behavior of such API may be changed or the API may be removed completely in any further release.
  *
- * This feature provides no guarantee and its API may change (including removals) at any time.
+ * Please create an issue or join the Discord server if you encounter a problem or want to submit feedback.
  *
- * Please create an issue if you encounter a problem, including if it needs adaptations for your use case.
+ * Any usage of a declaration annotated with `@ExperimentalRestartApi` must be accepted either by
+ * annotating that usage with the [@OptIn][OptIn] annotation, e.g. `@OptIn(ExperimentalRestartApi::class)`,
+ * or by using the compiler argument `-opt-in=dev.freya02.botcommands.restarter.api.ExperimentalRestartApi`.
  */
-@RequiresOptIn(
-    message = "This feature is experimental, please see the documentation of this opt-in annotation (@ExperimentalRestartApi) for more details.",
-    level = RequiresOptIn.Level.ERROR
-)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.PROPERTY_SETTER)
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
 @Retention(AnnotationRetention.BINARY)
+@Target(
+    CLASS,
+    ANNOTATION_CLASS,
+    PROPERTY,
+    FIELD,
+    LOCAL_VARIABLE,
+    VALUE_PARAMETER,
+    CONSTRUCTOR,
+    FUNCTION,
+    PROPERTY_GETTER,
+    PROPERTY_SETTER,
+    TYPEALIAS
+)
 @MustBeDocumented
 annotation class ExperimentalRestartApi
