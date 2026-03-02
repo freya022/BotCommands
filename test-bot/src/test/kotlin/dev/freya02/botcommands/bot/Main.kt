90e513bc814da3435e2ad4460237825198634db7
freya02 <41875020+freya022@users.noreply.github.com>
2026-03-02 16:07:31 +0100

    test-bot: Rethrow `ImmediateRestartException`

--- test-bot/src/test/kotlin/dev/freya02/botcommands/bot/Main.kt
+++ test-bot/src/test/kotlin/dev/freya02/botcommands/bot/Main.kt
@@ -6,11 +6,13 @@ import dev.freya02.botcommands.method.accessors.api.MethodAccessorsConfig
 import dev.freya02.botcommands.method.accessors.api.annotations.ExperimentalMethodAccessorsApi
 import dev.freya02.botcommands.restarter.api.ExperimentalRestartApi
 import dev.freya02.botcommands.restarter.api.config.registerRestarter
+import dev.freya02.botcommands.restarter.api.exceptions.ImmediateRestartException
 import io.github.freya022.botcommands.api.core.BotCommands
 import io.github.freya022.botcommands.api.core.config.DevConfig
 import io.github.oshai.kotlinlogging.KotlinLogging
 import net.dv8tion.jda.api.interactions.DiscordLocale
 import kotlin.io.path.absolutePathString
+import kotlin.system.exitProcess
 import kotlin.time.Duration.Companion.milliseconds
 
 object Main {
@@ -18,59 +20,66 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<out String>) {
-        System.setProperty(ClassicConstants.CONFIG_FILE_PROPERTY, Environment.logbackConfigPath.absolutePathString())
-        logger.info { "Loading logback configuration at ${Environment.logbackConfigPath.absolutePathString()}" }
+        try {
+            System.setProperty(ClassicConstants.CONFIG_FILE_PROPERTY, Environment.logbackConfigPath.absolutePathString())
+            logger.info { "Loading logback configuration at ${Environment.logbackConfigPath.absolutePathString()}" }
 
-        @OptIn(ExperimentalMethodAccessorsApi::class)
-        MethodAccessorsConfig.preferClassFileAccessors()
+            @OptIn(ExperimentalMethodAccessorsApi::class)
+            MethodAccessorsConfig.preferClassFileAccessors()
 
-        BotCommands.create {
-            disableExceptionsInDMs = true
+            BotCommands.create {
+                disableExceptionsInDMs = true
 
-            addSearchPath("dev.freya02.botcommands.bot")
+                addSearchPath("dev.freya02.botcommands.bot")
 
-            database {
-                queryLogThreshold = 250.milliseconds
+                database {
+                    queryLogThreshold = 250.milliseconds
 
-                @OptIn(DevConfig::class)
-                dumpLongTransactions = true
-            }
-
-            localization {
-                responseBundles += "Test"
-            }
-
-            components {
-                enable = true
-            }
-
-            textCommands {
-                enable = true
-
-                usePingAsPrefix = true
-            }
-
-            services {
-                debug = false
-            }
-
-            applicationCommands {
-                enable = true
-
-                databaseCache {
                     @OptIn(DevConfig::class)
-                    checkOnline = true
+                    dumpLongTransactions = true
                 }
 
-                addLocalizations("MyCommands", DiscordLocale.ENGLISH_US, DiscordLocale.ENGLISH_UK, DiscordLocale.FRENCH)
-            }
+                localization {
+                    responseBundles += "Test"
+                }
 
-            modals {
-                enable = true
-            }
+                components {
+                    enable = true
+                }
 
-            @OptIn(ExperimentalRestartApi::class)
-            registerRestarter(args)
+                textCommands {
+                    enable = true
+
+                    usePingAsPrefix = true
+                }
+
+                services {
+                    debug = false
+                }
+
+                applicationCommands {
+                    enable = true
+
+                    databaseCache {
+                        @OptIn(DevConfig::class)
+                        checkOnline = true
+                    }
+
+                    addLocalizations("MyCommands", DiscordLocale.ENGLISH_US, DiscordLocale.ENGLISH_UK, DiscordLocale.FRENCH)
+                }
+
+                modals {
+                    enable = true
+                }
+
+                @OptIn(ExperimentalRestartApi::class)
+                registerRestarter(args)
+            }
+        } catch (e: ImmediateRestartException) {
+            throw e
+        } catch (e: Exception) {
+            logger.error(e) { "Could not start the test bot" }
+            exitProcess(1)
         }
     }
 }
