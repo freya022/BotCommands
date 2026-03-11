021dcf79833715ffe21d3823a86aa8d27f7cbfd6
freya02 <41875020+freya022@users.noreply.github.com>
2026-03-11 12:48:42 +0100

    Document `TextCommandOptionBuilder`

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/text/options/builder/TextCommandOptionBuilder.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/text/options/builder/TextCommandOptionBuilder.kt
@@ -1,8 +1,19 @@
 package io.github.freya022.botcommands.api.commands.text.options.builder
 
 import io.github.freya022.botcommands.api.commands.options.builder.CommandOptionBuilder
+import io.github.freya022.botcommands.api.parameters.resolvers.TextParameterResolver
 
 interface TextCommandOptionBuilder : CommandOptionBuilder {
+    /**
+     * Example input for the text command option,
+     * displayed on the "Example" part of the built-in help command.
+     */
     var helpExample: String?
+
+    /**
+     * Whether this option needs to be considered as a snowflake ID.
+     *
+     * Mainly used by [TextParameterResolver.getHelpExample].
+     */
     var isId: Boolean
 }
