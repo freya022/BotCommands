3cbbb82bdb497c69c5e4df2e0d3cdabfc32f4ff4
freya02 <41875020+freya022@users.noreply.github.com>
2026-05-02 12:59:49 +0200

    components: Remove pointless factory of RoleResolver

--- BotCommands-components/src/main/kotlin/io/github/freya022/botcommands/internal/components/resolvers/RoleResolver.kt
+++ BotCommands-components/src/main/kotlin/io/github/freya022/botcommands/internal/components/resolvers/RoleResolver.kt
@@ -2,11 +2,15 @@ package io.github.freya022.botcommands.internal.components.resolvers
 
 import io.github.freya022.botcommands.api.components.options.ComponentOption
 import io.github.freya022.botcommands.api.components.serialization.SerializedComponentData
+import io.github.freya022.botcommands.api.core.service.annotations.Resolver
+import io.github.freya022.botcommands.api.core.service.annotations.ServiceName
 import io.github.freya022.botcommands.api.parameters.ClassParameterResolver
 import io.github.freya022.botcommands.api.parameters.resolvers.ComponentParameterResolver
 import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.events.interaction.component.GenericComponentInteractionCreateEvent
 
+@Resolver
+@ServiceName("componentRoleResolver")
 internal class RoleResolver :
         ClassParameterResolver<RoleResolver, Role>(Role::class),
         ComponentParameterResolver<RoleResolver, Role> {
