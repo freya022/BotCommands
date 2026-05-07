76fb9fdf6ba4cc08baa197a248b447c543cc4458
freya02 <41875020+freya022@users.noreply.github.com>
2026-05-07 15:23:27 +0200

    Accessors. kotlin-reflect: Move KParameters to default method accessors
    Reduces retained objects for direct calls, as they are only necessary for default calls
    
    Also stopped inlining `argsToMap` as it was trivial

--- BotCommands-method-accessors/kotlin-reflect/src/main/kotlin/dev/freya02/botcommands/method/accessors/internal/AbstractKotlinReflectMethodAccessor.kt
+++ BotCommands-method-accessors/kotlin-reflect/src/main/kotlin/dev/freya02/botcommands/method/accessors/internal/AbstractKotlinReflectMethodAccessor.kt
@@ -7,22 +7,9 @@ internal abstract class AbstractKotlinReflectMethodAccessor<R>(
     protected val function: KFunction<R>,
 ) : MethodAccessor<R> {
 
-    protected val parameters = function.parameters.filter { it.kind != KParameter.Kind.INSTANCE }
-    private val parameterCount = parameters.size
+    private val parameterCount = function.parameters.count { it.kind != KParameter.Kind.INSTANCE }
 
     override fun createBlankArguments(): MethodArguments {
         return MethodArguments(parameterCount)
-    }
-
-    protected inline fun argsToMap(args: MethodArguments, block: MutableMap<KParameter, Any?>.() -> Unit = {}): Map<KParameter, Any?> {
-        return buildMap(args.size()) {
-            block()
-            parameters.forEachIndexed { index, parameter ->
-                val arg = args[index]
-                if (arg != MethodArguments.NO_VALUE) {
-                    this[parameter] = arg
-                }
-            }
-        }
     }
 }
