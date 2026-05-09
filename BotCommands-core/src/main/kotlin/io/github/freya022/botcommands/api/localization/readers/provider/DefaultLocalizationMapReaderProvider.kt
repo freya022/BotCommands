4983de5975a1e241ce7ad5c47cb4884b2e6347ac
freya02 <41875020+freya022@users.noreply.github.com>
2026-05-09 19:00:40 +0200

    Make the `DefaultLocalizationMapProvider` and `DefaultLocalizationMapReader` instances lazily created

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/localization/readers/provider/DefaultLocalizationMapReaderProvider.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/localization/readers/provider/DefaultLocalizationMapReaderProvider.kt
@@ -5,14 +5,17 @@ import com.fasterxml.jackson.core.json.JsonReadFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.github.freya022.botcommands.api.core.BContext
 import io.github.freya022.botcommands.api.core.service.annotations.BService
+import io.github.freya022.botcommands.api.core.service.annotations.Lazy
 import io.github.freya022.botcommands.api.localization.readers.JacksonLocalizationMapReader
 import io.github.freya022.botcommands.api.localization.readers.LocalizationMapReader
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+@Lazy
 @Configuration
 @BService
 internal open class DefaultLocalizationMapReaderProvider {
+    @Lazy
     @Bean("builtinJsonLocalizationMapReader")
     @BService(name = "builtinJsonLocalizationMapReader")
     open fun defaultJsonLocalizationMapReader(context: BContext): LocalizationMapReader {
