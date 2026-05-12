4369cf64632317d9d56ea3f46a7b4535de4313eb
freya02 <41875020+freya022@users.noreply.github.com>
2026-05-12 13:04:49 +0200

    Specify nullabilities in `RichTextType`

--- BotCommands-core/src/main/java/io/github/freya022/botcommands/api/utils/RichTextType.java
+++ BotCommands-core/src/main/java/io/github/freya022/botcommands/api/utils/RichTextType.java
@@ -1,9 +1,12 @@
 package io.github.freya022.botcommands.api.utils;
 
 import net.dv8tion.jda.api.entities.Message.MentionType;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 
 import java.util.regex.Pattern;
 
+@NullMarked
 public enum RichTextType {
     USER(MentionType.USER),
     EMOJI(MentionType.EMOJI),
@@ -15,17 +18,21 @@ public enum RichTextType {
     URL(null),
     TEXT(null);
 
+    @Nullable
     private final MentionType mentionType;
 
-    RichTextType(MentionType mentionType) {
+    RichTextType(@Nullable MentionType mentionType) {
         this.mentionType = mentionType;
     }
 
+    @Nullable
     public MentionType getMentionType() {
         return mentionType;
     }
 
+    @Nullable
     public Pattern getPattern() {
+        if (mentionType == null) return null;
         return mentionType.getPattern();
     }
 }
