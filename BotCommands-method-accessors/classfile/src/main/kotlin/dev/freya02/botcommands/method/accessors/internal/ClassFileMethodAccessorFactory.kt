cc00d8ffab1b893b65e98bc942967e503839ea8f
freya02 <41875020+freya022@users.noreply.github.com>
2026-05-25 14:28:00 +0200

    method accessors: Add support for inner class constructors

--- BotCommands-method-accessors/classfile/src/main/kotlin/dev/freya02/botcommands/method/accessors/internal/ClassFileMethodAccessorFactory.kt
+++ BotCommands-method-accessors/classfile/src/main/kotlin/dev/freya02/botcommands/method/accessors/internal/ClassFileMethodAccessorFactory.kt
@@ -1,8 +1,10 @@
 package dev.freya02.botcommands.method.accessors.internal
 
 import dev.freya02.botcommands.method.accessors.internal.codegen.ClassFileMethodAccessorGenerator
+import dev.freya02.botcommands.method.accessors.internal.utils.isInnerClass
 import dev.freya02.botcommands.method.accessors.internal.utils.javaExecutable
 import java.lang.invoke.MethodHandles
+import java.lang.reflect.Constructor
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 import kotlin.reflect.KFunction
@@ -16,10 +18,17 @@ class ClassFileMethodAccessorFactory : MethodAccessorFactory {
         function: KFunction<R>,
     ): MethodAccessor<R> {
         val executable = function.javaExecutable
+        // Require an instance for non-static methods or inner class constructors
         if (executable is Method && !Modifier.isStatic(executable.modifiers)) {
             requireNotNull(instance)
             require(executable.declaringClass.isAssignableFrom(instance.javaClass)) {
                 "Function is not from the instance's class, function: ${executable.declaringClass.name}, instance: ${instance.javaClass.name}"
+            }
+        } else if (executable is Constructor<*> && executable.declaringClass.isInnerClass) {
+            requireNotNull(instance)
+            val outerClass = executable.declaringClass.declaringClass
+            require(outerClass.isAssignableFrom(instance.javaClass)) {
+                "Passed instance is not assignable to the outer class of this inner class constructor, required: ${outerClass.name}, instance: ${instance.javaClass.name}"
             }
         }
 
--- BotCommands-method-accessors/kotlin-reflect/src/main/kotlin/dev/freya02/botcommands/method/accessors/internal/invoker/direct/JavaReflectDirectMethodAccessor.kt
+++ BotCommands-method-accessors/kotlin-reflect/src/main/kotlin/dev/freya02/botcommands/method/accessors/internal/invoker/direct/JavaReflectDirectMethodAccessor.kt
@@ -2,14 +2,18 @@ package dev.freya02.botcommands.method.accessors.internal.invoker.direct
 
 import dev.freya02.botcommands.method.accessors.internal.MethodAccessor
 import dev.freya02.botcommands.method.accessors.internal.MethodArguments
+import dev.freya02.botcommands.method.accessors.internal.utils.isInnerClass
 import java.lang.reflect.Constructor
 import java.lang.reflect.Executable
 import java.lang.reflect.Method
 
 internal class JavaReflectDirectMethodAccessor<R> internal constructor(
     private val instance: Any?,
-    private val method: Executable,
+    private val executable: Executable,
 ) : MethodAccessor<R> {
+
+    // Java treats outer class as an argument, but we always pass it as the instance
+    private val argumentCount = executable.parameterCount - if (executable is Constructor<*> && executable.declaringClass.isInnerClass) 1 else 0
 
     override fun hasInstance(): Boolean {
         return instance != null
@@ -17,21 +21,23 @@ internal class JavaReflectDirectMethodAccessor<R> internal constructor(
 
     @Suppress("UNCHECKED_CAST")
     override suspend fun callSuspend(args: MethodArguments): R {
-        return when (method) {
-            is Constructor<*> -> method.newInstance(*args.args)
-            is Method -> method.invoke(instance, *args.args)
+        return when (executable) {
+            is Constructor<*> if instance != null -> executable.newInstance(instance, *args.args)
+            is Constructor<*> -> executable.newInstance(*args.args)
+            is Method -> executable.invoke(instance, *args.args)
         } as R
     }
 
     @Suppress("UNCHECKED_CAST")
     override fun call(args: MethodArguments): R {
-        return when (method) {
-            is Constructor<*> -> method.newInstance(*args.args)
-            is Method -> method.invoke(instance, *args.args)
+        return when (executable) {
+            is Constructor<*> if instance != null -> executable.newInstance(instance, *args.args)
+            is Constructor<*> -> executable.newInstance(*args.args)
+            is Method -> executable.invoke(instance, *args.args)
         } as R
     }
 
     override fun createBlankArguments(): MethodArguments {
-        return MethodArguments(method.parameterCount)
+        return MethodArguments(argumentCount)
     }
 }
