db7fcc404a9fa03e88284462275c446f0b6cad23
freya02 <41875020+freya022@users.noreply.github.com>
2026-06-27 18:28:39 +0200

    jda-ktx: Add file type filtering to `AttachmentUpload`

--- BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/components/AttachmentUpload.kt
+++ BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/components/AttachmentUpload.kt
@@ -2,6 +2,8 @@ package dev.freya02.botcommands.jda.ktx.components
 
 import net.dv8tion.jda.api.components.Component
 import net.dv8tion.jda.api.components.attachmentupload.AttachmentUpload
+import net.dv8tion.jda.api.interactions.FileType
+import net.dv8tion.jda.api.interactions.IFilterableFileTypes
 
 class InlineAttachmentUpload(
     val builder: AttachmentUpload.Builder,
@@ -48,26 +50,119 @@ class InlineAttachmentUpload(
             builder.setMaxValues(value)
         }
 
+    /**
+     * The file types this attachment upload is accepting, up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES].
+     *
+     * @see FileType
+     */
+    val fileTypes = FileTypeAccumulator()
+
+    /**
+     * Adds up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] file extensions to filter for.
+     *
+     * @param  extensions The extensions, up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES]
+     *
+     * @throws IllegalArgumentException There are more than [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] extensions,
+     *                                  or, an extension is empty or isn't alphanumeric
+     *
+     * @see FileType
+     */
+    fun addFileTypeExtensions(extensions: List<String>) {
+        fileTypes += extensions
+    }
+
+    /**
+     * Adds up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] file extensions to filter for.
+     *
+     * @param  extensions The extensions, up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES]
+     *
+     * @throws IllegalArgumentException There are more than [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] extensions,
+     *                                  or, an extension is empty or isn't alphanumeric
+     *
+     * @see FileType
+     */
+    fun addFileTypeExtensions(vararg extensions: String) {
+        fileTypes += extensions.asList()
+    }
+
+    /**
+     * Sets up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] file extensions to filter for.
+     * Leave the arguments empty to remove file type filtering.
+     *
+     * @param  extensions The extensions, up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES]
+     *
+     * @throws IllegalArgumentException There are more than [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] extensions,
+     *                                  or, an extension is empty or isn't alphanumeric
+     *
+     * @see FileType
+     */
+    fun setFileTypeExtensions(extensions: List<String>) {
+        fileTypes.clear()
+        fileTypes += extensions
+    }
+
+    /**
+     * Sets up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] file extensions to filter for.
+     * Leave the arguments empty to remove file type filtering.
+     *
+     * @param  extensions The extensions, up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES]
+     *
+     * @throws IllegalArgumentException There are more than [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES] extensions,
+     *                                  or, an extension is empty or isn't alphanumeric
+     *
+     * @see FileType
+     */
+    fun setFileTypeExtensions(vararg extensions: String) {
+        fileTypes.clear()
+        fileTypes += extensions.asList()
+    }
+
     /** See [AttachmentUpload.Builder.build] */
     fun build(): AttachmentUpload {
         return builder.build()
+    }
+
+    inner class FileTypeAccumulator {
+        @JvmName("plusAssignExtensions")
+        operator fun plusAssign(extensions: Collection<String>) {
+            builder.addFileTypeExtensions(extensions)
+        }
+
+        operator fun plusAssign(extension: String) {
+            builder.addFileTypeExtensions(extension)
+        }
+
+        @JvmName("plusAssignFileTypes")
+        operator fun plusAssign(extensions: Collection<FileType>) {
+            builder.addFileTypes(extensions)
+        }
+
+        operator fun plusAssign(extension: FileType) {
+            builder.addFileTypes(extension)
+        }
+
+        fun clear() {
+            builder.setFileTypes()
+        }
     }
 }
 
 /**
  * Component accepting files from users, see [AttachmentUpload][net.dv8tion.jda.api.components.attachmentupload.AttachmentUpload].
  *
- * @param customId The custom ID of the input, see [AttachmentUpload.Builder.setCustomId]
- * @param uniqueId Unique identifier of this component, see [Component.withUniqueId]
- * @param required Whether the user must upload files, see [AttachmentUpload.Builder.setRequired]
- * @param range    Minimum and maximum amount of files a user can send, see [AttachmentUpload.Builder.setRequiredRange]
- * @param block    Lambda allowing further configuration
+ * @param customId  The custom ID of the input, see [AttachmentUpload.Builder.setCustomId]
+ * @param uniqueId  Unique identifier of this component, see [Component.withUniqueId]
+ * @param required  Whether the user must upload files, see [AttachmentUpload.Builder.setRequired]
+ * @param range     Minimum and maximum amount of files a user can send, see [AttachmentUpload.Builder.setRequiredRange]
+ * @param fileTypes The file types to accept, up to [MAX_FILE_TYPES][IFilterableFileTypes.MAX_FILE_TYPES]
+ * @param block     Lambda allowing further configuration
  */
 inline fun AttachmentUpload(
     customId: String,
     uniqueId: Int = -1,
     required: Boolean = true,
     range: IntRange? = null,
+    fileTypes: Collection<FileType> = emptyList(),
     block: InlineAttachmentUpload.() -> Unit = {},
 ): AttachmentUpload {
     return AttachmentUpload.create(customId)
@@ -79,6 +174,7 @@ inline fun AttachmentUpload(
                 this.required = false
             if (range != null)
                 this.range = range
+            this.fileTypes += fileTypes
             block()
         }
         .build()
