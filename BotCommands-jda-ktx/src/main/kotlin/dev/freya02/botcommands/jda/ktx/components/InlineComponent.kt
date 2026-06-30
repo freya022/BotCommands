d1060c7d772fdb03c504ce088900306025a1e0b8
freya02 <41875020+freya022@users.noreply.github.com>
2026-06-30 21:11:05 +0200

    Modals: Add `InlineComponentDSL` scope
    To avoid using methods of a modal DSL inside a component's DSL

--- BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/components/InlineComponent.kt
+++ BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/components/InlineComponent.kt
@@ -3,7 +3,7 @@ package dev.freya02.botcommands.jda.ktx.components
 import net.dv8tion.jda.api.components.Component
 
 @DslMarker
-internal annotation class InlineComponentDSL
+annotation class InlineComponentDSL
 
 @InlineComponentDSL
 interface InlineComponent {
