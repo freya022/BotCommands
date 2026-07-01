85bc582e047953b03b50eb0c4103f516afe00a0b
freya02 <41875020+freya022@users.noreply.github.com>
2026-07-01 16:33:47 +0200

    Make exception constructors internal

--- BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/retrieve/InvalidChannelTypeException.kt
+++ BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/retrieve/InvalidChannelTypeException.kt
@@ -3,4 +3,4 @@ package dev.freya02.botcommands.jda.ktx.retrieve
 /**
  * Exception thrown when retrieving a channel by ID, but the type is incorrect.
  */
-class InvalidChannelTypeException(message: String) : IllegalArgumentException(message)
+class InvalidChannelTypeException internal constructor(message: String) : IllegalArgumentException(message)
--- BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/retrieve/ParentGuildMismatchException.kt
+++ BotCommands-jda-ktx/src/main/kotlin/dev/freya02/botcommands/jda/ktx/retrieve/ParentGuildMismatchException.kt
@@ -3,4 +3,4 @@ package dev.freya02.botcommands.jda.ktx.retrieve
 /**
  * Exception thrown when retrieving a channel by ID from a guild, but their expected guild is incorrect.
  */
-class ParentGuildMismatchException(message: String) : IllegalArgumentException(message)
+class ParentGuildMismatchException internal constructor(message: String) : IllegalArgumentException(message)
