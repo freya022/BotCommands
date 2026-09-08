4d68c0435a5a5a2f6b40a02cb224d3d3f2464c57
freya02 <41875020+freya022@users.noreply.github.com>
2026-09-08 11:47:08 +0200

    app. commands: Check range in `ValueRange`

--- BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/application/ValueRange.kt
+++ BotCommands-core/src/main/kotlin/io/github/freya022/botcommands/api/commands/application/ValueRange.kt
@@ -1,6 +1,18 @@
 package io.github.freya022.botcommands.api.commands.application
 
+import net.dv8tion.jda.api.interactions.commands.build.OptionData
+
 class ValueRange private constructor(val min: Number, val max: Number) {
+
+    init {
+        require(min.toDouble() in OptionData.MIN_NEGATIVE_NUMBER..OptionData.MAX_POSITIVE_NUMBER) {
+            "Minimum value must be between ${OptionData.MIN_NEGATIVE_NUMBER} and ${OptionData.MAX_POSITIVE_NUMBER}, provided: $min)"
+        }
+        require(max.toDouble() in OptionData.MIN_NEGATIVE_NUMBER..OptionData.MAX_POSITIVE_NUMBER) {
+            "Maximum value must be between ${OptionData.MIN_NEGATIVE_NUMBER} and ${OptionData.MAX_POSITIVE_NUMBER}, provided: $max)"
+        }
+    }
+
     companion object {
         @JvmStatic
         fun ofLong(minValue: Long, maxValue: Long) =
